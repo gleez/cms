1bf3a07f004728153819d02e188a51c52f7cc64d
Sergey Yakovlev <sadhooklay@gmail.com>
2012-12-11 01:21:35 +0400

    Added phpdoc and minor changes in Gleez Theme

--- modules/gleez/classes/gleez/theme.php
+++ modules/gleez/classes/gleez/theme.php
@@ -17,19 +17,26 @@
  */
 class Gleez_Theme {
 
+  /** @var string The file contains information about theme */
+  const INFO_FILE = 'theme.info';
+
   /** @var string Site theme name */
-  public static $site_theme_name = 'anytime';
+  public static $site_theme_name = 'fluid';
 
   /** @var string  admin theme name
    */
-  public static $admin_theme_name = 'anytime';
+  public static $admin_theme_name = 'fluid';
 
   /** @var boolean Admin? */
   public static $is_admin = FALSE;
 
   /**
-   * Load the active theme. This is called at bootstrap time.
+   * Load the active theme.
+   *
+   * This is called at bootstrap time.
    * We will only ever have one theme active for any given request.
+   *
+   * @uses Kohana::modules
    */
   public static function load_themes()
   {
@@ -38,13 +45,13 @@ class Gleez_Theme {
     if (self::$is_admin)
     {
       // Load the admin theme
-      self::$admin_theme_name = $config->get('admin_theme', 'anytime');
+      self::$admin_theme_name = $config->get('admin_theme', self::$admin_theme_name);
       $array  = array(self::$admin_theme_name => THEMEPATH . self::$admin_theme_name);
     }
     else
     {
       // Load the site theme
-      self::$site_theme_name  = $config->get('theme', 'anytime');
+      self::$site_theme_name  = $config->get('theme', self::$site_theme_name);
       $array = array(self::$site_theme_name => THEMEPATH . self::$site_theme_name);
     }
 
@@ -55,24 +62,38 @@ class Gleez_Theme {
     unset($array);
   }
 
+  /**
+   * Gets info abou theme
+   *
+   * @param   string        $theme_name   Theme name
+   * @return  \ArrayObject  An array containinig information about theme
+   */
   public static function get_info($theme_name)
   {
-    $file                    = THEMEPATH . "$theme_name/theme.info";
-    $theme_info              = new ArrayObject(parse_ini_file($file), ArrayObject::ARRAY_AS_PROPS);
+    $info_file = THEMEPATH.$theme_name.DIRECTORY_SEPARATOR.Theme::INFO_FILE;
+    $theme_info = new ArrayObject(parse_ini_file($info_file), ArrayObject::ARRAY_AS_PROPS);
+    $theme_info->title = __($theme_info->title);
     $theme_info->description = __($theme_info->description);
-    $theme_info->name        = __($theme_info->name);
 
     return $theme_info;
   }
 
+  /**
+   * Gets list of avaliable themes
+   *
+   * @return  array  Avaliable themes aray
+   */
   public static function avaliable()
   {
     $themes = array();
 
     foreach (scandir(THEMEPATH) as $theme_name)
     {
-      if (file_exists(THEMEPATH . "$theme_name/theme.info"))
+      $info_file = THEMEPATH.$theme_name.DIRECTORY_SEPARATOR.Theme::INFO_FILE;
+      // File can be exists and can be readable
+      if (is_readable($info_file))
       {
+        // Skip hidden files
         if ($theme_name[0] == ".")
         {
           continue;
