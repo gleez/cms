10518876bb732f3738a43ff3cc13861f9152f941
Sergey Yakovlev <sadhooklay@gmail.com>
2012-12-11 04:33:34 +0400

    Cleaned code, minor fixes in Gleez_Log_File

--- modules/gleez/classes/gleez/log.php
+++ modules/gleez/classes/gleez/log.php
@@ -6,26 +6,31 @@
  *
  * Override add() with information hostname, referer, user, url, user_agent
  *
- * @package	Gleez
- * @category	Logging
- * @author	Sandeep Sangamreddi - Gleez
- * @copyright	(c) 2012 Gleez Technologies
- * @license	http://gleezcms.org/license
+ * @package   Gleez
+ * @category  Logging
+ * @author    Sandeep Sangamreddi - Gleez
+ * @copyright (c) 2012 Gleez Technologies
+ * @license   http://gleezcms.org/license
  */
 class Gleez_log extends Kohana_Log {
 
 	/**
-	 * Adds a message to the log. Replacement values must be passed in to be
+	 * Adds a message to the log.
+   *
+   * Replacement values must be passed in to be
 	 * replaced using [strtr](http://php.net/strtr).
 	 *
-	 *     $log->add(Log::ERROR, 'Could not locate user: :user', array(
-	 *         ':user' => $username,
+   *    // Usage
+	 *    $log->add(Log::ERROR, 'Could not locate user: :user',
+   *      array(
+   *        ':user' => $username,
 	 *     ));
 	 *
-	 * @param   string  level of message
-	 * @param   string  message body
-	 * @param   array   values to replace in the message
+	 * @param   string  $level    Level of message
+	 * @param   string  $message  Message body
+	 * @param   array   $values   Values to replace in the message [Optional]
 	 * @return  Log
+   * @uses    Date::formatted_tine For time form
 	 */
 	public function add($level, $message, array $values = NULL)
 	{
@@ -38,9 +43,9 @@ class Gleez_log extends Kohana_Log {
 		// Create a new message and timestamp it
 		$this->_messages[] = array
 		(
-			'time'       => Date::formatted_time('now', Log::$timestamp, Log::$timezone),
-			'level'      => $level,
-			'body'       => $message,
+			'time'    => Date::formatted_time('now', Log::$timestamp, Log::$timezone),
+			'level'   => $level,
+			'body'    => $message,
 		);
 
 		if (Log::$write_on_add)
@@ -51,5 +56,5 @@ class Gleez_log extends Kohana_Log {
 
 		return $this;
 	}
-        
+
 }
