fd6bac90095bb9976020bae3554a74c7be781263
Sergey Yakovlev <sadhooklay@gmail.com>
2012-12-24 06:18:06 +0400

    Minor changes in System Helper

--- modules/gleez/classes/gleez/system.php
+++ modules/gleez/classes/gleez/system.php
@@ -15,17 +15,20 @@ class Gleez_System {
    * Get the server load averages (if possible)
    *
    * @return  string
-   * @link http://php.net/manual/en/function.sys-getloadavg.php sys-getloadavg()
+   * @link    http://php.net/manual/en/function.sys-getloadavg.php sys-getloadavg()
    */
   public static function get_avg()
   {
+    // Default return
+    $server_load = __('Not available');
+
     if (function_exists('sys_getloadavg') && is_array(sys_getloadavg()))
     {
       $load_averages = sys_getloadavg();
       array_walk($load_averages, create_function('&$v', '$v = round($v, 3);'));
       $server_load = $load_averages[0].' '.$load_averages[1].' '.$load_averages[2];
     }
-    else if (@is_readable('/proc/loadavg'))
+    elseif (@is_readable('/proc/loadavg'))
     {
       // We use @ just in case
       $fh = @fopen('/proc/loadavg', 'r');
@@ -34,15 +37,12 @@ class Gleez_System {
 
       $load_averages = empty($load_averages) ? array() : explode(' ', $load_averages);
 
-      $server_load = isset($load_averages[2]) ? $load_averages[0].' '.$load_averages[1].' '.$load_averages[2] : 'Not available';
+      $server_load = isset($load_averages[2]) ? $load_averages[0].' '.$load_averages[1].' '.$load_averages[2] : __('Not available');
     }
-    else if (! in_array(PHP_OS, array('WINNT', 'WIN32')) && preg_match('/averages?: ([0-9\.]+),[\s]+([0-9\.]+),[\s]+([0-9\.]+)/i', @exec('uptime'), $load_averages))
+    elseif (! in_array(PHP_OS, array('WINNT', 'WIN32')) && preg_match('/averages?: ([0-9\.]+),[\s]+([0-9\.]+),[\s]+([0-9\.]+)/i', @exec('uptime'), $load_averages))
     {
       $server_load = $load_averages[1].' '.$load_averages[2].' '.$load_averages[3];
     }
-
-    else
-      $server_load = $lang_admin_index['Not available'];
 
     return $server_load;
   }
@@ -58,7 +58,7 @@ class Gleez_System {
    * @param   boolean $recursive  Create directories recursively if necessary [Optional]
    * @return  boolean             Returns TRUE on success or FALSE on failure
    *
-   * @see     http://php.net/manual/en/function.mkdir.php
+   * @link    http://php.net/manual/en/function.mkdir.php mkdir()
    */
   public static function mkdir($path, $mode = 0777, $recursive = TRUE)
   {
