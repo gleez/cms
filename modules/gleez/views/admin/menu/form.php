c5c1c6992ec44cb961755adce6e223b7d8a8c81b
Sergey Yakovlev <sadhooklay@gmail.com>
2013-01-15 14:16:00 +0400

    Minor changes in menu form views

--- modules/gleez/views/admin/menu/form.php
+++ modules/gleez/views/admin/menu/form.php
@@ -1,46 +1,36 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
 <div class="help">
-        <?php echo __('Add new menus to your site, edit existing menus, and rename and reorganize menu links.') ?>
+	<?php echo __('Add new menus to your site, edit existing menus, and rename and reorganize menu links.') ?>
 </div>
-    
+
 <?php $parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add');
 	echo Form::open(Route::get('admin/menu')->uri($parms), array('id'=>'menu-form ', 'class'=>'menu-form form form-horizontal well')) ?>
-    
-<?php if ( ! empty($errors)): ?>
-	<div id="formerrors" class="errorbox">
-		<h3>Ooops!</h3>
-		<ol>
-			<?php foreach($errors as $field => $message): ?>
-				<li> <?php echo $message ?> </li>
-			<?php endforeach ?>
-		</ol>
-	</div>
-<?php endif ?>
+
+	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
 	<div class="control-group <?php echo isset($errors['title']) ? 'error': ''; ?>">
-		<?php echo Form::label('title', 'Title:', array('class' => 'control-label')) ?>
+		<?php echo Form::label('title', __('Title:'), array('class' => 'control-label')) ?>
 		<div class="controls">
 			<?php echo Form::input('title', $post->title, array('class' => 'input-xlarge')); ?>
 		</div>
 	</div>
 
 	<div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
-		<?php echo Form::label('name', 'Slug:', array('class' => 'control-label')) ?>
+		<?php echo Form::label('name', __('Slug:'), array('class' => 'control-label')) ?>
 		<div class="controls">
 			<?php echo Form::input('name', $post->name, array('class' => 'input-xlarge')); ?>
 		</div>
 	</div>
 
 	<div class="control-group <?php echo isset($errors['descp']) ? 'error': ''; ?>">
-		<?php echo Form::label('description', 'Description:', array('class' => 'control-label')) ?>
-		<div class="controls">	
+		<?php echo Form::label('description', __('Description:'), array('class' => 'control-label')) ?>
+		<div class="controls">
 			<?php echo Form::textarea('descp', $post->descp, array('class' => 'input-xlarge', 'rows' => 3)) ?>
 		</div>
 	</div>
 
-<div class="clearfix"></div>
-<?php echo Form::submit('menu', __('Submit'), array('class' => 'btn btn-primary btn-large')) ?>
+	<div class="clearfix"></div>
+	<?php echo Form::submit('menu', __('Submit'), array('class' => 'btn btn-primary btn-large')) ?>
 
 <?php echo Form::close() ?>
-	
--- modules/gleez/views/admin/menu/item/form.php
+++ modules/gleez/views/admin/menu/item/form.php
@@ -1,26 +1,15 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 
 <?php
-        $parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add', 'id' => $menu->id);
+	$parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add', 'id' => $menu->id);
 	$items = isset($post->id) ? $post->select_list('id', 'title', '--') : $menu->select_list('id', 'title', '--');
 
 	echo Form::open(Route::get('admin/menu/item')->uri($parms), array('id'=>'menu-form', 'class'=>'form form-horizontal well')) ?>
 
-	<?php if ( ! empty($errors)): ?>
-		<div id="formerrors" class="errorbox">
-			<h3>Ooops!</h3>
-			<ol>
-				<?php foreach($errors as $field => $message): ?>
-					<li>	
-						<?php echo $message ?>
-					</li>
-				<?php endforeach ?>
-			</ol>
-		</div>
-	<?php endif ?>
+	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
 <div class="control-group <?php echo isset($errors['title']) ? 'error': ''; ?>">
-	<?php echo Form::label('title', __('Title: '), array('class' => 'control-label')) ?>
+	<?php echo Form::label('title', __('Title:'), array('class' => 'control-label')) ?>
 	<div class="controls">
 		<?php echo Form::input('title', $post->title, array('class' => 'input-large')); ?>
 	</div>
@@ -35,7 +24,7 @@
 
 <div class="control-group <?php echo isset($errors['url']) ? 'error': ''; ?>">
 	<?php echo Form::label('url', __('Link:'), array('class' => 'control-label')) ?>
-   	<div class="controls">
+	<div class="controls">
 		<?php echo Form::input('url', $post->url, array('class' => 'input-large')); ?>
 	</div>
 </div>
@@ -55,7 +44,7 @@
 			<?php echo Form::rawselect('image', Menu::icons(), $post->image, array('class' => 'input-large')); ?>
 		</div>
 	</div>
-	
+
 <div class="control-group <?php echo isset($errors['descp']) ? 'error': ''; ?>">
  	<?php echo Form::label('description', __('Description:'), array('class' => 'control-label')) ?>
 	<div class="controls">
