0ac234dfe987ea92dd22fd3039036a7017fab03f
Sergey Yakovlev <sadhooklay@gmail.com>
2013-02-13 19:03:02 +0400

    Resulted `errors/partial` view into correct Twitter Bootstrap style

--- modules/gleez/views/errors/partial.php
+++ modules/gleez/views/errors/partial.php
@@ -1,12 +1,10 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
 	<?php if ( ! empty($errors)): ?>
-		<div id="formerrors" class="errorbox">
-			<h3>Ooops!</h3>
-			<ol>
+		<div class="alert alert-error alert-block">
+			<h4><?php echo __('Error'); ?></h4>
 				<?php foreach($errors as $field => $message): ?>
-					<li> <?php echo $message ?> </li>
+					<p><?php echo $message ?></p>
 				<?php endforeach ?>
-			</ol>
 		</div>
-	<?php endif ?>
+	<?php endif; ?>
