a233e794b71fa064fd749aea5b460588009325aa
Sergey Yakovlev <sadhooklay@gmail.com>
2013-02-19 04:03:19 +0400

    Added initial file for Russian localization in Gleez User.

---
+++ modules/user/i18n/ru.php
@@ -0,0 +1,57 @@
+<?php defined("SYSPATH") OR die("No direct script access.");
+/**
+ * Localized strings into Russian
+ *
+ * @package   User\I18n
+ * @author    Sergey Yakovlev - Gleez
+ * @copyright (c) 2013 Gleez Technologies
+ * @license   http://gleezcms.org/license
+ */
+  return array
+  (
+    // A
+    // B
+    // C
+    'Create One.' => 'Создать один.',
+    // D
+    "Don't have an account? :url" => 'Нет аккаунта? :url',
+    // E
+    // F
+    'Fast, safe & secure way!' => 'Быстро и безопасно!',
+    'Forgot Password?' => 'Забыли пароль?',
+    // G
+    // H
+    // I
+    // J
+    // K
+    // L
+    'Login' => 'Логин',
+    'Login with :provider' => 'Войсти с помощью :provider',
+    'Login with any of these providers:' => 'Войти с помощью одного их этих провайдеров:',
+    // M
+    'My Account' => 'Мой аккаунт',
+    // N
+    // O
+    // P
+    'Password' => 'Пароль',
+    'Password or username is incorrect.' => 'Пароль или имя пользователя не верны.',
+    'Profile %title' => 'Профиль %title',
+    // Q
+    // R
+    // S
+    'Stay Signed in' => 'Оставаться в системе',
+    // T
+    'The username must be at least :param2 characters long.' => 'Имя пользователя должно быть не менее :param2 символов.',
+    'The username must be less than :param2 characters long.' => 'Имя пользователя должно быть не менее :param2 символов.',
+    'This account is blocked' => 'Этот аккаунт заблокирован',
+    // U
+    'Username/Email' => 'Имя пользователя/Email',
+    // V
+    // W
+    // X
+    // Y
+    'You must provide an email address.' => 'Вы долны предоставить email адрес.',
+    'You must provide a password.' => 'Вы долны предоставить пароль.',
+    'You must provide a username.' => 'Вы долны предоставить имя пользователя.',
+    // Z
+  );
