d4a531ca6b9e4f1ca826432f437a6ba35c4aaf81
Sergey Yakovlev <sadhooklay@gmail.com>
2013-02-23 07:22:35 +0400

    Amended modules/gleez/views/page/none.php

--- modules/gleez/views/page/none.php
+++ modules/gleez/views/page/none.php
@@ -1,6 +1,10 @@
-<h2>No Pages!</h2>
+<?php defined('SYSPATH') OR die('No direct script access.') ?>
+
+<h3><?php echo __('No Pages!'); ?></h3>
+
 <p>
-	<?php echo __('There are no Pages that have been published.') ?>
+	<?php echo __('There are no Pages that have been published.'); ?>
 </p>
 
-
+<?php echo HTML::anchor(Route::get('page')->uri(array('action' => 'add')), '<i class="icon-plus icon-white"></i> ' . __('Add Page'), array('class' => 'btn btn-primary pull-right')) ?>
+<div class='clearfix'></div><br/>
