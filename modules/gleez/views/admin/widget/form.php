18ec422f927d1b8905a9e173a8ccca91fc7706ef
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-01 14:56:02 +0400

    Minor fixes in Widget Form view.

--- modules/gleez/views/admin/widget/form.php
+++ modules/gleez/views/admin/widget/form.php
@@ -22,12 +22,12 @@
 		<div class="span6">
 
 			<div class="control-group <?php echo isset($errors['title']) ? 'error': ''; ?>">
-				<?php echo Form::label('title', __('Title:'), array('class' => 'control-label')) ?>
+				<?php echo Form::label('title', __('Title'), array('class' => 'control-label')) ?>
 				<?php echo Form::input('title', $widget->title, array('class' => 'input-large')); ?>
 			</div>
 
 			<div class="control-group <?php echo isset($errors['region']) ? 'error': ''; ?>">
-				<?php echo Form::label('region', __('Region:'), array('class' => 'control-label')) ?>
+				<?php echo Form::label('region', __('Region'), array('class' => 'control-label')) ?>
 				<?php echo Form::select('region', $regions, $widget->region, array('class' => 'input-large')); ?>
 			</div>
 
@@ -35,17 +35,17 @@
 				<?php $enabled_off = (isset($widget->status) AND $widget->status == 0) ? TRUE : FALSE; ?>
 				<?php $enabled_on  = (isset($widget->status) AND $widget->status == 1) ? TRUE : FALSE; ?>
 
-				<?php echo Form::label('status', __('Enabled:'), array('class' => 'control-label')) ?>
+				<?php echo Form::label('status', __('Status'), array('class' => 'control-label')) ?>
 				<div class="controls">
-					<?php echo Form::label('enabled', Form::radio('status', 0, $enabled_off).__('Disabled'), array('class' => 'radio inline')) ?>
+					<?php echo Form::label('status', Form::radio('status', 0, $enabled_off).__('Disabled'), array('class' => 'radio inline')) ?>
 					<?php echo Form::label('status', Form::radio('status', 1, $enabled_on).__('Enabled'),  array('class' => 'radio inline')) ?>
 				</div>
 			</div>
 
 			<div class="control-group <?php echo isset($errors['roles']) ? 'error': ''; ?>">
-				<?php echo Form::label('roles', __('Roles:'), array('class' => 'control-label')) ?>
+				<?php echo Form::label('roles', __('Roles'), array('class' => 'control-label')) ?>
 				<?php foreach($roles as $role => $name): ?>
-					<?php echo Form::label('roles', Form::checkbox('roles['.$role.']', $role, in_array( $role, explode(',', $widget->roles)) ? TRUE : FALSE).ucfirst($name), array('class' => 'checkbox')) ?>
+					<?php echo Form::label('roles', Form::checkbox('roles['.$role.']', $role, in_array($role, explode(',', $widget->roles)) ? TRUE : FALSE).ucfirst($name), array('class' => 'checkbox')) ?>
 				<?php endforeach ?>
 			</div>
 
@@ -57,18 +57,18 @@
 				<?php $show_title_off = (isset($widget->show_title) AND $widget->show_title == 0) ? TRUE : FALSE; ?>
 				<?php $show_title_on = (isset($widget->show_title) AND $widget->show_title == 1) ? TRUE : FALSE; ?>
 
-				<?php echo Form::label('show_title', __('Show Title:'), array('class' => 'control-label')) ?>
+				<?php echo Form::label('show_title', __('Show Title'), array('class' => 'control-label')) ?>
 				<div class="controls">
 					<?php echo Form::label('show_title', Form::radio('show_title', 0, $show_title_off).__('Hide'), array('class' => 'radio inline')) ?>
 					<?php echo Form::label('show_title', Form::radio('show_title', 1, $show_title_on).__('Show'), array('class' => 'radio inline')) ?>
 				</div>
 			</div>
 
-			<div class="control-group <?php echo isset($errors['status']) ? 'error': ''; ?>">
+			<div class="control-group <?php echo isset($errors['visibility']) ? 'error': ''; ?>">
 				<?php $visible_off = (isset($widget->status) AND $widget->visibility == 0) ? TRUE : FALSE; ?>
 				<?php $visible_on  = (isset($widget->status) AND $widget->visibility == 1) ? TRUE : FALSE; ?>
 
-				<?php echo Form::label('pages', __('Show widget on specific pages: '), array('class' => 'control-label')) ?>
+				<?php echo Form::label('pages', __('Show widget on specific pages'), array('class' => 'control-label')) ?>
 				<?php echo Form::label('visibility', Form::radio('visibility', 0, $visible_off).__('All pages except those listed'), array('class' => 'radio')) ?>
 				<?php echo Form::label('visibility', Form::radio('visibility', 1, $visible_on).__('Only the listed pages'),  array('class' => 'radio')) ?>
 			</div>
@@ -79,16 +79,16 @@
 
 			<?php echo $fields; /* custom fields set by widget */ ?>
 
-			<?php if ( $static): ?>
+			<?php if ($static): ?>
 				<div class="control-group <?php echo isset($errors['body']) ? 'error': ''; ?>">
-					<?php echo Form::label('content', __('Content:'), array('class' => 'control-label')) ?>
+					<?php echo Form::label('body', __('Content'), array('class' => 'control-label')) ?>
 					<?php echo Form::textarea('body', $widget->body, array('class' => 'textarea medium nowrap', 'rows' => 5)) ?>
 				</div>
 
 				<?php $formats = Inputfilter::formats(); ?>
 				<div class="control-group <?php echo isset($errors['format']) ? 'error': ''; ?>">
 					<div class="controls">
-						<?php echo Form::label('format', __('Text format:'), array('class' => 'control-label')) ?>
+						<?php echo Form::label('format', __('Text format'), array('class' => 'control-label')) ?>
 						<?php echo Form::select('format', $formats, $widget->format, array('class' => 'input-large')); ?>
 					</div>
 				</div>
@@ -100,5 +100,5 @@
 	<div class="clearfix"></div>
 
 	<?php echo Form::hidden('widget', $widget->name ) ?>
-	<?php echo Form::submit('widget', __('Submit'), array('class' => 'btn btn-primary btn-large')) ?>
+	<?php echo Form::submit('widget', __('Save'), array('class' => 'btn btn-primary btn-large')) ?>
 <?php echo Form::close() ?>
