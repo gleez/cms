c9a8a7c4a74a672897a64771d705aed1ac6fd98f
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-01 20:12:55 +0400

    Cleanup Admin Dashboard Widgets. Minor fixes in strings that need to be localized. Added missed strings to __() method

--- modules/gleez/views/widgets/systeminfo.php
+++ modules/gleez/views/widgets/systeminfo.php
@@ -1,30 +1,30 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
 <table class="hor-minimalist-a" id="admin-info">
-  <tbody>
-	  <tr>
-      <th><?php echo __('Gleez Version ') ?></th>
-      <td><?php echo HTML::chars(Gleez::VERSION) ?></td>
-    </tr>
-    <tr>
-      <th><?php echo __('Host Name ') ?></th>
-      <td><?php echo HTML::chars(php_uname("n")) ?></td>
-    </tr>
-    <tr>
-      <th><?php echo __('Webserver ') ?></th>
-      <td><?php echo $_SERVER['SERVER_SOFTWARE']; ?></td>
-    </tr>
-    <tr>
-      <th><?php echo __('PHP Version ') ?></th>
-      <td><?php echo HTML::chars(PHP_VERSION) ?></td>
-    </tr>
-    <tr>
-      <th><?php echo __("Database Version") ?></th>
-      <td><?php echo function_exists("mysql_get_server_info") ? mysql_get_server_info() : __('Unavailable'); ?></td>
-    </tr>
-    <tr>
-      <th><?php echo __('Load Average ') ?></th>
-      <td><?php echo System::get_avg() ?></td>
-    </tr>
-  </tbody>
+	<tbody>
+		<tr>
+			<th><?php echo __('Gleez Version') ?></th>
+			<td><?php echo HTML::chars(Gleez::VERSION) ?></td>
+		</tr>
+		<tr>
+			<th><?php echo __('Host Name') ?></th>
+			<td><?php echo HTML::chars(php_uname("n")) ?></td>
+		</tr>
+		<tr>
+			<th><?php echo __('Webserver') ?></th>
+			<td><?php echo $_SERVER['SERVER_SOFTWARE']; ?></td>
+		</tr>
+		<tr>
+			<th><?php echo __('PHP Version') ?></th>
+			<td><?php echo HTML::chars(PHP_VERSION) ?></td>
+		</tr>
+		<tr>
+			<th><?php echo __('MySQL Version') ?></th>
+			<td><?php echo function_exists("mysql_get_server_info") ? mysql_get_server_info() : __('Unavailable'); ?></td>
+		</tr>
+		<tr>
+			<th><?php echo __('Load Average') ?></th>
+			<td><?php echo System::get_avg() ?></td>
+		</tr>
+	</tbody>
 </table>
