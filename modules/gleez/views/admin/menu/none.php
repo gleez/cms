9ddeac116fd91a1b9e9fde67a6656f785fdee2c9
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-02 20:05:18 +0400

    More fixes in none.php views. Part #3

--- modules/gleez/views/admin/menu/none.php
+++ modules/gleez/views/admin/menu/none.php
@@ -1,6 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct script access.') ?>
 
 <h3><?php echo __('No Menus!'); ?></h3>
+<?php echo HTML::anchor(Route::get('admin/menu')->uri(array('action' => 'add')), '<i class="icon-plus icon-white"></i>' . __('Add Menu'), array('class' => 'btn btn-primary pull-right')) ?>
+
 <p>
 	<?php echo __('There are no Menus.') ?>
 </p>
