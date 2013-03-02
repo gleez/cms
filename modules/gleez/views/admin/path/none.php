00739614b59e43b5da6c60749da64593297d39da
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-02 18:59:24 +0400

    More fixes in none.php views.

--- modules/gleez/views/admin/path/none.php
+++ modules/gleez/views/admin/path/none.php
@@ -1,7 +1,7 @@
-<?php defined('SYSPATH') or die('No direct script access.') ?>
+<?php defined('SYSPATH') OR die('No direct script access.') ?>
 
 <h3><?php echo __('No Paths!'); ?></h3>
-<?php echo Html::anchor(Route::get('admin/path')->uri(array('action' =>'add')), 'Add alias') ?>
+<?php echo HTML::anchor(Route::get('admin/path')->uri(array('action' =>'add')), 'Add alias') ?>
 
 <p>
 	<?php echo __('There are no Paths.') ?>
--- modules/gleez/views/admin/taxonomy/none.php
+++ modules/gleez/views/admin/taxonomy/none.php
@@ -1,8 +1,8 @@
-<?php defined('SYSPATH') or die('No direct script access.') ?>
+<?php defined('SYSPATH') OR die('No direct script access.') ?>
 
 <h3><?php echo __('No Vocabulary!'); ?></h3>
 
-<?php echo html::anchor(Route::get('admin/taxonomy')->uri(array('action' =>'add')), 'Add new Vocabulary') ?>
+<?php echo HTML::anchor(Route::get('admin/taxonomy')->uri(array('action' =>'add')), 'Add new Vocabulary') ?>
 
 <p>
 	<?php echo __('There are no Terms.') ?>
