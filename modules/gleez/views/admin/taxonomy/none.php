1b676ac274e451c6ee1fa038b6a29566be238e61
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-02 20:12:29 +0400

    More fixes in none.php views. Part #5

--- modules/gleez/views/admin/taxonomy/none.php
+++ modules/gleez/views/admin/taxonomy/none.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct script access.') ?>
 
 <h3><?php echo __('No Vocabulary!'); ?></h3>
-<?php echo HTML::anchor(Route::get('admin/taxonomy')->uri(array('action' =>'add')), __('Add new Vocabulary')), array('class' => 'btn btn-primary pull-right')) ?>
+<?php echo HTML::anchor(Route::get('admin/taxonomy')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i> ' . __('Add new Vocabulary'), array('class' => 'btn btn-primary pull-right')); ?>
 
 <p>
 	<?php echo __('There are no Terms.') ?>
