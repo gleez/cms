e63f3ecd0b3b9d5192da7fd5e86158c734a1e03d
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-10 22:50:04 +0400

    Amended Page config file. Cleanup code. Added PHPDoc

--- modules/gleez/config/page.php
+++ modules/gleez/config/page.php
@@ -1,51 +1,91 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 return array(
-                //Default Page Status. draft,review,publish etc
-                'default_status'        => 'draft',
-                
-                //Pages per page; ex: 5, 10, 15 etc
-                'items_per_page'        => 15,
-                
-                //Enable captcha; true/false
-                'use_captcha'           => 0,
-                
-                // Enable/disbale to set page author; true/false
-                'use_authors'           => 1,
-                
-                //Enable teaser; true/false
-                'use_excerpt'           => 0,
-                
-                //Enable comment; true/false
-                'use_comment'           => 1,
-                
-                //Enable tags; true/false
-                'use_tags'              => 1,
-                
-                //Enable book support; true/false
-                'use_book'              => 0,
-                
-                //View submitted info in views; true/false
-                'use_submitted'         => 1,
-                
-                //Enable terms; array/false array of term id's or false to disable
-                'use_category'          => 1,
-                
-                //Enable login buttons above comment form; true or false to disable
-                'use_provider_buttons'  => 1,
-                
-                //Allow people to post Comment(s); 0 => disabled, 1 => read, 2 => read/write
-                'comment'               => 0,
-                
-                //Comment display mode 
-                'comment_default_mode'  => 0,
-                
-                //Allow anonymous commenting (with contact information); true/false
-                'comment_anonymous'     => 0,
-                
-                //Comments per page
-                'comments_per_page'     => 20,
-                
-                //Comments displayed with the older/new comments; asc/desc
-                'comment_order'         => 'asc',
-        );
+	/**
+	 * Default Page Status (eg: draft, review, publish, etc)
+	 * @var string
+	 */
+	'default_status' => 'draft',
+
+	/**
+	 * Pages per page (eg: 5, 10, 15, etc)
+	 * @var integer
+	 */
+	'items_per_page' => 15,
+
+	/**
+	 * Enable captcha
+	 * @var boolean
+	 */
+	'use_captcha' => FALSE,
+
+	/**
+	 * Enable to set page author
+	 * @var boolean
+	 */
+	'use_authors' => TRUE,
+
+	/**
+	 * Enable teaser
+	 * @var boolean
+	 */
+	'use_excerpt' => FALSE,
+
+	/**
+	 * Enable comments
+	 * @var boolean
+	 */
+	'use_comment' => TRUE,
+
+	/**
+	 * View submitted info in views
+	 * @var boolean
+	 */
+	'use_submitted' => TRUE,
+
+	/**
+	 * Enable taxonomy. Array of term id's for sets or FALSE to disable
+	 * @var mixed
+	 */
+	'use_category' => FALSE,
+
+
+	/** @var boolean Enable tags */
+	'use_tags' => TRUE,
+
+	/**
+	 * Enable login buttons above comment form
+	 * @var boolean
+	 */
+	'use_provider_buttons' => TRUE,
+
+	/**
+	 * Allow people to post Comment(s): 0 - disabled, 1 - read, 2 - read/write
+	 * @var integer
+	 */
+	'comment' => 0,
+
+	/**
+	 * Comment display mode
+	 * @var integer
+	 */
+	'comment_default_mode' => 0,
+
+	/**
+	 * Allow anonymous commenting (with contact information)
+	 * @var boolean
+	 */
+	'comment_anonymous' => FALSE,
+
+	/**
+	 * Comments per page
+	 * @var integer
+	 */
+	'comments_per_page' => 20,
+
+	/**
+	 * Comments displayed with the older/new comments ('asc' OR 'desc')
+	 * @var string
+	 */
+	'comment_order' => 'asc',
+);
