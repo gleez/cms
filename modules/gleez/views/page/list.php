5ca646495430ed9dbc204afcf671a2c4af5963a6
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-12 20:58:06 +0400

    Cleanup some views.

--- modules/gleez/views/page/list.php
+++ modules/gleez/views/page/list.php
@@ -1,18 +1,19 @@
-<?php defined("SYSPATH") OR die("No direct script access.") ?>
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
 <?php foreach($posts as $i => $post): ?>
-
-	<div id="post-<?php echo $i; ?>" class="post-list<?php if ($post->sticky) { echo ' sticky'; } ?> <?php echo ' post-'. $post->status; ?>">
-		<h2 class="post-title"><?php echo HTML::anchor($post->url, $post->title) ?></h2>
-			<?php
-				// @todo It need be in Controller, but don't in View. Views must contains HTML only, but no php-code
-				echo View::factory($post->type.'/teaser')
-							->set('post', $post)
-							->set('config', $config)
-							->set('page_title', TRUE);
-			?>
+	<!-- Post #<?php echo $i; ?> Teaser start -->
+	<div id="post-<?php echo $i; ?>" class="<?php echo 'post-list' . ($post->sticky) ? ' sticky' : ' post-'. $post->status; ?>">
+		<h2 class="post-title">
+			<?php echo HTML::anchor($post->url, $post->title) ?>
+		</h2>
+		<?php
+			echo View::factory($post->type.'/teaser')
+					->set('post', $post)
+					->set('config', $config)
+					->set('page_title', TRUE);
+		?>
 	</div>
-
+	<!-- Post #<?php echo $i; ?> Teaser end -->
 <?php endforeach; ?>
 
 <?php echo $pagination ?>
