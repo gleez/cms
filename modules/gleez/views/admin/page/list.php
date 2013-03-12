be72e252cd335c0fa8b99ef8d269723f19c09f12
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-12 20:58:06 +0400

    Cleanup some views.

--- modules/gleez/views/admin/page/list.php
+++ modules/gleez/views/admin/page/list.php
@@ -1,16 +1,13 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
-	<div class="help">
-		<?php echo __('View, edit, and delete your site\'s pages.'); ?>
-	</div>
+<div class="help">
+	<?php echo __('View, edit, and delete your site\'s pages.'); ?>
+</div>
 
-	<?php include Kohana::find_file('views', 'errors/partial'); ?>
+<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-	<div class="content">
-		<?php echo HTML::anchor(Route::get('page')->uri(array('action' => 'add')), '<i class="icon-plus icon-white"></i> ' . __('Add Page'), array('class' => 'btn btn-primary pull-right')); ?>
-		<?php echo Form::open(Route::get('admin/page')->uri(array('action' => 'list')).URL::query($destination),
-							array('id'=>'admin-page-form', 'class'=>'no-form')); ?>
-
+<div class="content">
+	<?php echo Form::open(Route::get('admin/page')->uri($params).URL::query($destination), array('id'=>'admin-page-form', 'class'=>'no-form')); ?>
 		<fieldset>
 			<legend><?php echo __('Bulk Actions'); ?></legend>
 			<div class="control-group edit-operation <?php echo isset($errors['operation']) ? 'error': ''; ?>">
@@ -18,32 +15,24 @@
 				<?php echo Form::submit('page-bulk-actions', __('Apply'), array('class'=>'btn btn-danger btn-small')); ?>
 			</div>
 		</fieldset>
-
 		<table class="table table-striped table-bordered" id="posts-admin-list">
-
 			<thead>
 				<tr>
 					<th> # </th>
-					<th> <?php echo __('Title'); ?>&nbsp;<?php echo URL::sortAnchor('title'); ?> </th>
-					<th> <?php echo __('Author'); ?>&nbsp;<?php echo URL::sortAnchor('author'); ?>  </th>
-					<th> <?php echo __('Status'); ?>&nbsp;<?php echo URL::sortAnchor('status'); ?>  </th>
-					<th> <?php echo __('Updated'); ?>&nbsp;<?php echo URL::sortAnchor('created'); ?>  </th>
-					<th> <?php echo __('Actions'); ?></th>
+					<th><?php echo __('Title'); ?>&nbsp;<?php echo URL::sortAnchor('title'); ?> </th>
+					<th><?php echo __('Author'); ?>&nbsp;<?php echo URL::sortAnchor('author'); ?></th>
+					<th><?php echo __('Status'); ?>&nbsp;<?php echo URL::sortAnchor('status'); ?></th>
+					<th><?php echo __('Updated'); ?>&nbsp;<?php echo URL::sortAnchor('created'); ?></th>
+					<th><?php echo __('Actions'); ?></th>
 				</tr>
 			</thead>
 			<tbody>
-				<?php foreach ($posts as $post):  ?>
+				<?php foreach ($posts as $post): ?>
 					<tr id="post-row-<?php echo $post->id ?>" class="<?php echo Text::alternate("odd", "even"); ?>">
-						<td><?php echo Form::checkbox('posts['.$post->id.']', $post->id, isset($_POST['posts'][$post->id]) ); ?> </td>
-						<td>
-							<?php echo HTML::anchor($post->url, $post->title, array('class'=>'action-view')) ?>
-						</td>
-						<td>
-							<?php echo HTML::anchor(Route::get('user')->uri(array('action' => 'profile', 'id' => $post->author)), $post->user->nick, array()) ?>
-						</td>
-						<td class="status">
-							<span class="label label-<?php echo $post->status; ?>"><?php echo $post->status; ?></span>
-						</td>
+						<td><?php echo Form::checkbox('posts['.$post->id.']', $post->id, isset($_POST['posts'][$post->id])); ?></td>
+						<td><?php echo HTML::anchor($post->url, $post->title, array('class'=>'action-view')); ?></td>
+						<td><?php echo HTML::anchor(Route::get('user')->uri(array('action' => 'profile', 'id' => $post->author)), $post->user->nick, array()); ?></td>
+						<td class="status"><span class="label label-<?php echo $post->status; ?>"><?php echo $post->status; ?></span></td>
 						<td><?php echo Date::date($post->updated); ?></td>
 						<td class="action">
 							<?php echo HTML::anchor($post->edit_url.URL::query($destination), __('Edit'), array('class'=>'action-edit', 'title'=> __('Edit'))) ?>
@@ -53,8 +42,7 @@
 				<?php endforeach ?>
 			</tbody>
 		</table>
+	<?php echo Form::close(); ?>
+</div>
 
-		<?php echo Form::close(); ?>
-	</div>
-
-	<?php echo $pagination ?>
+<?php echo $pagination ?>
