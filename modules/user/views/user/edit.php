194af256597de68069f686848d4cbc8aceeef8d1
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-17 02:01:49 +0400

    Fixed error when saving user profile

--- modules/user/views/user/edit.php
+++ modules/user/views/user/edit.php
@@ -1,42 +1,47 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
-	$param = array (
-		'id' => $user->id,
-		'action' => 'edit'
-	);
+<?php echo Form::open(Route::get('user')->uri($params), array('class' => 'form-horizontal', 'enctype' => 'multipart/form-data')); ?>
+    <?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-	echo Form::open(Route::get('user')->uri(array($param)), array('class' => 'form-horizontal', 'enctype' => 'multipart/form-data'));
-
-	include Kohana::find_file('views', 'errors/partial');
-?>
+    <div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
+        <?php echo Form::label('name', __('Username'), array('class' => 'control-label')) ?>
+        <div class="controls">
+            <?php echo Form::input('name', $user->name, array('class' => 'input-large disabled', 'disabled')); ?>
+            <p class="help-block"><?php echo __('Username for logging in can\'t be changed.') ?></p>
+        </div>
+    </div>
 
 	<div class="control-group <?php echo isset($errors['nick']) ? 'error': ''; ?>">
-		<?php echo Form::label('nick', __('Display Name:'), array('class' => 'control-label')) ?>
-		<?php echo Form::input('nick', $user->nick, array('class' => 'input-large')); ?>
+		<?php echo Form::label('nick', __('Display Name'), array('class' => 'control-label')) ?>
+        <div class="controls">
+		    <?php echo Form::input('nick', $user->nick, array('class' => 'input-large')); ?>
+        </div>
 	</div>
 
 	<div class="control-group <?php echo isset($errors['mail']) ? 'error': ''; ?>">
-		<?php echo Form::label('mail', __('Mail:'), array('class' => 'control-label')) ?>
-		<?php echo Form::input('mail', $user->mail, array('class' => 'input-large')); ?>
+		<?php echo Form::label('mail', __('Mail'), array('class' => 'control-label')) ?>
+        <div class="controls">
+		    <?php echo Form::input('mail', $user->mail, array('class' => 'input-large')); ?>
+        </div>
 	</div>
 
 	<div class="control-group <?php echo isset($errors['gender']) ? 'error': ''; ?>">
-		<?php $gender1 = (isset($user->gender) AND $user->gender == 1) ? TRUE : FALSE; ?>
-		<?php $gender2 = (isset($user->gender) AND $user->gender == 2) ? TRUE : FALSE; ?>
-
-		<?php echo Form::label('gender', __('Gender: '), array('class' => 'control-label')) ?>
+		<?php echo Form::label('gender', __('Gender'), array('class' => 'control-label')) ?>
 		<div class="controls">
-			<?php echo Form::label('gender1', Form::radio('gender', 1, $gender1).__('Male'), array('class' => 'radio inline')) ?>
-			<?php echo Form::label('gender2', Form::radio('gender', 2, $gender2).__('Female'), array('class' => 'radio inline')) ?>
-		</div>
+			<?php echo Form::label('gender1', Form::radio('gender', 1, $male).__('Male'), array('class' => 'radio')) ?>
+		    <?php echo Form::label('gender2', Form::radio('gender', 2, $female).__('Female'), array('class' => 'radio')) ?>
+        </div>
 	</div>
 
 	<div class="control-group <?php echo isset($errors['dob']) ? 'error': ''; ?>">
-		<?php echo Form::label('dob', __('Birthday:'), array('class' => 'control-label')) ?>
-		<?php echo Form::select('month', Date::months(Date::MONTHS_SHORT), date('n', $user->dob), array('class' => 'input-small')); ?>
-		<?php echo Form::select('days',  Date::days(Date::DAY), date('j', $user->dob), array('class' => 'input-small')); ?>
-		<?php echo Form::select('years', Date::years(date('Y') - 95,date('Y') - 5), date('Y', $user->dob), array('class' => 'input-small')); ?>
+		<?php echo Form::label('dob', __('Birthday'), array('class' => 'control-label')) ?>
+        <div class="controls">
+            <?php echo Form::select('month', Date::months(Date::MONTHS_SHORT), date('n', $user->dob), array('class' => 'span1')); ?>
+            <?php echo Form::select('days',  Date::days(Date::DAY), date('j', $user->dob), array('class' => 'span1')); ?>
+            <?php echo Form::select('years', Date::years(date('Y') - 95,date('Y') - 5), date('Y', $user->dob), array('class' => 'input-small')); ?>
+        </div>
 	</div>
 
-	<?php echo Form::submit('user_edit', __('Update Profile'), array('class' => 'btn btn-primary')) ?>
+	<?php echo Form::submit('user_edit', __('Update Profile'), array('class' => 'btn btn-primary pull-right')) ?>
+    <div class="clearfix"></div><br>
 <?php echo Form::close() ?>
