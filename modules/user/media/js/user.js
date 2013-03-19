c916bb76d279dea4ba3e5e7410a6d4dea82267b2
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-19 13:43:24 +0400

    Cleanup user.js, added JSDoc

--- modules/user/media/js/user.js
+++ modules/user/media/js/user.js
@@ -1,47 +1,58 @@
+/* =================================================
+ * user.js v1.2
+ * http://github.com/gleez/cms
+ *
+ * Copyright 2011-2013 Gleez Technologies
+ * Gleez CMS License http://gleezcms.org/license
+ * ============================================== */
 (function ($) {
 
-        /**
-        * Wait until the DOM has loaded before querying the document
-        */
-        $(document).ready(function(){
-                profile_photo();
-        });
+	/**
+	 * Wait until the DOM has loaded before querying the document
+	 */
+	$(document).ready(function(){
+			scan_modals();
+	});
 
-        function profile_photo()
-        {
-                $('a#add-pic').click(function(e){
+	/**
+	 * Scan modal windows
+	 */
+	function scan_modals()
+	{
+		$('a#add-pic').click(function(e){
+			$.get('/user/photo', function(data){
+				$('#upload-photo').modal({show: true});
+				$('#upload-photo').find('.modal-data').html(data);
+				picture_upload();
+			});
 
-                        $.get('/user/photo', function(data){
-                                $('#upload-photo').modal({show: true});
-                                $('#upload-photo').find('.modal-data').html(data);
-                                profile_upload();
-                        });
+			e.preventDefault();
+		});
+	}
 
-                        e.preventDefault();
-                });
-        }
+	/**
+	 * Wait until the DOM has loaded before querying the document
+	 */
+	function picture_upload()
+	{
+		var bar = $('.bar');
+		var percent = $('.percent');
+		var status = $('#status');
 
-        function profile_upload()
-        {
-                var bar = $('.bar');
-                var percent = $('.percent');
-                var status = $('#status');
-
-                $('form').ajaxForm({
-                        beforeSend: function() {
-                                status.empty();
-                                $('.progress').show();
-                                var percentVal = '0%';
-                                bar.width(percentVal);
-                        },
-                        uploadProgress: function(event, position, total, percentComplete) {
-                                var percentVal = percentComplete + '%';
-                                bar.width(percentVal);
-                        },
-                        complete: function(xhr) {
-                                location.reload();
-                        }
-                });
-        }
-
+		$('form').ajaxForm({
+			beforeSend: function() {
+				status.empty();
+				$('.progress').show();
+				var percentVal = '0%';
+				bar.width(percentVal);
+			},
+			uploadProgress: function(event, position, total, percentComplete) {
+				var percentVal = percentComplete + '%';
+				bar.width(percentVal);
+			},
+			complete: function(xhr) {
+				location.reload();
+			}
+		});
+	}
 })(jQuery);
