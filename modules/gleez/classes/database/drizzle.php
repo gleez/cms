838e40e4ed2f30e0b4515884b5cf6b6af0cfec7a
Gleez Technologies <github@gleez.tech>
2013-03-27 11:44:56 +0530

    Remove drizzle support, no longer used

--- modules/gleez/classes/database/drizzle.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Database_Drizzle extends Gleez_Database_Drizzle {}
--- modules/gleez/classes/database/drizzle/result.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Database_Drizzle_Result extends Gleez_Database_Drizzle_Result {}
