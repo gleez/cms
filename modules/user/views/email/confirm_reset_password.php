da39b71157522fed0d3a306881827ed0c77bbc6a
Sergey Yakovlev <sadhooklay@gmail.com>
2013-03-31 02:04:42 +0400

    Added i18n support to confirm reset password mail

--- modules/user/views/email/confirm_reset_password.php
+++ modules/user/views/email/confirm_reset_password.php
@@ -1,13 +1,13 @@
-Dear <?php echo $name ?> ,
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
-A request to reset the password for your account has been made at !site.
-
-You may now log in to !uri_brief clicking on this link or copying and pasting it in your browser:
-<?php echo $url ?> 
-
-This is a one-time login, so it can be used only once. It expires after one day and nothing will happen if it's not used.
-
-After logging in, you will be redirected, so you can change your password. We do not encourage you to lose your password again. Be aware that this behaviour can be dangerous.
-
-Best Regards
-www.gleez.com
+<?php
+	echo
+		__('Hello :name!', array(':name' => $name)) . PHP_EOL . PHP_EOL .
+		__('A request to reset the password for your account has been made at !site.', array('!site' => $config->site_url)) . PHP_EOL . PHP_EOL .
+		__('You may now log in to !uri_brief clicking on this link or copying and pasting it in your browser.', array('!uri_brief' => $url)) . PHP_EOL . PHP_EOL .
+		__("This is a one-time URL, so it can be used only once. It expires after one day and nothing will happen if it's not used.") . PHP_EOL .
+		__('After logging in, you will be redirected, so you can change your password. We do not encourage you to lose your password again. Be aware that this behaviour can be dangerous.') . PHP_EOL  . PHP_EOL . PHP_EOL .
+		__('Best Regards') . PHP_EOL .
+		$config->site_name  . PHP_EOL .
+		$config->site_slogan;
+?>
