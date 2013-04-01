3b5f08029aa8c9e5fd3deba968e449d633f790bf
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-02 03:54:45 +0400

    Cleanup code

--- modules/gleez/classes/gleez/path.php
+++ modules/gleez/classes/gleez/path.php
@@ -2,135 +2,158 @@
 /**
  * An adaptation of handle path aliasing.
  *
- * @package	Gleez
- * @category	Path
- * @author	Sandeep Sangamreddi - Gleez
- * @copyright	(c) 2012 Gleez Technologies
- * @license	http://gleezcms.org/license
+ * @package    Gleez\Path
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2011-2013 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
  */
 class Gleez_Path {
 
-        /**
-	 * Get a path via its alias.
+	/** Default alias for frontpage */
+	const FRONT_ALIAS = '<front>';
+
+	/**
+	 * Get a path via its alias
 	 *
-	 * @param   string   $alias  'about'
-	 * @return  mixed    array/false
+	 * @param   string  $alias  Alias, eg. 'about'
+	 * @return  mixed  array|FALSE
 	 */
-	public static function lookup( $alias )
-        {
-		$regex 	= "#(/p(?P<page>\d+))+$#uD"; 	//preg_match()
-		$reg_ex = "#(/p\d+)+$#uD";		//preg_replace()
-		$page 	= NULL; 			//default pager id is null
-	
+	public static function lookup($alias)
+	{
+		$regex 	= "#(/p(?P<page>\d+))+$#uD";  // preg_match()
+		$reg_ex = "#(/p\d+)+$#uD";            // preg_replace()
+		$page 	= NULL;                       // default pager id is null
+
 		// Save this value for pagination
 		// @todo use preg_replace_callback to handle both set and replace
-		if( @preg_match($regex, $alias, $matches) )
+		if (@preg_match($regex, $alias, $matches))
 		{
-			if( isset($matches['page']) ) $page = $matches['page'];
-			//Message::debug( Debug::vars($matches) );
+			if (isset($matches['page']))
+			{
+				$page = $matches['page'];
+			}
 			unset($matches);
 		}
-	
-		//Remove pagination ex /p1 /p2 etc
+
+		// Remove pagination ex /p1 /p2 etc
 		$alias = @preg_replace($reg_ex, '', rtrim($alias, '/'));
 
-		//check if it's a front page request and set <front> tag
-		if( empty($alias) AND $alias == NULL ) $alias = '<front>';
-	
-                $result = Path::load( array('alias' => $alias) );
-		if( !$result ) return false;
-	
-		//reset the <front> tag to '' orelse request fails
-		if( $alias === '<front>' AND $result ) $result['alias'] = '';
-	
-		//$uri = $result['alias'];
-		//if( !is_null($page)) $uri = $result['alias'] .'/p'. $page;
-	
+		// Check if it's a front page request and set <front> tag
+		if (empty($alias) AND $alias == NULL)
+		{
+			$alias = self::FRONT_ALIAS;
+		}
+
+		$result = Path::load(array('alias' => $alias));
+		if ( ! $result)
+		{
+			return FALSE;
+		}
+
+		// reset the self::FRONT_ALIAS tag to '' orelse request fails
+		if ($alias === self::FRONT_ALIAS AND $result)
+		{
+			$result['alias'] = '';
+		}
+
 		return array(
-				'directory'  => $result['route_directory'],
-				'controller' => $result['route_controller'],
-				'action'     => $result['route_action'],
-				'id' 	     => $result['route_id'],
-				'uri'	     => $result['alias'],
-				'page'	     => $page
+			'directory'  => $result['route_directory'],
+			'controller' => $result['route_controller'],
+			'action'     => $result['route_action'],
+			'id' 	     => $result['route_id'],
+			'uri'	     => $result['alias'],
+			'page'	     => $page
 		);
 	}
-	
-        /**
-	 * Creates path alias.
+
+	/**
+	 * Creates path alias
 	 *
-	 * @param   array   $values  array('source' => 'page/1', 'alias' => 'about')
+	 * Example:<br>
+	 * <code>
+	 *   Path::save(array('source' => 'page/1', 'alias' => 'about'))
+	 * </code>
+	 *
+	 * @param   array  $values  Array of aliases
 	 * @return  string
 	 */
-	public static function save( array $values)
+	public static function save(array $values)
 	{
-                try
+		try
 		{
-			if( isset($values['id']) AND is_numeric($values['id']) )
+			if (isset($values['id']) AND is_numeric($values['id']))
 			{
-				$path = ORM::factory('path', $values['id'] )->values($values)->save();
+				$path = ORM::factory('path', $values['id'])
+							->values($values)
+							->save();
 			}
 			else
 			{
-				$path = ORM::factory('path')->values($values)->save();
+				$path = ORM::factory('path')
+							->values($values)
+							->save();
 			}
 		}
-		catch( Exception $e)
+		catch (Exception $e)
 		{
-			//log error and return, to avoid breaking process
-			Kohana::$log->add(LOG::ERROR, 'Error: :error creating path alias!', array(':error' => $e->getMessage()) );
-			return false;
+			// log error and return, to avoid breaking process
+			Kohana::$log->add(LOG::ERROR, 'Error: :error creating path alias!',
+				array(':error' => $e->getMessage())
+			);
+			return FALSE;
 		}
-                
-                return $path;
+
+		return $path;
 	}
 
-        /**
-	 * Deletes path alias.
+	/**
+	 * Deletes path alias
 	 *
-	 * @param   $criteria  A number representing the pid or an array of criteria.
-	 * @return  boolen     TRUE/FASLE
+	 * @param   $criteria  A number representing the pid or an array of criteria
+	 * @return  boolen     TRUE|FASLE
 	 */
-	public static function delete( $criteria )
+	public static function delete($criteria)
 	{
-                try
+		try
 		{
 			$query = DB::delete('paths');
-			
-			if ( !is_array($criteria) )
+
+			if ( ! is_array($criteria))
 			{
 				$criteria = array('id' => $criteria);
 			}
-		
+
 			foreach ($criteria as $field => $value)
 			{
 				$query->where($field, '=', $value);
 			}
-		
+
 			$query->execute();
 		}
-		catch( Exception $e)
+		catch (Exception $e)
 		{
-			Kohana::$log->add(LOG::ERROR, 'Error: :error deleting path alias!', array(':error' => $e->getMessage()) );
-			return false;
+			Kohana::$log->add(LOG::ERROR, 'Error: :error deleting path alias!',
+				array(':error' => $e->getMessage())
+			);
+			return FALSE;
 		}
-                
-                return TRUE;
+
+		return TRUE;
 	}
 
 	/**
-	 * Fetch a specific URL alias from the database.
+	 * Fetch a specific URL alias from the database
 	 *
-	 * @param   mixed    A string representing the source, a number representing the id, or an array of query conditions.
-	 * @return  mixed    FALSE if no alias was found or mysql result set
+	 * @param   mixed  $conditions  A string representing the source, a number representing the id, or an array of query conditions
+	 * @return  mixed  FALSE if no alias was found or MySQL result set
 	 */
-	public static function load( $conditions )
-        {
-                try
+	public static function load($conditions)
+	{
+		try
 		{
 			$path = DB::select()->from('paths');
-		
-			if( is_numeric($conditions))
+
+			if (is_numeric($conditions))
 			{
 				$path->where('id', '=', $conditions);
 			}
@@ -138,7 +161,7 @@ class Gleez_Path {
 			{
 				$path->where('source', '=', $conditions);
 			}
-			elseif( is_array($conditions))
+			elseif (is_array($conditions))
 			{
 				foreach($conditions as $field => $value)
 				{
@@ -147,95 +170,97 @@ class Gleez_Path {
 			}
 			else
 			{
-				return false;
+				return FALSE;
 			}
-		
+
 			$path = $path->execute()->current();
 		}
-		catch( Exception $e)
+		catch(Exception $e)
 		{
-			Kohana::$log->add(LOG::ERROR, 'Error: :error lookup path alias!', array(':error' => $e->getMessage()) );
-			return false;
+			Kohana::$log->add(LOG::ERROR, 'Error: :error lookup path alias!',
+				array(':error' => $e->getMessage())
+			);
+			return FALSE;
 		}
-                
-                return $path;
+
+		return $path;
 	}
 
 	/**
-	 * Fetch a specific URL alias from the database.
+	 * Fetch a specific URL alias from the database
 	 *
-	 * @param   string    A string representing the source.
-	 * @return  string    if alias exists alias or source
+	 * @param   string  $source  A string representing the source.
+	 * @return  string  If alias exists alias or source
 	 */
-	public static function alias( $source )
-        {
-                try
+	public static function alias($source)
+	{
+		try
 		{
 			return DB::select('alias')->from('paths')
 					->where('source', '=', $source)
 					->limit(1)
-					->execute()->get('alias', $source);
+					->execute()
+					->get('alias', $source);
 		}
-		catch(Exception $e)
+		catch (Exception $e)
 		{
-			Kohana::$log->add(LOG::ERROR, 'Error: :error getting alias!', array(':error' => $e->getMessage()) );
+			Kohana::$log->add(LOG::ERROR, 'Error: :error getting alias!',
+				array(':error' => $e->getMessage())
+			);
 			return $source;
 		}
 	}
-	
+
 	/**
-	 * Clean up a string segment to be used in an URL alias.
+	 * Clean up a string segment to be used in an URL alias
 	 *
 	 * Performs the following possible alterations:
-	 * - Remove all HTML tags.
-	 * - Replace or remove punctuation with the separator character.
-	 * - Remove back-slashes.
-	 * - Replace non-ascii and non-numeric characters with the separator.
-	 * - Remove common words.
-	 * - Replace whitespace with the separator character.
-	 * - Trim duplicate, leading, and trailing separators.
-	 * - Convert to lower-case.
-	 * - Shorten to a desired length and logical position based on word boundaries.
+	 * - Remove all HTML tags
+	 * - Replace or remove punctuation with the separator character
+	 * - Remove back-slashes
+	 * - Replace non-ascii and non-numeric characters with the separator
+	 * - Remove common words
+	 * - Replace whitespace with the separator character
+	 * - Trim duplicate, leading, and trailing separators
+	 * - Convert to lower-case
+	 * - Shorten to a desired length and logical position based on word boundaries
 	 *
-	 * @param    string	A string to clean
-	 * @return   string	The cleaned string
+	 * @param   string  $string  A string to clean
+	 * @return  string  The cleaned string
 	 */
 	public static function clean($string)
 	{
 		$separator = '-';
-	
+
 		// Empty strings do not need any proccessing.
 		if ($string === '' || $string === NULL)
 		{
 			return '';
 		}
-	
+
 		// Remove all HTML tags from the string.
 		$string = strip_tags($string);
-	
+
 		//Replace all characters that are not the separator, letters, numbers, or whitespace
 		$string = preg_replace('![^'.preg_quote($separator).'\pL\pN\/\s]+!u', $separator, $string);
-	
+
 		// Replace all separator characters and whitespace by a single separator
 		$string = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $string);
-	
+
 		//convert to lower case.
 		$string = UTF8::strtolower($string);
-	
+
 		// Trim separators from the beginning and end
 		return trim($string, $separator);
 	}
 
 	/**
-	 * Check if a path matches any pattern in a set of patterns.
+	 * Check if a path matches any pattern in a set of patterns
 	 *
-	 * @param $path
-	 *   The path to match.
-	 * @param $patterns
-	 *   String containing a set of patterns separated by \n, \r or \r\n.
+	 * @param  string  $path  The path to match
+	 * @param  string  $patterns  String containing a set of patterns separated by \n, \r or \r\n.
 	 *
-	 * @return
-	 *   Boolean value: TRUE if the path matches a pattern, FALSE otherwise.
+	 * @return  boolean  TRUE if the path matches a pattern, FALSE otherwise
 	*/
 	public static function match_path($path, $patterns)
 	{
@@ -256,5 +281,5 @@ class Gleez_Path {
 
 		return (bool)preg_match($regexps[$patterns], $path);
 	}
-	
+
 }
