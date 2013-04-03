20c270814a09e345e72ec8457beec6541ac4656e
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-03 23:21:33 +0400

    New feature: Added "None Page" with "Add" button for admin

---
+++ modules/gleez/views/admin/page/none.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') OR die('No direct script access.') ?>
+
+<h3><?php echo __('No Pages!') ?></h3>
+<?php echo HTML::anchor(Route::get('page')->uri(array('action' => 'add')), '<i class="icon-plus icon-white"></i> ' . __('Add Page'), array('class' => 'btn btn-primary pull-right')); ?>
+<div class="clearfix"></div><br>
+
+<p>
+	<?php echo __('There are no Pages that have been published.'); ?>
+</p>
