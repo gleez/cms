4c5e10139a983f57cd74bd05c2fbe3ad5b3525b7
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-04-05 03:42:43 +0530

    Update Feature: Migrated admin/paths to datatables

--- modules/gleez/views/admin/path/list.php
+++ modules/gleez/views/admin/path/list.php
@@ -1,36 +1,29 @@
 <?php defined('SYSPATH') or die('No direct script access.') ?>
 
-<div class="help">
-	<?php echo __('An alias defines a different name for an existing URL path - for example, the alias \'about\' for the URL path \'page/1\'. A URL path can have multiple aliases.'); ?>
-</div>
+<?php if ($is_datatables): ?>
+	<?php echo $datatables->render(); ?>
+<?php else:?>
+	<?php Assets::datatables(); ?>
+	<div class="help">
+		<?php echo __('An alias defines a different name for an existing URL path - for example, the alias \'about\' for the URL path \'page/1\'. A URL path can have multiple aliases.'); ?>
+	</div>
 
-<?php echo HTML::anchor(Route::get('admin/path')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i>'.__('Add Alias'), array('class' => 'btn btn-danger pull-right')) ?>
+	<?php echo HTML::anchor(Route::get('admin/path')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i>'.__('Add Alias'), array('class' => 'btn btn-danger pull-right')) ?>
+	<div class='clearfix'></div>
 
-    <table id="path-admin-list" class="table table-striped">
-      <thead>
-        <tr>
-          <th><?php echo __('Path') ?></th>
-          <th><?php echo __('Alias') ?></th>
-          <th><?php echo __('Actions') ?></th>
-        </tr>
-      </thead>
-         <?php foreach ($paths as $i => $path): ?>
-          <tr id="path-row-<?php echo $path->id ?>" class="<?php echo text::alternate("odd", "even") ?>">
-        
-            <td id="path-<?php echo $path->id ?>">
-              <?php echo Text::plain($path->source) ?>
-            </td>
-        
-            <td id="path-alias-<?php echo $path->id ?>">
-              <?php echo Text::plain($path->alias) ?>
-            </td>
-        
-            <td class="action">
-                 <?php echo HTML::anchor(Route::get('admin/path')->uri(array('action' => 'edit', 'id' => $path->id)), '<i class="icon-edit"></i>', array('class'=>'action-edit', 'title'=> __('Edit Alias'))) ?>
-                 <?php echo HTML::anchor(Route::get('admin/path')->uri(array('action' => 'delete', 'id' => $path->id)), '<i class="icon-trash"></i>', array('class'=>'action-delete', 'title'=> __('Delete Alias'))) ?>
-		</td>
-          </tr>
-          <?php endforeach ?>
-    </table>
-    
-  <?php echo $pagination ?>
+	<table id = "admin-list-paths" class="table table-striped table-bordered" data-toggle="datatable" data-target="<?php echo $url?>" data-sorting='[["1", "desc"]]'>
+		<thead>
+			<tr>
+				<th width="45%" class="sorting_desc"><?php echo __("Path"); ?></th>
+				<th width="45%" class="sorting_desc"><?php echo __("Alias"); ?></th>
+				<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td colspan="7" class="dataTables_empty"><?php echo __("Loading data from server"); ?></td>
+			</tr>
+		</tbody>
+	</table>
+
+<?php endif; ?>
