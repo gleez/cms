a3f38119a40b62bdddfc4edc36f6d131c5d4d864
Gleez Technologies <github@gleez.tech>
2013-04-06 19:24:36 +0530

    Fixed Issue: Cleanup page views.

--- modules/gleez/views/page/body.php
+++ modules/gleez/views/page/body.php
@@ -7,28 +7,30 @@
                 $widget_p_bot = Widgets::instance()->render('post_bottom');
         ?>
 
-	<div class="row-fluid meta">
-		<div class="span6">
+	<?php if($post->taxonomy OR $config->use_submitted): ?>
+		<div class="row-fluid meta">
 			<?php if ($config->use_submitted): ?>
-				<span class="author">
-				<?php
-					$nick = $post->user->nick;
-					$url  = $post->user->url;
-					$pic  = ( strlen($post->user->picture) > 4 ) ? $post->user->picture : 'media/images/commentor.jpg';
-					$img = HTML::resize($pic, array('title' => $nick, 'width' => 32, 'height' => 32, 'type' => 'resize') );
-
-					echo HTML::anchor($url, $img);
-					echo HTML::anchor($url, $nick, array('title' => $nick));
-					unset($nick, $img, $url);
-				?>
-				</span>
-
-				<span class="DateCreated"> <?php echo Date::date($post->created); ?> </span>
+				<div class="span6">
+					<span class="author">
+					<?php
+						$nick = $post->user->nick;
+						$url  = $post->user->url;
+						$pic  = ( strlen($post->user->picture) > 4 ) ? $post->user->picture : 'media/images/commentor.jpg';
+						$img = HTML::resize($pic, array('title' => $nick, 'width' => 32, 'height' => 32, 'type' => 'resize') );
+	
+						echo HTML::anchor($url, $img);
+						echo HTML::anchor($url, $nick, array('title' => $nick));
+						unset($nick, $img, $url);
+					?>
+					</span>
+	
+					<span class="DateCreated"> <?php echo Date::date($post->created); ?> </span>
+				</div>
 			<?php endif;?>
+	
+			<?php if ($post->taxonomy): ?> <div class="taxonomy span6 pull-right"> <?php echo $post->taxonomy; ?> </div> <?php endif;?>
 		</div>
-
-		<?php if ($post->taxonomy): ?> <div class="taxonomy span6"> <?php echo $post->taxonomy; ?> </div> <?php endif;?>
-	</div>
+	<?php endif;?>
 
 	<div class="content"> <?php echo $post->body; ?> </div>
 
--- modules/gleez/views/page/teaser.php
+++ modules/gleez/views/page/teaser.php
@@ -2,38 +2,35 @@
 
 <div id="post-<?php echo $post->id; ?>" class="<?php echo $post->type . ' post teaser post-' . $post->status; ?>">
 
-	<div class="row-fluid meta">
-
-		<div class="span6">
+	<?php if($post->taxonomy OR $config->use_submitted): ?>
+		<div class="row-fluid meta">
 			<?php if ($config->use_submitted): ?>
-				<span class="author">
-				<?php
-					$nick = $post->user->nick;
-					$url  = $post->user->url;
-					$pic  = ( strlen($post->user->picture) > 4 ) ? $post->user->picture : 'media/images/commentor.jpg';
-					$img = HTML::resize($pic, array('title' => $nick, 'width' => 32, 'height' => 32, 'type' => 'resize') );
-
-					echo HTML::anchor($url, $img);
-					echo HTML::anchor($url, $nick, array('title' => $nick));
-					unset($nick, $img, $url);
-				?>
-				</span>
-
-				<span class="DateCreated"><?php echo Date::date($post->created); ?></span>
+				<div class="span6">
+					<span class="author">
+					<?php
+						$nick = $post->user->nick;
+						$url  = $post->user->url;
+						$pic  = ( strlen($post->user->picture) > 4 ) ? $post->user->picture : 'media/images/commentor.jpg';
+						$img = HTML::resize($pic, array('title' => $nick, 'width' => 32, 'height' => 32, 'type' => 'resize') );
+	
+						echo HTML::anchor($url, $img);
+						echo HTML::anchor($url, $nick, array('title' => $nick));
+						unset($nick, $img, $url);
+					?>
+					</span>
+	
+					<span class="DateCreated"><?php echo Date::date($post->created); ?></span>
+				</div>
 			<?php endif;?>
+		
+			<?php if ($post->taxonomy): ?> <div class="taxonomy span6 pull-right"> <?php echo $post->taxonomy; ?> </div> <?php endif;?>
 		</div>
+	<?php endif;?>
 
-		<?php if ($post->taxonomy): ?>
-			<div class="taxonomy span6"> <?php echo $post->taxonomy; ?> </div>
-		<?php endif;?>
-	</div>
+	<div class="post-content"> <?php echo $post->teaser; ?> </div>
 
-	<div class="post-content">
-		<?php echo $post->teaser; ?>
-	</div>
-
-		<?php if ($post->tagcloud): ?>
-			<div class="tags"><?php echo __('Tagged with :tag', array(':tag' => $post->tagcloud)); ?></div>
-		<?php endif;?>
+	<?php if ($post->tagcloud): ?>
+		<div class="tags"><?php echo __('Tagged with :tag', array(':tag' => $post->tagcloud) ); ?></div>
+	<?php endif;?>
 
 </div>
