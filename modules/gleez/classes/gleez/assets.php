f4564e423426e53dd205c2c0b2647a8c694aef28
Gleez Technologies <github@gleez.tech>
2013-04-07 17:38:10 +0530

    Update Feature: Removed jQuery once support, no longer needed

--- modules/gleez/classes/gleez/assets.php
+++ modules/gleez/classes/gleez/assets.php
@@ -752,7 +752,6 @@ class Gleez_Assets {
 		{
 			Assets::js('jquery', 'media/js/jquery-1.9.1.min.js', NULL, FALSE, array('weight' => -20));
 			Assets::js('jquery-ua', 'media/js/jquery.browser.js', NULL, FALSE, array('weight' => -18));
-			Assets::js('jquery_once', 'media/js/jquery.once-1.1.js', array('jquery'), FALSE, array('weight' => -10));
 			Assets::js('gleez', 'media/js/gleez.js', array('jquery'), FALSE, array('weight' => -5));
 
 			$data = Arr::merge(array(array('basePath' => URL::base(TRUE))), Assets::$settings);
