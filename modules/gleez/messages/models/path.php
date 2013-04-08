28594e2dc755ea61885a5f7f474531fc01610e03
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-08 23:46:07 +0400

    Fixed Issue: Repair path managing

---
+++ modules/gleez/messages/models/path.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+	'source' => array(
+		'not_empty' => ':field must not be empty',
+	),
+	'alias' => array(
+		'not_empty' => ':field must not be empty',
+		'invalid_source' => 'Invalid URL Path'
+	),
+	'lang' => array(
+		'min_length' => 'Language must be at least :param2 characters long',
+		'max_length' => 'Language must be less than :param2 characters long',
+		'regex' => '',
+	),
+);
--- modules/gleez/views/admin/path/none.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.') ?>
-
-<h3><?php echo __('No Paths!'); ?></h3>
-<?php echo HTML::anchor(Route::get('admin/path')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i> ' . __('Add alias'), array('class' => 'btn btn-primary pull-right')); ?>
-
-<p>
-	<?php echo __('There are no Paths.') ?>
-</p>
