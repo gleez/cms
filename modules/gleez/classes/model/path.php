87f6c54274b51789eaf825ca349e162fd2fd3bed
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-08 23:46:07 +0400

    Fixed Issue: Repair path managing

--- modules/gleez/classes/model/path.php
+++ modules/gleez/classes/model/path.php
@@ -1,104 +1,139 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 /**
- * An adaptation of handle path aliasing.
+ * An adaptation of handle path aliasing
  *
- * @package    Gleez
- * @category   Path
+ * @package    Gleez\Path
  * @author     Sandeep Sangamreddi - Gleez
  * @copyright  (c) 2011 Gleez Technologies
- * @license    http://gleezcms.org/license
+ * @license    http://gleezcms.org/license  Gleez CMS License
  */
-class Model_Path extends ORM {       
+class Model_Path extends ORM {
 
+	/**
+	 * Table columns
+	 * @var array
+	 */
 	protected $_table_columns = array(
-					'id'               => array( 'type' => 'int' ),
-					'source'           => array( 'type' => 'string' ),
-					'alias'            => array( 'type' => 'string' ),
-					'lang'             => array( 'type' => 'string' ),
-					'route_name' 	   => array( 'type' => 'string' ),
-					'route_directory'  => array( 'type' => 'string' ),
-					'route_controller' => array( 'type' => 'string' ),
-					'route_action'     => array( 'type' => 'string' ),
-					'route_id'         => array( 'type' => 'string' ),
-					);
-	
-	protected $_ignored_columns = array('type', 'action');
-	
-        /**
-         * The language code used when no language is explicitly assigned.
-         *
-         * Defined by ISO639-2 for "Undetermined".
-         *
-         * @access  protected
-	 * @var     string  language
-         */
-        protected $_language_none = 'und';
+		'id'               => array( 'type' => 'int' ),
+		'source'           => array( 'type' => 'string' ),
+		'alias'            => array( 'type' => 'string' ),
+		'lang'             => array( 'type' => 'string' ),
+		'route_name' 	   => array( 'type' => 'string' ),
+		'route_directory'  => array( 'type' => 'string' ),
+		'route_controller' => array( 'type' => 'string' ),
+		'route_action'     => array( 'type' => 'string' ),
+		'route_id'         => array( 'type' => 'string' ),
+	);
 
+	/**
+	 * Ignored columns
+	 * @var array
+	 */
+	protected $_ignored_columns = array(
+		'type',
+		'action'
+	);
+
+ 	/**
+	 * The language code used when no language is explicitly assigned.
+	 * Defined by ISO639-2 for "Undetermined".
+	 * @link  http://www.loc.gov/standards/iso639-2/php/code_list.php
+	 * @var string
+	 */
+	protected $_language_none = 'und';
+
+	/**
+	 * Labels for fields in this model
+	 *
+	 * @return  array  Array of labels
+	 */
+	public function labels()
+	{
+		return array(
+			'source' => __('URL Path'),
+			'alias'  => __('Alias'),
+			'lang'   => __('Language'),
+		);
+	}
+
+	/**
+	 * Rules for the post model
+	 *
+	 * @return  array  Rules
+	 */
 	public function rules()
 	{
 		return array(
-                        'source' => array(
+			'source' => array(
 				array('not_empty'),
 			),
 			'alias' => array(
 				array('not_empty'),
 				array(array($this, 'process_alias'), array(':validation', ':field')),
 			),
-                        'lang' => array(
+			'lang' => array(
 				array('min_length', array(':value', 2)),
 				array('max_length', array(':value', 3)),
-				array('regex', array(':value', '/^[a-z]{2}/')),
-			),
-                        'route_controller' => array(
-				//array('not_empty'),
+				array('regex', array(':value', '/^[a-z]{2,3}/')),
 			),
 		);
 	}
 
 	/**
-	 * Check by triggering error if process.
+	 * Check by triggering error if process
+	 *
 	 * Validation callback.
 	 *
-	 * @param   Validation  Validation object
-	 * @param   string      Field name
+	 * @param   Validation  $validation  Object for validation
+	 * @param   string      $field       Field name
 	 * @return  void
+	 *
+	 * @uses    Module::event
 	 */
 	public function process_alias(Validation $validation, $field)
 	{
-		//always set unique alias if its set
-		$alias  = $this->_unique_slug( trim($this->alias) );
-	
-		//make sure only one alias exists for home page <front>
-		if($this->alias === '<front>') $alias = '<front>';
-	
-		$this->type   = empty($this->type) ? NULL : $this->type;
-	
-		//allow other modules to interact with alias
+		// always set unique alias if its set
+		$alias  = $this->_unique_slug(trim($this->alias));
+
+		// make sure only one alias exists for home page <front>
+		if ($this->alias === Path::FRONT_ALIAS)
+		{
+			$alias = Path::FRONT_ALIAS;
+		}
+
+		$this->type = empty($this->type) ? NULL : $this->type;
+
+		// allow other modules to interact with alias
 		Module::event('path_aliases', $this);
-	
-		$source = trim($this->source, '/');
-		$this->lang   = empty($this->lang) ? $this->_language_none : $this->lang;
-	
-		if( $params = $this->_process_uri($source) )
-                {
-			if(isset($params['directory']))
+
+		$source     = trim($this->source, '/');
+		$this->lang = empty($this->lang) ? $this->_language_none : $this->lang;
+
+		if ($params = $this->_process_uri($source))
+		{
+			if (isset($params['directory']))
+			{
 				$this->route_directory  = $params['directory'];
-			
-			if(isset($params['controller']))
+			}
+			if (isset($params['controller']))
+			{
 				$this->route_controller = $params['controller'];
-			
-			if(isset($params['action']))
+			}
+			if (isset($params['action']))
+			{
 				$this->route_action = $params['action'];
-			
-			if(isset($params['id']))
+			}
+			if (isset($params['id']))
+			{
 				$this->route_id  = $params['id'];
-			
-			if(isset($params['route']))
+			}
+			if (isset($params['route']))
+			{
 				$this->route_name  = $params['route'];
-			
+			}
 			$this->alias = $alias;
-                }
-		elseif( !isset($params['controller']) OR !isset($params) OR empty($params['controller']) OR empty($params) )
+		}
+		elseif ( ! isset($params['controller']) OR ! isset($params) OR empty($params['controller']) OR empty($params))
 		{
 			$validation->error($field, 'invalid_source', array($validation[$field]));
 		}
@@ -106,26 +141,25 @@ class Model_Path extends ORM {
 
 	private function _unique_slug($str)
 	{
-		$slug = $str;
+		$path   = ORM::factory('path', array('alias' => $str));
+		$slug   = $str;
 		$suffix = 0;
 
-		while( $path = ORM::factory('path', array('alias' => $str) )
-			AND $path->loaded()
-			AND $path->source != $this->source
-		      )
+		while ($path AND $path->loaded() AND $path->source != $this->source )
 		{
-			$str = substr( $slug, 0, 200 - ( strlen( $suffix ) + 1 ) ) . "-$suffix";
+			$str = substr($slug, 0, 200 - (strlen($suffix) + 1)) . "-$suffix";
 			$suffix++;
 		}
-	
+
 		return $str;
 	}
-	
+
 	/**
 	 * Process URI
 	 *
-	 * @param   string  $uri     URI
-	 * @return  array
+	 * @param   string  $uri  URI
+	 * @return  array|boolean
+	 * @uses    Route::all
 	 */
 	private function _process_uri($uri)
 	{
@@ -145,5 +179,28 @@ class Model_Path extends ORM {
 
 		return FALSE;
 	}
-	
+
+	/**
+	 * Reading data from inaccessible properties
+	 *
+	 * @param   string  $field
+	 * @return  mixed
+	 *
+	 * @uses  Route::get
+	 * @uses  Route::uri
+	 */
+	public function __get($field)
+	{
+		switch ($field)
+		{
+			case 'edit_url':
+				return Route::get('admin/path')->uri(array('action' => 'edit', 'id' => $this->id));
+			break;
+			case 'delete_url':
+				return Route::get('admin/path')->uri(array('action' => 'delete', 'id' => $this->id));
+			break;
+		}
+
+		return parent::__get($field);
+	}
 }
