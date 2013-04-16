349f6b104655d7c733ee49085b064cee85074d97
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-16 06:02:23 +0400

    Updated feature: Inproved Gleez Installer Template

--- modules/gleez/views/install/template.php
+++ modules/gleez/views/install/template.php
@@ -1,81 +1,86 @@
 <!DOCTYPE html>
 <html lang="<?php echo substr(I18n::$lang, 0, 2) ?>">
 <head>
-  <meta charset="utf-8">
-  <title><?php echo $title; ?> | Gleez CMS</title>
-  <?php
-  foreach ($styles as $style => $media)
-  {
-    echo HTML::style($style, array('media' => $media), TRUE).PHP_EOL;
-  }
-  ?>
-  <link rel="shortcut icon" href="<?php echo URL::site($link); ?>" type="image/x-icon">
-  <style>
-    body {
-      padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-    }
-  </style>
-  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-  <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+    <meta charset="utf-8">
+    <title><?php echo $title; ?> | Gleez CMS</title>
+    <?php
+        foreach ($styles as $style => $media)
+        {
+            echo HTML::style($style, array('media' => $media), TRUE).PHP_EOL;
+        }
+    ?>
+    <link rel="shortcut icon" href="<?php echo URL::site($link); ?>" type="image/x-icon">
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 </head>
 
 <body class="<?php echo substr(I18n::$lang, 0, 2) ?>">
+    <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="brand" href="/"><?php echo __('Gleez Installer'); ?></a>
+            </div>
+        </div>
+    </div>
 
-<div class="navbar navbar-fixed-top">
-  <div class="navbar-inner">
     <div class="container">
-      <a class="brand" href="#"><?php echo __('Gleez Installer'); ?></a>
-    </div>
-  </div>
-</div>
-
-<div class="container">
-  <div class="row">
-    <div class="span12">
-      <h1><?php echo HTML::chars($title) ?></h1>
-
-      <?php if ( ! empty($error)): ?>
-      <div class="alert alert-error">
-        <p><?php echo HTML::chars($error) ?></p>
-      </div>
-      <?php endif; ?>
-
-      <div style="margin-bottom: 2em;" class="progress progress-warning progress-striped">
-        <div style="width:<?php echo $_activity; ?>%" class="bar"></div>
-      </div>
-
-      <div class="row">
-        <div class="span8">
-          <?php echo $content; ?>
+        <div class="row">
+            <div class="span12">
+                <!--[if lt IE 7]>
+                    <div class="alert">
+                        <strong><?php echo __('Warning!'); ?></strong>
+                        <?php
+                        echo __('You are using an :out browser. Please :url or :frame to improve your experience', array(
+                                ':out' => '<strong>'.__('outdated').'</strong>',
+                                ':url' => HTML::anchor('http://browsehappy.com/', __('upgrade your browser')),
+                                ':frame' => HTML::anchor('http://www.google.com/chromeframe/?redirect=true&hl='.substr(I18n::$lang, 0, 2), __('activate Google Chrome Frame')),
+                            )
+                        )
+                        ?>
+                    </div>
+                <![endif]-->
+                <div class="row">
+                    <div class="span9">
+                        <?php if ( ! empty($error)): ?>
+                            <div class="alert alert-error">
+                                <p><?php echo HTML::chars($error) ?></p>
+                            </div>
+                        <?php endif; ?>
+                        <div class="hero-unit">
+                            <h1><?php echo HTML::chars($title) ?></h1>
+                            <?php echo $content; ?>
+                        </div>
+                        <div class="progress progress-info progress-striped active">
+                            <div style="width:<?php echo $_activity; ?>%" class="bar"></div>
+                        </div>
+                    </div>
+                    <div class="span3 menu">
+                        <ol>
+                            <?php foreach ($menu as $item): ?>
+                                <li><?php echo $item; ?></li>
+                            <?php endforeach; ?>
+                        </ol>
+                        <hr>
+                        <blockquote>
+                            <?php
+                            echo __('Did something go wrong? Try the :github.', array(':github' => HTML::anchor('https://github.com/gleez/cms/issues', 'Github Issues') ));
+                            ?>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div id="menu" class="span3 well">
-          <ol>
-            <?php foreach ($menu as $item): ?>
-            <li><?php echo $item; ?> </li>
-            <?php endforeach; ?>
-          </ol>
+    </div>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-credits">
+                <p><?php echo __('Powered by :gleez', array(':gleez' => HTML::anchor('http://gleezcms.org/', 'Gleez CMS') )); ?> v<?php echo Gleez::VERSION ?></p>
+                <p>&copy; 2011-<?php echo date('Y') ?> Gleez Technologies</p>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="span12">
-      <footer class="footer">
-        <blockquote>
-          <?php
-          echo __('Did something go wrong? Try the :github.', array(':github' => HTML::anchor('https://github.com/gleez/cms/issues', 'Github Issues') ));
-          ?>
-        </blockquote>
-
-        <p class="powered pull-right"><?php echo __('Powered by :gleez', array(':gleez' => HTML::anchor('http://gleezcms.org/', 'Gleez') )); ?> v<?php echo Gleez::VERSION ?></p>
-        <p class="copyright pull-left">&copy; <?php echo date('Y') ?> Gleez Technologies</p>
-
-      </footer>
-    </div>
-  </div>
-</div>
+    </footer>
 
 </body>
 </html>
