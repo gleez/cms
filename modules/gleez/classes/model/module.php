b3d60c906da634846f3a91009dfe13b8be95c864
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-17 03:11:22 +0400

    Fixed issue: Cleanup Model, added PHPDoc.

--- modules/gleez/classes/model/module.php
+++ modules/gleez/classes/model/module.php
@@ -1,14 +1,25 @@
-<?php defined("SYSPATH") or die("No direct script access.");
-
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Module Model Class
+ *
+ * @package    Gleez\Module
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2011-2013 Gleez Technologies
+ * @license    http://gleezcms.org/license Gleez CMS License
+ */
 class Model_Module extends ORM {
-        
-        protected $_table_columns = array(
-					'id'       => array('type'=>'int'),
-					'name'     => array('type'=>'string'),
-					'active'   => array('type'=>'int'),
-					'weight'   => array('type'=>'int'),
-					'version'  => array('type'=>'float'),
-					'path'     => array('type'=>'string'),
-					);
+
+	/**
+	 * Table columns
+	 * @var array
+	 */
+	protected $_table_columns = array(
+		'id'      => array( 'type'=>'int' ),
+		'name'    => array( 'type'=>'string' ),
+		'active'  => array( 'type'=>'int' ),
+		'weight'  => array( 'type'=>'int' ),
+		'version' => array( 'type'=>'float' ),
+		'path'    => array( 'type'=>'string' ),
+	);
 
 }
