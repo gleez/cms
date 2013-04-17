24d9649a6d4a7b50a9285933e5772a798442fea3
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-17 06:41:09 +0400

    Fixed issue: Fixed Role add/edit form

--- modules/user/views/admin/role/form.php
+++ modules/user/views/admin/role/form.php
@@ -1,8 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
-<?php $parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add'); ?>
-
-<?php echo Form::open(Route::get('admin/role')->uri($parms), array('id'=>'role-form', 'class'=>'role-form form form-horizontal')); ?>
+<?php echo Form::open($action, array('id'=>'role-form', 'class'=>'role-form form form-horizontal well')); ?>
 
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
@@ -21,12 +19,13 @@
 	</div>
 
 	<div class="control-group <?php echo isset($errors['special']) ? 'error': ''; ?>">
-		<?php echo Form::label('special', __('Special'), array('class' => 'control-label')); ?>
+		<?php echo Form::label('special', __('Special Role'), array('class' => 'control-label')); ?>
 		<div class="controls">
-			<?php echo Form::select('special', array(0 => __('No'), 1 => __('Yes')), $post->special, array('class' => 'input-xlarge')); ?>
+			<?php echo Form::select('special', array(0 => __('No'), 1 => __('Yes')), $post->special, array('class' => 'input-small')); ?>
 		</div>
 	</div>
 
-	<?php echo Form::submit('role', __('Save'), array('class' => 'btn btn-primary')) ?>
+	<?php echo Form::submit('role', __('Save'), array('class' => 'btn btn-primary pull-right')) ?>
+	<div class="clearfix"></div><br>
 
 <?php echo Form::close(); ?>
