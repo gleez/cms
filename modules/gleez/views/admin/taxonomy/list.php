6aae9a0d93e8ff35a99de2a7f2ad284e3fee1efe
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-04-25 13:51:21 +0530

    Update Feature: migrate admin/taxonomy to datatables

--- modules/gleez/views/admin/taxonomy/list.php
+++ modules/gleez/views/admin/taxonomy/list.php
@@ -1,47 +1,31 @@
 <?php defined('SYSPATH') or die('No direct script access.') ?>
 
-<div class="help">
-	<?php echo __('Taxonomy is for categorizing content. Terms are grouped into vocabularies. For example, a vocabulary called "Fruit" would contain the terms "Apple" and "Banana".'); ?>
-</div>
+<?php if ($is_datatables): ?>
+	<?php echo $datatables->render(); ?>
+<?php else:?>
+	<?php Assets::datatables(); ?>
+	<div class="help">
+		<p><?php echo __('Taxonomy is for categorizing content. Terms are grouped into vocabularies. For example, a vocabulary called "Fruit" would contain the terms "Apple" and "Banana".'); ?></p>
+	</div>
 
-<?php echo HTML::anchor(Route::get('admin/taxonomy')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i>'.__('Add New Vocabulary'), array('class' => 'btn btn-danger pull-right')) ?>
-	<div class="clearfix"></div>
-    <table id="taxonomy-admin-list" class="table table-striped">
-      <thead>
-        <tr>
-          <th><?php echo __('Name') ?></th>
-          <th colspan="4"><?php echo __('Actions') ?></th>
-        </tr>
-      </thead>
-         <?php foreach ($terms as $i => $term): ?>
-          <tr id="taxonomy-row-<?php echo $term->id ?>" class="<?php echo text::alternate('odd', 'even') ?>">
-	  
-            <td id="taxonomy-<?php echo $term->id ?>">
-                <?php echo $term->name ?>
-                
-	      	<div class="description">
-			<?php echo HTML::chars($term->description) ?>
-	        </div>
-            </td>
+	<?php echo HTML::anchor($add_url, '<i class="icon-plus icon-white"></i> '.__('Add New Vocabulary'), array('class' => 'btn btn-danger pull-right')) ?>
+	<div class='clearfix'></div><br>
 
-            <td class="action">
-		<?php echo HTML::anchor(Route::get('admin/term')->uri(array('action' => 'list', 'id' => $term->id)), '<i class="icon-th-list"></i>', array('class'=>'action-list', 'title'=>__('List Terms'))) ?>
-	    </td>
-	    
-	    <td class="action">
-		<?php echo HTML::anchor(Route::get('admin/term')->uri(array('action' => 'add', 'id' => $term->id )), '<i class="icon-plus"></i>', array('class'=>'action-add', 'title'=>__('Add Term'))) ?>
-	    </td>
-	    
-	    <td class="action">
-	       <?php echo HTML::anchor(Route::get('admin/taxonomy')->uri(array('action' => 'edit', 'id' => $term->id )), '<i class="icon-edit"></i>', array('class'=>'action-edit', 'title'=>__('Edit Vocab'))) ?>
-	    </td>
-	    
-	    <td class="action">
-	       <?php echo HTML::anchor(Route::get('admin/taxonomy')->uri(array('action' => 'delete', 'id' => $term->id )), '<i class="icon-trash"></i>', array('class'=>'action-delete', 'title'=>__('Delete Vocab'))) ?>
-            </td>
-	    
-          </tr>
-          <?php endforeach ?>
-    </table>
-    
-  <?php echo $pagination ?>
+	<table id = "admin-list-paths" class="table table-striped table-bordered" data-toggle="datatable" data-target="<?php echo $url?>" data-sorting='[["0", "desc"]]'>
+		<thead>
+		<tr>
+			<th width="60%" class="sorting_desc"><?php echo __("Title"); ?></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+		</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td colspan="3" class="dataTables_empty"><?php echo __("Loading data from server"); ?></td>
+			</tr>
+		</tbody>
+	</table>
+
+<?php endif; ?>
