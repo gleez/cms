a98325df7dd57376ceced76e738d32027dfc7922
Gleez Technologies <github@gleez.tech>
2013-04-25 13:57:24 +0530

    Update Feature: migrate admin/comments to datatables

--- modules/gleez/views/admin/comment/list.php
+++ modules/gleez/views/admin/comment/list.php
@@ -1,67 +1,41 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
+<?php defined('SYSPATH') or die('No direct script access.') ?>
 
-    <div class="help">
-	<?php echo __('View, edit, and delete your site\'s comments.'); ?>
-    </div>
+<?php if ($is_datatables): ?>
+	<?php echo $datatables->render(); ?>
+<?php else:?>
+	<?php Assets::datatables(); ?>
+	<div class="help">
+		<p><?php echo __('View, edit, and delete your site\'s comments.'); ?></p>
+	</div>
 
-	<?php include Kohana::find_file('views', 'errors/partial'); ?>
-
-    <div class="content">
-	<?php echo Form::open(Route::get('admin/comment')->uri(array('action' => 'process')).URL::query($destination),
+	<?php echo Form::open(Route::get('admin/comment')->uri(array('action' => 'process')).$destination,
                                array('id'=>'admin-comment-form', 'class'=>'no-form')); ?>
 
-	<fieldset>
-	    <legend><?php echo __('Bulk Actions'); ?></legend>
-	    <div class="control-group edit-operation <?php echo isset($errors['operation']) ? 'error': ''; ?>">
-		<?php echo Form::select('operation', $bulk_actions, '', array('class' => 'list small')); ?>
-	    </div>
-	    <?php echo Form::submit('comment-bulk-actions', __('Apply'), array('class'=>'submit')); ?>
-	</fieldset>
+	<div class="thumbnail">
+		<legend><?php echo __('Bulk Actions'); ?></legend>
+		<div class="control-group edit-operation <?php echo isset($errors['operation']) ? 'error': ''; ?>">
+			<?php echo Form::select('operation', $bulk_actions, '', array('class' => 'input-xlarge')); ?>
+			<?php echo Form::submit('comment-bulk-actions', __('Apply'), array('class'=>'btn btn-danger')); ?>
+		</div>
+	</div><br>
 
-	<table class="table table-striped table-bordered" id="comments-admin-list">
-
-	    <thead>
+	<table id = "admin-list-paths" class="table table-striped table-bordered" data-toggle="datatable" data-target="<?php echo $url?>" data-sorting='[["1","desc"],["2","asc"],["4","desc"]]'>
+		<thead>
 		<tr>
-		    <th>#</th>
-		    <th> <?php echo __('Subject'); ?>&nbsp;<?php echo URL::sortAnchor('title'); ?> </th>
-		    <th> <?php echo __('Author'); ?>&nbsp;<?php echo URL::sortAnchor('author'); ?>  </th>
-                    <th> <?php echo __('Posted In');?>  </th>
-		    <th> <?php echo __('Created'); ?>&nbsp;<?php echo URL::sortAnchor('created'); ?>  </th>
-		    <th> <?php echo __('Actions'); ?></th>
+			<th width="5%">#</th>
+			<th width="20%"><?php echo __("Subject"); ?></th>
+			<th width="20%"><?php echo __('Author'); ?></th>
+			<th width="35%"><?php echo __('Posted In');?></th>
+			<th width="10%"><?php echo __('Created'); ?></th>
+			<th width="5%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="5%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
 		</tr>
-	    </thead>
+		</thead>
 		<tbody>
-	    <?php foreach ($posts as $post):  ?>
-	    <tr id="comment-row-<?php echo $post->id ?>" class="<?php print text::alternate("odd", "even"); ?>">
-
-		<td><?php echo Form::checkbox('comments['.$post->id.']', $post->id, isset($_POST['comments'][$post->id]) ); ?> </td>
-
-		<td> <?php echo HTML::anchor($post->url, $post->title, array('class'=>'action-view',
-                                                'title' => Text::limit_words( $post->rawbody, 128, ' ...'))) ?> </td>
-
-		<td>
-		    <?php if ($post->author == 1 AND ! is_null($post->guest_name)): ?>
-			<?php echo HTML::anchor($post->guest_url, $post->guest_name, array()) . __(' (not verified)'); ?>
-		    <?php else: ?>
-			<?php echo HTML::anchor(Route::get('user')->uri(array('action' => 'profile',
-			'id' => $post->author)), $post->user->nick, array()) ?>
-		    <?php endif ?>
-
-		</td>
-
-                <td> <?php echo HTML::anchor($post->post->url, $post->post->title, array('class'=>'action-view')) ?> </td>
-		<td> <?php echo Date::date($post->created); ?> </td>
-		<td class="action">
-		    <?php echo HTML::anchor($post->edit_url.URL::query($destination), '<i class="icon-edit"></i>', array('class'=>'action-edit', 'title'=> __('Edit'))) ?>
-		    <?php echo HTML::anchor($post->delete_url.URL::query($destination), '<i class="icon-trash"></i>', array('class'=>'action-delete', 'title'=> __('Delete'))) ?>
-		</td>
-
-	    </tr>
-	    <?php endforeach ?>
+			<tr>
+				<td colspan="7" class="dataTables_empty"><?php echo __("Loading data from server"); ?></td>
+			</tr>
 		</tbody>
-      </table>
-
+	</table>
 	<?php echo Form::close(); ?>
-    </div>
-
-    <?php echo $pagination ?>
+<?php endif; ?>
