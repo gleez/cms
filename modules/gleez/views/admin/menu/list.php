ea6fad9f2155f29d0b74a0de26bd6a7a9e97fae1
Gleez Technologies <github@gleez.tech>
2013-04-25 13:46:56 +0530

    Update Feature: migrate admin/menus to datatables

--- modules/gleez/views/admin/menu/list.php
+++ modules/gleez/views/admin/menu/list.php
@@ -1,46 +1,31 @@
-<?php defined("SYSPATH") OR die("No direct script access.") ?>
+<?php defined('SYSPATH') or die('No direct script access.') ?>
 
-<div class="help">
-	<?php echo __('The Menu wizard provides an interface for managing menus. A menu is a hierarchical collection of links, which can be within or external to the site, generally used for navigation.', array(':menus' => 'admin/menu')); ?>
-</div>
+<?php if ($is_datatables): ?>
+	<?php echo $datatables->render(); ?>
+<?php else:?>
+	<?php Assets::datatables(); ?>
+	<div class="help">
+		<p><?php echo __('The Menu wizard provides an interface for managing menus. A menu is a hierarchical collection of links, which can be within or external to the site, generally used for navigation.', array(':menus' => 'admin/menu')); ?></p>
+	</div>
 
-<?php echo HTML::anchor(Route::get('admin/menu')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i> '.__('Add Menu'), array('class' => 'btn btn-danger pull-right')); ?>
-<div class='clearfix'></div><br>
+	<?php echo HTML::anchor($add_url, '<i class="icon-plus icon-white"></i> '.__('Add Menu'), array('class' => 'btn btn-danger pull-right')) ?>
+	<div class='clearfix'></div><br>
 
-<table id="menu1-admin-list" class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th><?php echo __('Title') ?></th>
-          <th colspan="4"><?php echo __('Actions') ?></th>
-        </tr>
-      </thead>
+	<table id = "admin-list-paths" class="table table-striped table-bordered" data-toggle="datatable" data-target="<?php echo $url?>" data-sorting='[["0", "desc"]]'>
+		<thead>
+		<tr>
+			<th width="60%" class="sorting_desc"><?php echo __("Title"); ?></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+			<th width="10%" data-columns='{"bSortable":false, "bSearchable":false}'></th>
+		</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td colspan="3" class="dataTables_empty"><?php echo __("Loading data from server"); ?></td>
+			</tr>
+		</tbody>
+	</table>
 
-        <?php foreach ($menus as $i => $menu): ?>
-
-          <tr id="menu1-row-<?php echo $menu->id ?>" >
-
-            <td id="menu-<?php echo $menu->name ?>">
-              <?php echo HTML::chars($menu->title) ?>
-
-	       <div class="description">
-			<?php echo HTML::chars($menu->descp) ?>
-	       </div>
-            </td>
-
-            <td class="action1">
-		 <?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('id' => $menu->id)), '<i class="icon-th-list"></i>', array('class'=>'action-list', 'title'=>__('List Links'))) ?>
-	    </td>
-	    <td class="action">
-		 <?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'add', 'id' => $menu->id)), '<i class="icon-plus"></i>', array('class'=>'action-add', 'title'=>__('Add Link'))) ?>
-	    </td>
-	    <td class="action">
-               <?php echo HTML::anchor(Route::get('admin/menu')->uri(array('action' => 'edit', 'id' => $menu->id)), '<i class="icon-edit"></i>', array('class'=>'action-edit', 'title'=>__('Edit Menu'))) ?>
-	    </td>
-	    <td class="action">
-               <?php echo HTML::anchor(Route::get('admin/menu')->uri(array('action' => 'delete', 'id' => $menu->id)), '<i class="icon-trash"></i>', array('class'=>'action-delete', 'title'=>__('Delete Menu'))) ?>
-            </td>
-          </tr>
-          <?php endforeach ?>
-</table>
-
-  <?php echo $pagination ?>
+<?php endif; ?>
