f527b47adc5b41a14f0abe03fd1b262f8c9653dd
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-27 02:55:01 +0400

    Updated feature: Updated DB schema for Contact Permissions

--- modules/gleez/views/install/install.ru.sql
+++ modules/gleez/views/install/install.ru.sql
@@ -243,6 +243,7 @@ CREATE TABLE {permissions} (
 INSERT INTO `permissions` (`rid`, `permission`, `module`) VALUES
 (1, 'access content', 'content'),
 (1, 'access profiles', 'user'),
+(1, 'sending mail', 'contact'),
 (3, 'access content', 'content'),
 (3, 'access profiles', 'user'),
 (3, 'create page', 'content'),
