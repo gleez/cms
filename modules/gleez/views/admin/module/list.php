ad0b18d53f4f6fec909eb7612571d8338ac6e423
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-28 00:36:53 +0400

    Updated feature: Modified module list view

--- modules/gleez/views/admin/module/list.php
+++ modules/gleez/views/admin/module/list.php
@@ -6,32 +6,38 @@
 
 <?php echo Form::open($action, array('id'=>'module-form', 'class'=>'form')); ?>
 
-	<table class="table table-striped table-bordered" id="admin-modules">
+	<table class="table table-bordered table-striped table-highlight" id="admin-modules">
 		<thead>
 			<tr>
 				<th>#</th>
-				<th><?php echo __('Name'); ?></th>
-				<th><?php echo __('Version'); ?></th>
+				<th><?php echo __('Module'); ?></th>
 				<th><?php echo __('Description'); ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php foreach ($available as $module_name => $module_info):  ?>
-			<tr class="<?php echo Text::alternate("odd", "even"); ?>">
-				<td>
+			<tr>
+				<td class="check-column">
 					<?php if ($module_info->locked): ?>
 						<?php echo Form::checkbox($module_name, TRUE, $module_info->active, array('disabled')); ?>
 					<?php else: ?>
 						<?php echo Form::checkbox($module_name, TRUE, $module_info->active); ?>
 					<?php endif ?>
 				</td>
-				<td><?php echo $module_info->name; ?></td>
-				<td><?php echo $module_info->version; ?></td>
-				<td>
-					<p><?php echo __($module_info->description); ?></p>
-					<p class="pull-right">
-						<?php echo __($module_info->author) ?>
-					</p>
+				<td class="module-title-column">
+					<strong><?php echo $module_info->name; ?></strong>
+				</td>
+				<td class="module-description-column">
+					<div class="module-description">
+						<p><?php echo __($module_info->description); ?></p>
+					</div>
+					<div class="module-version">
+						<?php echo __('Version :ver | By :author', array(
+							':ver' => $module_info->version,
+							':author' => HTML::anchor($module_info->authorURL, __($module_info->author))
+						));
+						?>
+					</div>
 				</td>
 			</tr>
 			<?php endforeach ?>
