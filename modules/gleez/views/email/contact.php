603f69aaaa47d5435b8676670ac84f195401ede8
Sergey Yakovlev <sadhooklay@gmail.com>
2013-04-28 16:08:47 +0400

    Updated features: Updated mail template

--- modules/gleez/views/email/contact.php
+++ modules/gleez/views/email/contact.php
@@ -1,11 +1,18 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
 
-<?php echo __('Hello!').PHP_EOL ?>
+<h1><?php echo __('Hello!')?></h1>
 
-<?php echo __(':name sent a message using the contact form at :site.', array(
+<p><?php echo __(':name sent a message using the contact form at :site.', array(
 		':name' => Text::plain($name),
 		':site' => URL::site('contact', TRUE)
 	));
-?>
+?></p>
 
 <?php echo Text::markup($body); ?>
+<br>
+<hr>
+<p>
+	<?php echo __('Best Regards'); ?>,<br>
+	<?php echo $config->get('site_url', 'www.gleezcms.org'); ?><br>
+	<?php echo $config->get('site_name', 'Gleez CMS'); ?>
+</p>
