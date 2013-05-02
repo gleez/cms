572492c98a2ef48eca2eaf835ba209eaaf005de2
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-02 16:37:38 +0400

    New feature: Added config for blogs

---
+++ modules/gleez/config/blog.php
@@ -0,0 +1,93 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+
+	/**
+	 * Default blog status (eg: draft, review, publish, etc)
+	 * @var string
+	 */
+	'default_status' => 'draft',
+
+	/**
+	 * Pages per page (ex: 5, 10, 15, etc)
+	 * @var integer
+	 */
+	'items_per_page' => 15,
+
+	/**
+	 * Enable captcha?
+	 * @var boolean
+	 */
+	'use_captcha' => FALSE,
+
+	/**
+	 * Enable to set page author?
+	 * @var boolean
+	 */
+	'use_authors' => TRUE,
+
+	/**
+	 * Enable teaser?
+	 * @var boolean
+	 */
+	'use_excerpt' => FALSE,
+
+	/**
+	 * Enable comment?
+	 * @var boolean
+	 */
+	'use_comment' => TRUE,
+
+	/**
+	 * Enable tags?
+	 * @var boolean
+	 */
+	'use_tags' => FALSE,
+
+	/**
+	 * Show submitted info in views?
+	 * @var boolean
+	 */
+	'use_submitted' => TRUE,
+
+	/**
+	 * Enable terms?
+	 * @var boolean
+	 */
+	'use_category' => FALSE,
+
+	/**
+	 * Enable login buttons above comment form?
+	 * @var boolean
+	 */
+	'use_provider_buttons' => TRUE,
+
+	/**
+	 * Allow people to post Comments (0: disabled, 1: read, 2: read/write)
+	 * @var integer
+	 */
+	'comment' => 1,
+
+	/**
+	 * Comment display mode
+	 * @var boolean
+	 */
+	'comment_default_mode' => FALSE,
+
+	/**
+	 * Allow anonymous commenting (with contact information)?
+	 */
+	'comment_anonymous' => FALSE,
+
+	/**
+	 * Comments per page
+	 * @var integer
+	 */
+	'comments_per_page' => 20,
+
+	/**
+	 * Comments displayed with the older/new comments (asc||desc)
+	 * @var string
+	 */
+	'comment_order' => 'asc',
+);
