d634ab0d4e8e5944c535f5950567c60a1a72d701
Gleez Technologies <gleez@server.gleeztech.org>
2013-05-03 19:53:04 +0530

    Fixed issue: update bootstrap and fromat class to new config system

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -4,7 +4,6 @@
 
 // Load the core core classes
 require GLZPATH.'classes/kohana'.EXT;
-require GLZPATH.'classes/kohana/config'.EXT;
 require GLZPATH.'classes/gleez/core'.EXT;
 require GLZPATH.'classes/gleez'.EXT;
 
