c5ffd7f57e56749e68349b4594455bfb41d8eb87
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-04 19:59:01 +0400

    Fixed issue: Fixed blog links in DB schema

--- modules/gleez/views/install/install.sql
+++ modules/gleez/views/install/install.sql
@@ -183,7 +183,7 @@ INSERT INTO {menus} (`id`, `title`, `name`, `descp`, `image`, `url`, `params`, `
 (12, 'Contact', 'contact', '', 'icon-envelope', 'contact', NULL, 1, 1, 8, 9, 2, 1),
 (13, 'Administer', 'administer', '', 'icon-cog', 'admin', NULL, 1, 2, 2, 3, 2, 2),
 (14, 'Menus', 'menus', '', 'icon-bookmark', 'admin/menus', NULL, 1, 2, 6, 7, 2, 2),
-(15, 'Blogs', 'blogs', '', 'icon-book', 'admin/blog/list', NULL, 1, 2, 10, 11, 2, 2),
+(15, 'Blogs', 'blogs', '', 'icon-book', 'admin/blogs/list', NULL, 1, 2, 10, 11, 2, 2),
 (16, 'Input Formats', 'input-formats', '', 'icon-magnet', 'admin/formats', NULL, 1, 2, 16, 17, 2, 2),
 (17, 'Settings', 'settings', '', 'icon-cogs', 'admin/settings', NULL, 1, 2, 26, 27, 2, 2),
 (18, 'Path Alias', 'path-alias', '', 'icon-link', 'admin/paths', NULL, 1, 2, 18, 19, 2, 2),
