eeea9b3d472fc24c4916a08b20d8a490bd933ab4
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-04 19:57:47 +0400

    Updated feature: Now site config variable installed_locales is a multidimensional array containing the locale for the different operating systems and languages

--- modules/gleez/config/site.php
+++ modules/gleez/config/site.php
@@ -94,13 +94,32 @@ return array(
     ),
   ),
 
-  /** @var array Installed locales */
-  'installed_locales' => array(
-    'en',
-    'it',
-    'ru',
-    'zh',
-  ),
+	/**
+	 * Installed locales
+	 *@var array
+	 */
+	'installed_locales' => array(
+		'en' => array(
+			'LINUX'   => 'en_US.utf-8',
+			'WINDOWS' => 'english',
+			'OTHER'   => 'en_US.utf-8',
+		),
+		'it' => array(
+			'LINUX'   => 'it_IT.utf-8',
+			'WINDOWS' => 'italian',
+			'OTHER'   => 'it_IT.utf-8',
+		),
+		'ru' => array(
+			'LINUX'   => 'ru_RU.utf-8',
+			'WINDOWS' => 'russian',
+			'OTHER'   => 'ru_RU.utf-8',
+		),
+		'zh' => array(
+			'LINUX'   => 'zh_CN.utf-8',
+			'WINDOWS' => 'chinese-simplified',
+			'OTHER'   => 'ru_RU.utf-8',
+		),
+	),
 
   /** @var string Blocked ips */
   'blocked_ips' =>  NULL, //default to null, comma separated ip-addresses to block
