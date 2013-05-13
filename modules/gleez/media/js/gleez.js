739cbc3271ee559fa83d6a0fa461970c4b895a82
Gleez Technologies <github@gleez.tech>
2013-05-13 15:02:53 +0530

    Update feature: Added some js functions

--- modules/gleez/media/js/gleez.js
+++ modules/gleez/media/js/gleez.js
@@ -365,13 +365,220 @@ jQuery.noConflict();
             });
         }
     };
+
+    // Take any "inform" messages out of an ajax response and display them on the screen.
+    Gleez.inform = function(response) {
+	
+        if (!response)  return false;
+        
+        if (!response.InformMessages || response.InformMessages.length == 0)
+            return false;
+	
+	// If there is no message container in the page, add one
+	var informMessages = $('div.messages');
+	if (informMessages.length == 0)
+        {
+	    $('<div class="messages"></div>').appendTo('body');
+	    informMessages = $('div.messages');
+	}
+	
+        var wrappers = $('div.messages div.InformWrapper');
+	
+        // Loop through the inform messages and add them to the container
+	for (var i = 0; i < response.InformMessages.length; i++)
+        {
+	    var css = 'alert';
+	    if (response.InformMessages[i]['type'])
+		css += ' alert-' + response.InformMessages[i]['type'];
+
+	    try
+            {
+                var message = response.InformMessages[i]['text'];
+                var emptyMessage = message == '';
+                var skip = false;
+            
+                for (var j = 0; j < wrappers.length; j++)
+                {
+                    if ($(wrappers[j]).text() == $(message).text()) {
+                        skip = true;
+                    }
+                }
+                
+                if (!skip && !emptyMessage)
+                {
+                    // If the message is dismissable, add a close button
+                    message = '<a class="close" data-dismiss="alert">×</a>' + message;
+                    informMessages.prepend('<div class="'+css+'">'+message+'</div>');
+                }
+                
+            } catch (e) {}
+	}
+        
+	informMessages.show();
+        
+        return true;
+    }
+
+    // Inform an error returned from an ajax call.
+    Gleez.informError = function(xhr, silentAbort, returnRes) {
+	if (xhr == undefined || xhr == null)
+	    return;
+       
+	if (typeof(xhr) == 'string')
+	    xhr = {responseText: xhr, code: 500};
+       
+	var message = xhr.responseText;
+	var code = xhr.status;
+       
+	if (message == undefined || message == null || message == '') {
+	    switch (xhr.statusText) {
+		case 'error':
+		   if (silentAbort) 
+		      return;
+		   message = Gleez.t('There was an error performing your request. Please try again.');
+		   break;
+		case 'timeout':
+		   message = Gleez.t('Your request timed out. Please try again.');
+		   break;
+		case 'abort':
+		   return;
+	    }
+	}
+       
+	try
+	{
+	    var data = $.parseJSON(message);
+	    if (typeof(data.Exception) == 'string')
+		message = data.Exception;
+	} catch(e) {}
+       
+	if (message == '')
+	    message = Gleez.t('There was an error performing your request. Please try again.');
+       
+	if (returnRes) return {responseText: message, code: code};
+	
+	Gleez.informMessage('<span class="InformSprite Lightbulb Error'+code+'"></span>'+message);
+    }
+    
+    // Send an informMessage to the screen.
+    Gleez.informMessage = function(message, options) {
+	if (!options)   options = new Array();
+	
+	options['text'] = message;
+	Gleez.inform({'InformMessages' : new Array(options)});
+    }
+    
+    var keyString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var uTF8Encode = function(string) {
+            string = string.replace(/\x0d\x0a/g, "\x0a");
+            var output = "";
+            for (var n = 0; n < string.length; n++) {
+                    var c = string.charCodeAt(n);
+                    if (c < 128) {
+                            output += String.fromCharCode(c);
+                    } else if ((c > 127) && (c < 2048)) {
+                            output += String.fromCharCode((c >> 6) | 192);
+                            output += String.fromCharCode((c & 63) | 128);
+                    } else {
+                            output += String.fromCharCode((c >> 12) | 224);
+                            output += String.fromCharCode(((c >> 6) & 63) | 128);
+                            output += String.fromCharCode((c & 63) | 128);
+                    }
+            }
+            return output;
+    };
+    
+    var uTF8Decode = function(input) {
+            var string = "";
+            var i = 0;
+            var c = 0, c1 = 0, c2 = 0, c3 = 0;
+            while ( i < input.length ) {
+                    c = input.charCodeAt(i);
+                    if (c < 128) {
+                            string += String.fromCharCode(c);
+                            i++;
+                    } else if ((c > 191) && (c < 224)) {
+                            c2 = input.charCodeAt(i+1);
+                            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                            i += 2;
+                    } else {
+                            c2 = input.charCodeAt(i+1);
+                            c3 = input.charCodeAt(i+2);
+                            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                            i += 3;
+                    }
+            }
+            return string;
+    }
+    
+    $.extend({
+            base64Encode: function(input) {
+                    var output = "";
+                    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+                    var i = 0;
+                    input = uTF8Encode(input);
+                    while (i < input.length) {
+                            chr1 = input.charCodeAt(i++);
+                            chr2 = input.charCodeAt(i++);
+                            chr3 = input.charCodeAt(i++);
+                            enc1 = chr1 >> 2;
+                            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                            enc4 = chr3 & 63;
+                            if (isNaN(chr2)) {
+                                    enc3 = enc4 = 64;
+                            } else if (isNaN(chr3)) {
+                                    enc4 = 64;
+                            }
+                            output = output + keyString.charAt(enc1) + keyString.charAt(enc2) + keyString.charAt(enc3) + keyString.charAt(enc4);
+                    }
+                    return output;
+            },
+            base64Decode: function(input) {
+                    var output = "";
+                    var chr1, chr2, chr3;
+                    var enc1, enc2, enc3, enc4;
+                    var i = 0;
+                    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+                    while (i < input.length) {
+                            enc1 = keyString.indexOf(input.charAt(i++));
+                            enc2 = keyString.indexOf(input.charAt(i++));
+                            enc3 = keyString.indexOf(input.charAt(i++));
+                            enc4 = keyString.indexOf(input.charAt(i++));
+                            chr1 = (enc1 << 2) | (enc2 >> 4);
+                            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                            chr3 = ((enc3 & 3) << 6) | enc4;
+                            output = output + String.fromCharCode(chr1);
+                            if (enc3 != 64) {
+                                    output = output + String.fromCharCode(chr2);
+                            }
+                            if (enc4 != 64) {
+                                    output = output + String.fromCharCode(chr3);
+                            }
+                    }
+                    output = uTF8Decode(output);
+                    return output;
+            }
+    });
+    
+    $.postParseJson = function(json) {
+	if (json.Data) json.Data = $.base64Decode(json.Data);
+	return json;
+    }
+
+    /**
+     * Filter Jquery selector by attribute value
+     **/
+    $.fn.filterAttr = function(attr_name, attr_value) {
+	return this.filter(function() { return $(this).attr(attr_name) === attr_value; });
+    };
     
     Gleez.theme = function (func) {
-      for (var i = 1, args = []; i < arguments.length; i++) {
-        args.push(arguments[i]);
-      }
+	for (var i = 1, args = []; i < arguments.length; i++) {
+	    args.push(arguments[i]);
+	}
 
-      return (Gleez.theme[func] || Gleez.theme.prototype[func]).apply(this, args);
+	return (Gleez.theme[func] || Gleez.theme.prototype[func]).apply(this, args);
     };
 
     //Attach all behaviors.
