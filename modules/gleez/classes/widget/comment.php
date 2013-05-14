34527a9063cf053d379d2c44b18ef700a996a541
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-14 15:28:39 +0400

    Fixed issue: Cleanup Widget Classes

--- modules/gleez/classes/widget/comment.php
+++ modules/gleez/classes/widget/comment.php
@@ -1,53 +1,54 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-
+/**
+ * Comment Widget class
+ *
+ * @package    Gleez\Widget
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2011-2012 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
+ */
 class Widget_Comment extends Widget {
 
-	public function info()
-	{
-	}
-	public function form()
-	{
-	}
+	public function info(){}
+	public function form(){}
 
-	public function save( array $post )
-	{
-	}
+	public function save(array $post){}
 
-	public function delete( array $post )
-	{
-	}
+	public function delete(array $post){}
 	
 	public function render()
 	{
 		switch($this->name)
-                {
-                        case 'recent':
-                                return $this->recent($this->widget);
-                        break;
-                        default:
-                                return;
-                }
+		{
+			case 'recent':
+				return $this->recent($this->widget);
+			break;
+			default:
+				return;
+		}
 	}
 
-        public function recent($widget)
-        {
-                // Dont show the widget on edit or delete actions.
-		if( Request::current()->action() == 'edit' OR Request::current()->action() == 'delete' )
-			return false;
+	public function recent($widget)
+	{
+		// Don't show the widget on edit or delete actions.
+		if (Request::current()->action() == 'edit' OR Request::current()->action() == 'delete')
+		{
+			return FALSE;
+		}
 
 		$cache = Cache::instance('widgets');
 	
-		if( ! $comments = $cache->get('recent_comments') )
+		if ( ! $comments = $cache->get('recent_comments'))
 		{
 			$blogs = ORM::factory('comment')
 					->join('posts')
-						->on('posts.id', '=', 'comment.post_id')
+					->on('posts.id', '=', 'comment.post_id')
 					->where('comment.status', '=', 'publish')
 					->where('posts.status', '=', 'publish')
 					->order_by('comment.created', 'DESC')
 					->limit(10)
 					->find_all();
-	
+
 			$comments = array();
 			foreach($blogs as $blog)
 			{
@@ -55,14 +56,15 @@ class Widget_Comment extends Widget {
 				$comments[$blog->id]['type'] = $blog->type;
 				$comments[$blog->id]['title'] = $blog->title;
 				$comments[$blog->id]['post_url'] = $blog->post->url;
-				//$comments[$blog->id] = (object)$blog->as_array();
 			}
 
-			//set the cache
+			// set the cache
 			$cache->set('recent_comments', $comments, DATE::HOUR);
 		}
 	
-		return View::factory('widgets/comment/list')->set('comments', $comments)->render();
+		return View::factory('widgets/comment/list')
+					->set('comments', $comments)
+					->render();
         }
 
 }
