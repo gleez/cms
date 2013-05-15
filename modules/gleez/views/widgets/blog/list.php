85867c5497e357054e482e5a5c167f375d8d148e
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-15 16:32:05 +0400

    Fixed issue: Cleanup Recent Blog View

--- modules/gleez/views/widgets/blog/list.php
+++ modules/gleez/views/widgets/blog/list.php
@@ -1,9 +1,9 @@
 <?php defined("SYSPATH") OR die("No direct script access."); ?>
 
-<?php if (isset($items) AND ! empty($items)): ?>
+<?php if (isset($items)): ?>
 	<ul class="blogs-list">
-		<?php foreach($items as $i => $item) : ?>
-			<li class="widget-title" id='widget-title-'.$item['id']>
+		<?php foreach($items as $item) : ?>
+			<li class="widget-title" id='widget-title-'<?php echo $item['id'] ?>>
 				<?php echo HTML::anchor($item['url'], $item['title']) ?>
 			</li>
 		<?php endforeach; ?>
