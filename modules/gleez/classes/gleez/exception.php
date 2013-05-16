5f72a86e28f21e43944f435a9b9c9fe32dbbb7e9
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-16 07:26:37 +0400

    Fixed issue: Fixed PHPDoc

--- modules/gleez/classes/gleez/exception.php
+++ modules/gleez/classes/gleez/exception.php
@@ -1,10 +1,10 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 /**
- * Gleez exception class
+ * Gleez Exception Class
  *
  * @package    Gleez\Exceptions
  * @author     Sandeep Sangamreddi - Gleez
- * @copyright  (c) 2012 Gleez Technologies
+ * @copyright  (c) 2011-2013 Gleez Technologies
  * @license    http://gleezcms.org/license  Gleez CMS License
  */
 class Gleez_Exception extends Kohana_Kohana_Exception {}
