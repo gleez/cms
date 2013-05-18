958b3619578683a3c8f696169f2da0b5c8c907f6
Sergey Yakovlev <sadhooklay@gmail.com>
2013-05-18 14:57:01 +0400

    Fixed issue: Cleanup whitespaces

--- modules/gleez/classes/controller/admin/dashboard.php
+++ modules/gleez/classes/controller/admin/dashboard.php
@@ -2,20 +2,21 @@
 /**
  * Admin Dashboard Controller
  *
- * @package   Gleez\Admin\Controller
- * @author    Sandeep Sangamreddi - Gleez
- * @copyright (c) 2011-2013 Gleez Technologies
- * @license   http://gleezcms.org/license
+ * @package    Gleez\Admin\Controller
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2011-2013 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
  */
 class Controller_Admin_Dashboard extends Controller_Admin {
 
-  public function action_index()
-  {
-    $this->title = __('Administer');
-    $view = View::factory('admin/dashboard')
-          ->set('widgets', Widgets::instance()->render('dashboard'));
+	public function action_index()
+	{
+		$this->title = __('Administer');
 
-    $this->response->body($view);
-  }
+		$view = View::factory('admin/dashboard')
+			->set('widgets', Widgets::instance()->render('dashboard'));
+
+		$this->response->body($view);
+	}
 
 }
