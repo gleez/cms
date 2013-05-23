6743018482d722568939da6760dcb2641e9fc521
Gleez Technologies <gleez@server.gleeztech.org>
2013-05-23 17:18:43 +0530

    New feature: Added some basic test cases

---
+++ modules/gleez/tests/UserTest.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') OR die('Kohana bootstrap needs to be included before tests run');
+
+/**
+ * Tests the Config lib that's shipped with kohana
+ *
+ * @group Gleez
+ * @group Gleez.core
+ * @group Gleez.core.user
+ *
+ */
+class Gleez_Usertest extends Unittest_TestCase
+{
+	public function providerUsers()
+	{
+		return array(
+			array(array("name" => "admin", "password" => "gleez1co")),
+			array(array("name" => "sundar", "password" => "gleez1co"))
+		);
+	}
+	/**
+	 * @dataProvider providerUsers
+	 */
+	public function testValidUsers($info)
+	{
+		$user = ORM::factory('user');
+		$result = $user->login($info);
+		$this->assertInstanceOf('Model_user', $result);
+	}
+	
+	/**
+	 * @expectedException Validation_Exception
+	 */
+	public function testInvalidUsers()
+	{
+		$user = ORM::factory('user');
+		$user->login(array('name' => 'sundar1', 'password' => 'gleez1co'));
+	}
+}
