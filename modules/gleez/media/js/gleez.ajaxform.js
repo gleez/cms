2aeabcbd471dc588c457c747cdd368530e9804e2
Gleez Technologies <gleez@server.gleeztech.org>
2013-05-30 19:44:37 +0530

    Update Feature: Rewritten post in place and popup libraries as bootstrap plugins
    This is WIP rewriten the jquery libraries as bootstrap plugins for popup dialog and ajaxform. These libraries talk to eachother to handle events from form to popup etc. Tested to work with IE7 as well, still some css fixes for mobile devices.

---
+++ modules/gleez/media/js/gleez.ajaxform.js
@@ -0,0 +1,298 @@
+/*
+ * This turns any form into a "post-in-place" form so it is ajaxed to save
+ * without a refresh. Requires jquery form plugin @link
+ * 
+ * @package    	Gleez\AjaxForm
+ * @version    	1.0
+ * @requires   	jQuery v1.8 or later
+ * @author     	Sandeep Sangamreddi - Gleez
+ * @copyright  	(c) 2011-2013 Gleez Technologies
+ * @license    	http://gleezcms.org/license Gleez CMS License
+ * @link	https://github.com/malsup/form
+ */
+
+!function ($) { "use strict";
+
+    // AJAXFROM CLASS DEFINITION
+    // ======================
+
+    var Ajaxform = function (element, options) {
+		//Set the options
+		options.dataType        = options.datatype
+		options.beforeSerialize = options.beforeserialize
+		options.beforeSubmit    = options.beforesubmit || Ajaxform.prototype.beforeSubmit
+		options.success       	= options.success || Ajaxform.prototype.showResponse
+		options.error       	= options.error || Ajaxform.prototype.errorResponse
+		options.resetForm       = options.resetform
+		options.clearForm       = options.clearform
+		options.closeKeepAlive  = options.closekeepalive
+		options.extraData       = options.extradata
+		options.replaceTarget   = options.replacetarget
+		options.includeHidden   = options.includehidden
+		options.uploadProgress  = options.uploadprogress
+
+		//delete unsed options
+		delete options.datatype
+		delete options.beforeserialize
+		delete options.beforesubmit
+		delete options.resetform
+		delete options.clearform
+		delete options.closekeepalive
+		delete options.extradata
+		delete options.replacetarget
+		delete options.includehidden
+		delete options.uploadprogress
+
+		this.init(element, options)
+    }
+
+    Ajaxform.prototype.init = function(element, options) {
+		$(element).ajaxSubmit(options).removeData('jqxhr')
+    }
+    
+    Ajaxform.prototype.beforeSubmit = function(formData, form, options) { 
+		// Hide any errorContainers
+		$(form).find('.error-message-container').slideUp(250)
+		$(form).find('[type=submit]').attr('disabled', true).addClass('InProgress1')
+		return true; 
+    }
+
+    Ajaxform.prototype.showResponse = function(data, status, xhr, form) {
+		if (data.FormSaved == false && data.errors){
+			Ajaxform.prototype.validationErrors(data, form);	
+		}
+		else if (data.FormSaved == true){
+			//Lets check if the form is in popup window
+			var popup = $(form).find('[type=submit]').data('popup')
+			$(form).remove()
+
+			if(popup){
+				$(popup).find('.popup-body').html('Success')
+			}
+			
+		}
+    }
+
+    Ajaxform.prototype.errorResponse = function(xhr, status, error, form) {
+		console.log('Error Response')
+		console.log(error)
+    }
+
+    Ajaxform.prototype.validationErrors = function(data, form) {
+		var title = 'Error'
+		  , tmpl = '<div class="alert alert-error alert-block">'
+		
+		tmpl += '<h4 class="alert-heading">' + title + '</h4><ul>';
+			// Loop through the errors 
+			$.each(data.errors, function(i, value) {
+			// And add the error to the list.
+			tmpl += '<li>' + value + '</li>';
+			// Let's guesstimate the input that gave us an error
+			var $inputField = $('[name*="'+i+'"]')
+			
+			if ($inputField.length){
+				$($inputField).parent('div.controls').parent('div.control-group').addClass('error')
+			}
+			});					
+		tmpl += '</ul></div>';
+		
+		// If the target block doesn't exist..
+		if (!$('.error-message-container').length){
+			$(form).prepend('<div class="error-message-container" style="display:none"></div>')
+		}
+		 
+		// Empty any previous error messages, insert the new errors and slide it in to view.
+		$(form).find('.error-message-container').empty().html(tmpl).slideDown(250)
+		$(form).find('[type=submit]').removeAttr('disabled')
+    }
+    
+    Ajaxform.prototype.processData = function(data, $el) {
+	if (data.location) {
+	    window.location.href = data.location
+	}
+	else {
+	    var replace_selector = $el.attr('data-replace')
+	      , replace_closest_selector = $el.attr('data-replace-closest')
+	      , replace_inner_selector = $el.attr('data-replace-inner')
+	      , replace_closest_inner_selector = $el.attr('data-replace-closest-inner')
+	      , append_selector = $el.attr('data-append')
+	      , prepend_selector = $el.attr('data-prepend')
+	      , refresh_selector = $el.attr('data-refresh')
+	      , refresh_closest_selector = $el.attr('data-refresh-closest')
+	      , clear_selector = $el.attr('data-clear')
+	      , remove_selector = $el.attr('data-remove')
+	      , clear_closest_selector = $el.attr('data-clear-closest')
+	      , remove_closest_selector = $el.attr('data-remove-closest')
+	    
+	    if (replace_selector) {
+		$(replace_selector).replaceWith(data.html)
+	    }
+	    if (replace_closest_selector) {
+		$el.closest(replace_closest_selector).replaceWith(data.html)
+	    }
+	    if (replace_inner_selector) {
+		$(replace_inner_selector).html(data.html)
+	    }
+	    if (replace_closest_inner_selector) {
+		$el.closest(replace_closest_inner_selector).html(data.html)
+	    }
+	    if (append_selector) {
+		$(append_selector).append(data.html)
+	    }
+	    if (prepend_selector) {
+		$(prepend_selector).prepend(data.html)
+	    }
+	    if (refresh_selector) {
+		$.each($(refresh_selector), function(index, value) {
+		    $.getJSON($(value).data('refresh-url'), function(data) {
+			$(value).replaceWith(data.html)
+		    })
+		})
+	    }
+	    if (refresh_closest_selector) {
+		$.each($(refresh_closest_selector), function(index, value) {
+		    $.getJSON($(value).data('refresh-url'), function(data) {
+			$el.closest($(value)).replaceWith(data.html)
+		    })
+		})
+	    }
+	    if (clear_selector) {
+		$(clear_selector).html('')
+	    }
+	    if (remove_selector) {
+		$(remove_selector).remove()
+	    }
+	    if (clear_closest_selector) {
+		$el.closest(clear_closest_selector).html('')
+	    }
+	    if (remove_closest_selector) {
+		$el.closest(remove_closest_selector).remove()
+	    }
+	}
+	
+	if (data.fragments) {
+	    for (var s in data.fragments) {
+		$(s).replaceWith(data.fragments[s])
+	    }
+	}
+	if (data['inner-fragments']) {
+	    for (var i in data['inner-fragments']) {
+		$(i).html(data['inner-fragments'][i])
+	    }
+	}
+	if (data['append-fragments']) {
+	    for (var a in data['append-fragments']) {
+		$(a).append(data['append-fragments'][a])
+	    }
+	}
+	if (data['prepend-fragments']) {
+	    for (var p in data['prepend-fragments']) {
+		$(p).prepend(data['prepend-fragments'][p])
+	    }
+	}
+	
+	$el.trigger('ajaxform.success', [data, $el]);
+    }
+    
+    Ajaxform.prototype.captureSubmittingElement = function(e) {
+	var target = e.target;
+	var $el = $(target);
+	if (!($el.is("[type=submit],[type=image]"))) {
+	    // is this a child element of the submit el?  (ex: a span within a button)
+	    var t = $el.closest('[type=submit]');
+	    if (t.length === 0) {
+		return;
+	    }
+	    target = t[0];
+	}
+	var form = this;
+	form.clk = target;
+	if (target.type == 'image') {
+	    if (e.offsetX !== undefined) {
+		form.clk_x = e.offsetX;
+		form.clk_y = e.offsetY;
+	    } else if (typeof $.fn.offset == 'function') {
+		var offset = $el.offset();
+		form.clk_x = e.pageX - offset.left;
+		form.clk_y = e.pageY - offset.top;
+	    } else {
+		form.clk_x = e.pageX - target.offsetLeft;
+		form.clk_y = e.pageY - target.offsetTop;
+	    }
+	}
+	// clear form vars
+	setTimeout(function() { form.clk = form.clk_x = form.clk_y = null; }, 100);
+    }
+    
+    // AJAXFROM PLUGIN DEFINITION
+    // =======================
+
+    var old = $.fn.aform
+
+    $.fn.aform = function (option) {
+	return this.each(function () {
+	    var $this   = $(this)
+	    var data    = $this.data('ajaxform')
+	    var options = $.extend({}, $.fn.aform.defaults, $this.data(), typeof option == 'object' && option)
+
+	    if (!data) $this.data('ajaxform', (data = new Ajaxform(this, options)))
+	    if (typeof option == 'string') data[option]()
+	})
+    }
+
+    $.fn.aform.defaults = {
+	keyboard: true
+      , loading: true
+      , delegation: true
+      , datatype: 'json'
+      , type: 'POST'
+      , beforeserialize: false
+      , beforesubmit: false
+      , resetform: false
+      , clearform: false
+      , target: false
+      , success: false
+      , context: false
+      , error: false
+      , complete: false
+      , traditional: false
+      , iframe: false
+      , semantic: false
+      , closekeepalive: false
+      , extradata: false
+      , replacetarget:'html'
+      , includehidden: true
+      , uploadprogress: false
+    }
+    
+    $.fn.aform.Constructor = Ajaxform
+
+
+    // AJAXFROM NO CONFLICT
+    // =================
+
+    $.fn.aform.noConflict = function () {
+	$.fn.aform = old
+	return this
+    }
+
+
+   // AJAXFROM DATA-API
+   // ==============
+
+    $(document).on('submit.ajaxform.data-api, click.ajaxform.data-api', '[data-toggle="ajaxform"]', function (e) {
+	var $this = $(this)
+	, $target = $this.parents('form')
+	, option  = $.extend({}, $target.data(), $this.data())
+	
+	// if event has been canceled, don't proceed
+	if (!e.isDefaultPrevented()) {
+	    e.preventDefault();
+	    $target.removeData('ajaxform').aform(option)
+	}
+    })
+    
+    $(document.body).on('submit.ajaxform.data-api', 'form', Ajaxform.prototype.captureSubmittingElement)
+    $(document.body).on('click.ajaxform.data-api', 'form', Ajaxform.prototype.captureSubmittingElement)
+    
+}(window.jQuery);
---
+++ modules/gleez/media/js/gleez.popup.js
@@ -0,0 +1,458 @@
+/*
+ * This is a highly modified version of the bootstrap popup dialog.
+ * 
+ * @package    Gleez\Popup
+ * @version    1.0
+ * @requires   jQuery v1.8 or later
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2011-2013 Gleez Technologies
+ * @license    http://gleezcms.org/license Gleez CMS License
+ *
+*/
+
+!function ($) { "use strict";
+
+	// POPUP CLASS DEFINITION
+	// ======================
+
+	var Popup = function (element, options) {
+		this.init(element, options);
+	}
+	
+	Popup.prototype.init = function (element, options) {
+		this.options   = options
+		this.$element  = $(element).delegate('[data-dismiss="popup"]', 'click.dismiss.popup', $.proxy(this.hide, this))
+		this.$backdrop =
+		this.isShown   = null
+
+		this.options.loading && this.loading()
+		this.local()
+		this.remote()
+	}
+
+	Popup.prototype.local = function () {
+		//not implemented yet
+	}
+	
+	Popup.prototype.remote = function () {
+		if (this.options.remote){
+			var that = this
+		
+			//do the ajax call
+			$.ajax({
+				url: this.options.remote,
+				type: "GET",
+				cache: this.options.cache
+			}, 300)
+			.done(function(data, textStatus, jqXHR){
+				that.reveal(data, that)
+			})
+			.fail(function (jqXHR, textStatus, errorThrown) {
+				that.$element.find('.popup-title').text(textStatus)
+				that.$element.find('.popup-body').text(errorThrown)
+			})
+		}
+	}
+	
+	Popup.prototype.reveal = function (data, popup) {
+		var json = false
+		
+		// First see if we've retrieved json or something else
+		try {
+			json = $.parseJSON(data);
+		} catch (e) {
+			json = false;
+		}
+	
+		if (json) {
+			data = json['Data'];
+		}
+	
+		var $data = $($.parseHTML(data))
+	
+		// Now, if there are any forms in the popup, hijack them if necessary.
+		// Pass the popup object to the form as data() to handle popup events from form
+		if (data && this.options.consumeform) {
+			$data.find('[type=submit]')
+				 .attr('data-toggle', 'ajaxform')
+				 .data('popup', popup.$element)
+		}
+	
+		// Prevent blank popups
+		if (data) {
+			this.$element.find('.popup-title').html(this.options.title)
+			this.$element.find('.popup-body').html($data)
+		}
+		
+		// Trigger an event then plugins can attach to when popups are revealed.
+		this.$element.trigger('reveal.popup')
+	}
+
+	Popup.prototype.tab = function () {
+		var that = this;
+
+		if (this.isShown && this.options.consumetab) {
+			this.$element.on('keydown.tabindex.popup', '[data-tabindex]', function (e) {
+				if (e.keyCode && e.keyCode == 9){
+					var $next = $(this),
+						$rollover = $(this);
+
+					that.$element.find('[data-tabindex]:enabled:not([readonly])').each(function (e) {
+						if (!e.shiftKey){
+							$next = $next.data('tabindex') < $(this).data('tabindex') ?
+								$next = $(this) :
+								$rollover = $(this);
+						} else {
+							$next = $next.data('tabindex') > $(this).data('tabindex') ?
+								$next = $(this) :
+								$rollover = $(this);
+						}
+					});
+
+					$next[0] !== $(this)[0] ?
+						$next.focus() : $rollover.focus();
+
+					e.preventDefault();
+				}
+			});
+		} else if (!this.isShown) {
+			this.$element.off('keydown.tabindex.popup');
+		}
+	}
+	
+	Popup.prototype.removeLoading = function () {
+		this.$loading.remove();
+		this.$loading = null;
+		this.isLoading = false;
+	}
+	
+	Popup.prototype.loading = function (callback) {
+		callback = callback || function () {};
+		
+		var animate = this.$element.hasClass('fade') ? 'fade' : '';
+		
+		if (!this.isLoading) {
+			var doAnimate = $.support.transition && animate;
+
+			this.$loading = $('<div class="loading-spinner ' + animate + '">')
+				.append(this.options.spinner)
+				.appendTo(document.body);
+
+			if (doAnimate) this.$loading[0].offsetWidth; // force reflow
+
+			this.$loading.addClass('in');
+
+			this.isLoading = true;
+
+			doAnimate ?
+				this.$loading.one($.support.transition.end, callback) :
+				callback();
+
+		} else if (this.isLoading && this.$loading) {
+			this.$loading.removeClass('in');
+
+			var that = this;
+			$.support.transition && this.$element.hasClass('fade')?
+				this.$loading.one($.support.transition.end, function () { that.removeLoading() }) :
+				this.removeLoading();
+
+		} else if (callback) {
+			callback(this.isLoading);
+		}
+	}
+
+	Popup.prototype.layout = function () {
+		var prop = this.options.height ? 'height' : 'max-height',
+			value = this.options.height || this.options.maxHeight;
+
+		if (this.options.width){
+			this.$element.css('width', this.options.width);
+
+			var that = this;
+			this.$element.css('margin-left', function () {
+				if (/%/ig.test(that.options.width)){
+					return -(parseInt(that.options.width) / 2) + '%';
+				} else {
+					return -($(this).width() / 2) + 'px';
+				}
+			});
+		} else {
+			this.$element.css('width', '');
+			this.$element.css('margin-left', '');
+		}
+
+		this.$element.find('.popup-body')
+			.css('overflow', '')
+			.css(prop, '');
+
+		if (value){
+			this.$element.find('.popup-body')
+				.css('overflow', 'auto')
+				.css(prop, value);
+		}
+
+		var modalOverflow = $(window).height() - 10 < this.$element.height();
+		
+		if (modalOverflow || this.options.modaloverflow) {
+			this.$element
+				.css('margin-top', 0)
+				.addClass('popup-overflow');
+		} else {
+			this.$element
+				.css('margin-top', 0 - this.$element.height() / 2)
+				.removeClass('popup-overflow');
+		}
+	}
+	
+	Popup.prototype.toggle = function () {
+		return this[!this.isShown ? 'show' : 'hide']()
+	}
+
+	Popup.prototype.show = function () {
+		var that = this
+		var e    = $.Event('show.popup')
+	
+		this.$element.trigger(e)
+	
+		if (this.isShown || e.isDefaultPrevented()) return
+	
+		this.isShown = true
+	
+		this.keys()
+	
+		this.tab()
+	
+		this.options.loading && this.loading()
+	
+		this.backdrop(function () {
+		  var transition = $.support.transition && that.$element.hasClass('fade')
+	
+		  if (!that.$element.parent().length) {
+			//that.layout()
+			that.$element.appendTo(that.options.manager) //don't move modals dom position
+		  }
+	
+		  that.$element.show()
+	
+		  if (transition) {
+			that.$element[0].offsetWidth // force reflow
+		  }
+	
+		  that.$element
+			.addClass('in')
+			.attr('aria-hidden', false)
+	
+		  that.enforceFocus()
+	
+		  transition ?
+			that.$element.one($.support.transition.end, function () { that.$element.focus().trigger('shown.popup') }) :
+			that.$element.focus().trigger('shown.popup')
+		})
+	}
+
+	Popup.prototype.hide = function (e) {
+		if (e) e.preventDefault()
+	
+		e = $.Event('hide.popup')
+	
+		this.$element.trigger(e)
+	
+		if (!this.isShown || e.isDefaultPrevented()) return
+	
+		this.isShown = false
+	
+		this.keys()
+	
+		this.tab()
+	
+		this.isLoading && this.loading();
+	
+		$(document).off('focusin.popup')
+	
+		this.$element
+		  .removeClass('in')
+		  .attr('aria-hidden', true)
+	
+		$.support.transition && this.$element.hasClass('fade') ?
+		  this.hideWithTransition() :
+		  this.hidePopup()
+	}
+
+	Popup.prototype.enforceFocus = function () {
+		var that = this
+		$(document).on('focusin.popup', function (e) {
+			if (that.$element[0] !== e.target && !that.$element.has(e.target).length) {
+				that.$element.focus()
+			}
+		})
+	}
+
+	Popup.prototype.keys = function () {
+		var that = this;
+		if (this.isShown && this.options.keyboard) {
+			if (!this.$element.attr('tabindex')) this.$element.attr('tabindex', -1);
+			
+			this.$element.on('keyup.dismiss.popup', function (e) {
+				e.which == 27 && that.hide();
+			});
+			
+		} else if (!this.isShown) {
+			this.$element.off('keyup.dismiss.popup')
+		}
+	}
+	
+	Popup.prototype.hideWithTransition = function () {
+		var that    = this
+		var timeout = setTimeout(function () {
+		  that.$element.off($.support.transition.end)
+		  that.hidePopup()
+		}, 500)
+	
+		this.$element.one($.support.transition.end, function () {
+		  clearTimeout(timeout)
+		  that.hidePopup()
+		})
+	}
+
+	Popup.prototype.hidePopup = function () {
+		var that = this
+		var prop = this.options.height ? 'height' : 'max-height';
+		var value = this.options.height || this.options.maxHeight;
+
+		if (value){
+			this.$element.find('.popup-body')
+				.css('overflow', '')
+				.css(prop, '');
+		}
+		
+		this.$element.hide()
+		this.backdrop(function () {
+			that.removeBackdrop()
+			that.$element.trigger('hidden.popup')
+			
+			//destroy the popup for remote modals
+			if(that.options.remote) that.$element.remove()
+		})
+	}
+
+	Popup.prototype.removeBackdrop = function () {
+		this.$backdrop && this.$backdrop.remove()
+		this.$backdrop = null
+	}
+
+	Popup.prototype.backdrop = function (callback) {
+		var that    = this
+		var animate = this.$element.hasClass('fade') ? 'fade' : ''
+	
+		if (this.isShown && this.options.backdrop) {
+			var doAnimate = $.support.transition && animate
+	  
+			this.$backdrop = $('<div class="popup-backdrop ' + animate + '" />')
+			  .appendTo(document.body)
+	  
+			this.$backdrop.click(
+			  this.options.backdrop == 'static' ?
+				$.proxy(this.$element[0].focus, this.$element[0])
+			  : $.proxy(this.hide, this)
+			)
+	  
+			if (doAnimate) this.$backdrop[0].offsetWidth // force reflow
+	  
+			this.$backdrop.addClass('in')
+	  
+			if (!callback) return
+	  
+			doAnimate ?
+			  this.$backdrop.one($.support.transition.end, callback) :
+			  callback()
+	
+		} else if (!this.isShown && this.$backdrop) {
+			this.$backdrop.removeClass('in')
+	  
+			$.support.transition && this.$element.hasClass('fade')?
+			  this.$backdrop.one($.support.transition.end, callback) :
+			  callback()
+	
+		} else if (callback) {
+			callback()
+		}
+	}
+
+
+   // POPUP PLUGIN DEFINITION
+   // =======================
+
+	var old = $.fn.popup
+  
+	$.fn.popup = function (option) {
+		return this.each(function () {
+			var $this   = $(this)
+			var data    = $this.data('popup')
+			var options = $.extend({}, $.fn.popup.defaults, $this.data(), typeof option == 'object' && option)
+	  
+			if (!data) $this.data('popup', (data = new Popup(this, options)))
+			if (typeof option == 'string') data[option]()
+			else if (options.show) data.show()
+		})
+	}
+
+	$.fn.popup.defaults = {
+	  backdrop: true
+	  , keyboard: true
+	  , loading: true
+	  , show: true
+	  , cache: false
+	  , width : false
+	  , height : false
+	  , minWidth : 100
+	  , minHeight : 100
+	  , maxWidth : 9999
+	  , maxHeight : 9999
+	  , modaloverflow: true
+	  , consumetab: true
+	  , consumeform: true
+	  , focusOn: false
+	  , replace: false
+	  , resize: false
+	  , manager: 'body'
+	  , icon: false
+	  , title: '&nbsp;'
+	  , spinner:  '<i class="icon-spinner icon-spin icon-2x"></i>'
+	  , template: '<div id="{popup.id}" class="popup fade" tabIndex="-1" role="dialog"><div class="popup-dialog"><div class="popup-content"><div class="popup-header"><button type="button" class="close" data-dismiss="popup">&times;</button><h4 class="popup-title">&nbsp;</h4></div><div class="popup-body"></div><div class="popup-footer"></div></div></div></div>'
+	}
+	
+	$.fn.popup.Constructor = Popup
+
+
+   // POPUP NO CONFLICT
+   // =================
+
+	$.fn.popup.noConflict = function () {
+		$.fn.popup = old
+		return this
+	}
+
+
+   // POPUP DATA-API
+   // ==============
+
+	$(document).on('click.popup.data-api', '[data-toggle="popup"]', function (e) {
+		var $this   = $(this)
+		var href    = $this.attr('href')
+		var $target = $($.fn.popup.defaults.template.replace('{popup.id}', 'popup-'+ new Date().getTime()))
+		var option  = $.extend({ remote:!/#/.test(href) && href }, $target.data(), $this.data())
+	
+		e.preventDefault()
+
+		$target
+		  .popup(option)
+		  .one('hide', function () {
+			$this.focus()
+		})
+    })
+
+    var $body = $(document.body)
+      .on('shown.popup',  '.popup', function () { $body.addClass('popup-open') })
+      .on('hidden.popup', '.popup', function () { $body.removeClass('popup-open') })
+
+}(window.jQuery);
