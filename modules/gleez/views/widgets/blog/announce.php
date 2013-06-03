335d06bad2abfab821b85aa48fdf4b609177a63e
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-03 18:44:14 +0400

    Fixed issue: Cleanup widget view for announce of recent blogs

--- modules/gleez/views/widgets/blog/announce.php
+++ modules/gleez/views/widgets/blog/announce.php
@@ -5,9 +5,7 @@
 		<div class="recent-announce-wrapper" itemscope itemtype="http://schema.org/CreativeWork">
 			<?php foreach($items as $item) : ?>
 				<div class="announce-blog">
-					<div class="image">
-						<?php echo ( ! empty($item['image'])) ? HTML::resize($item['image'], array('alt' => $item['title'], 'height' => 140, 'width' => 180, 'type' => 'resize', 'itemprop' => 'image')) : '<div class="empty-photo"><i class="icon-camera-retro icon-2x"></i></div>'; ?>
-					</div>
+					<div class="image"><?php echo $item['image']; ?></div>
 					<h3 itemprop='url'><?php echo HTML::anchor($item['url'], $item['title'], array('itemprop' => 'url')) ?></h3>
 				</div>
 			<?php endforeach; ?>
