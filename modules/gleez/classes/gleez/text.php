25924fdc35fecc7c22f951695a8228ca4a7258ff
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-06-15 03:53:15 +0530

    Update feature: Its wise use as object instead as static for domdocumentload in text

--- modules/gleez/classes/gleez/text.php
+++ modules/gleez/classes/gleez/text.php
@@ -768,12 +768,14 @@ class Gleez_Text {
 	 */
 	static function dom_load($text)
 	{
+		$dom = new DOMDocument;
 		// Ignore warnings during HTML soup loading.
-		$dom_document = @DOMDocument::loadHTML('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+		@$dom->loadHTML('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 				"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html
 				xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type"
 				content="text/html; charset=utf-8" /></head><body>' . $text . '</body></html>');
-		return $dom_document;
+		
+		return $dom;
 	}
 
 	/**
