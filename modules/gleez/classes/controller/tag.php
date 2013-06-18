c3edcd94b858dc80c89f11d77c73ea4e2770d355
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-18 13:13:32 +0400

    Fixed issue: Cleanup Tag Controller

--- modules/gleez/classes/controller/tag.php
+++ modules/gleez/classes/controller/tag.php
@@ -9,49 +9,61 @@
  */
 class Controller_Tag extends Template {
 
+	/**
+	 * The before() method is called before controller action
+	 *
+	 * @uses    ACL::required
+	 */
 	public function before()
 	{
-		// Internal request only!
-		if ($this->request->is_initial())
-		{
-			throw new HTTP_Exception_404('Accessing an internal request <small>:type</small> externally', array(
-                                        ':type' => $this->request->uri(),
-                                ));
-		}
-		
-		ACL::Required('access content');
+		ACL::required('access content');
+
 		parent::before();
 	}
 	
 	public function action_list()
 	{
-        }
-        
-        public function action_view()
+		// @todo
+	}
+
+	/**
+	 * List of pages (blogs/posts/etc.) with a specific tag
+	 *
+	 * @throws  HTTP_Exception_404
+	 *
+	 * @uses    Log::add
+	 * @uses    Text::ucfirst
+	 * @uses    ACL::check
+	 * @uses    Meta::links
+	 * @uses    URL::canonical
+	 * @uses    Route::url
+	 */
+	public function action_view()
 	{
-                $id = (int) $this->request->param('id', 0);
-                $tag = ORM::factory('tag', $id);
+		$id = (int) $this->request->param('id', 0);
+		$tag = ORM::factory('tag', $id);
         
-                if( ! $tag->loaded() )
+		if( ! $tag->loaded())
 		{
 			Kohana::$log->add(LOG::ERROR, 'Attempt to access non-existent tag');
-			throw new HTTP_Exception_404( __('Tag ":tag" Not Found'), array(':tag'=>$id));
+			throw new HTTP_Exception_404(__('Tag :tag not found!'), array(':tag' => $id));
 		}
         
-        	$this->title    = __(':title', array(':title' => Text::ucfirst($tag->name) ) );
-		$view           = View::factory('tag/view')
-						->set('teaser', TRUE)
-						->bind('pagination', $pagination)
-						->bind('posts', $posts);
+		$this->title = __(':title', array(':title' => Text::ucfirst($tag->name)));
+
+		$view = View::factory('tag/view')
+				->set('teaser',      TRUE)
+				->bind('pagination', $pagination)
+				->bind('posts',      $posts);
         
-                $posts = $tag->posts;
+		$posts = $tag->posts;
                 
-                if(!ACL::check('administer tags') AND !ACL::check('administer content'))
+		if( ! ACL::check('administer tags') AND ! ACL::check('administer content'))
 		{
 			$posts->where('status', '=', 'publish');
 		}
                 
-                $total      = $posts->reset(FALSE)->count_all();
+		$total = $posts->reset(FALSE)->count_all();
 
 		if ($total == 0)
 		{
@@ -60,23 +72,25 @@ class Controller_Tag extends Template {
 			return;
 		}
         
-                $pagination = Pagination::factory(array(
-				'current_page'   => array('source'=>'cms', 'key'=>'page'),
-				'total_items' 	 => $total,
-				'items_per_page' => 15,
-				'uri'		 => $tag->url,
-				));
+		$pagination = Pagination::factory(array(
+			'current_page'   => array('source'=>'cms', 'key'=>'page'),
+			'total_items'    => $total,
+			'items_per_page' => 15,
+			'uri'            => $tag->url,
+		));
 		
-		$posts  = $posts->order_by('created', 'DESC')->limit($pagination->items_per_page)
-						->offset($pagination->offset)->find_all();
+		$posts = $posts->order_by('created', 'DESC')
+					->limit($pagination->items_per_page)
+					->offset($pagination->offset)
+					->find_all();
                 
-                $this->response->body($view);
+		$this->response->body($view);
 		
-		//Set the canocial and shortlink for search engines
-		if ( $this->auto_render === TRUE )
+		// Set the canonical and shortlink for search engines
+		if ($this->auto_render === TRUE)
 		{
-			Meta::links( URL::canonical($tag->url, $pagination), array('rel' => 'canonical'));
-			Meta::links( Route::url('tag', array('action' => 'view', 'id' => $tag->id) ), array('rel' => 'shortlink'));
+			Meta::links(URL::canonical($tag->url, $pagination), array('rel' => 'canonical'));
+			Meta::links(Route::url('tag', array('action' => 'view', 'id' => $tag->id)), array('rel' => 'shortlink'));
 		}
         }
         
