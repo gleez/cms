eb0bb7d28fafc531863022b2eee8c7961c5ccd22
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-20 08:36:45 +0400

    Fixed issue: Fixed comment view

--- modules/gleez/views/page/post.php
+++ modules/gleez/views/page/post.php
@@ -1,27 +1,38 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 
 <div class="post-page-wrapper">
-	<?php if ( isset($page) ): ?>
-		<div class="post-page"><?php echo $page; ?></div>
+	<?php if (isset($page)): ?>
+		<div class="post-page">
+			<?php echo $page; ?>
+		</div>
 	<?php endif;?>
 </div>
 
 <div class="list-comments">
-	<?php if ( isset($comments) ): ?>
-		<div class="post-comments"><?php echo $comments; ?></div>
+	<?php if (isset($comments)): ?>
+		<div class="post-comments">
+			<?php echo $comments; ?>
+		</div>
 	<?php endif;?>
 </div>
 
 <div class="post-comment-form-wrapper">
-	<?php if ( isset($provider_buttons) ): ?>
+	<?php if (isset($provider_buttons) AND ! isset($comment_form)): ?>
+		<p>
+			<?php
+			_e('Only authorized users can post comments. :register or login using one of these services:',
+				array(':register' => HTML::anchor(Route::get('user')->uri(array('action' => 'register')), __('Please register')))
+			);
+			?>
+		</p>
 		<div id="post-provider-buttons">
 			<?php echo $provider_buttons; ?>
-			<?php echo Form::textarea('comment', '', array('disabled' => true,
-								       'class' => 'textarea full', 'rows' => 5)) ?>
 		</div>
 	<?php endif;?>
 	
-	<?php if ( isset($comment_form) ): ?>
-		<div class="post-comment-form"><?php echo $comment_form; ?></div>
+	<?php if (isset($comment_form)): ?>
+		<div class="post-comment-form">
+			<?php echo $comment_form; ?>
+		</div>
 	<?php endif;?>
 </div>
