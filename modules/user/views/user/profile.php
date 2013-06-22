e02a9e37d9371abd83424c1be6e521f31f53c8b3
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-22 17:48:56 +0400

    Updated feature: Added ability for admins to edit users profiles

--- modules/user/views/user/profile.php
+++ modules/user/views/user/profile.php
@@ -12,7 +12,7 @@
 				<?php endif; ?>
 			</div>
 
-			<?php if ($is_owner): ?>
+			<?php if ($is_owner OR ACL::check('administer users')): ?>
 				<ul class="nav nav-list">
 					<li><?php echo HTML::anchor('user/photo', '<i class="icon-upload"></i>'.__('Change Avatar'), array('id' => 'add-pic', 'title' => __('Change your avatar'))) ?></li>
 					<li><?php echo HTML::anchor('user/edit', '<i class="icon-pencil"></i>'.__('Edit Account')) ?></li>
