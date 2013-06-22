a38246a414f11e831b8fc8cce5997f839ded1adf
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-22 17:20:57 +0400

    Fixed issue: Cleanup Admin Role Controller

--- modules/user/classes/controller/admin/role.php
+++ modules/user/classes/controller/admin/role.php
@@ -43,7 +43,7 @@ class Controller_Admin_Role extends Controller_Admin {
 						Text::plain($role->name),
 						Text::plain($role->description),
 						$role->special ? '<i class="icon-ok-sign"></i>' : '<i class="icon-ban-circle"></i>',
-	
+
 						$role->special
 							? HTML::icon($role->perm_url, 'icon-lock', array('class'=>'icon-large', 'title'=> __('Edit Permissions')))
 							: HTML::icon($role->edit_url, 'icon-edit', array('class'=>'icon-large', 'title'=> __('Edit Role'))) . '&nbsp;' .
@@ -184,38 +184,42 @@ class Controller_Admin_Role extends Controller_Admin {
 		$this->title = __('Delete :title', array(':title' => $role->name ));
 
 		$view = View::factory('form/confirm')
-						->set('action', Route::url('admin/role',
-								array('action' => 'delete', 'id' => $role->id)
-								))
+						->set('action', Route::url('admin/role', array('action' => 'delete', 'id' => $role->id)))
 						->set('title', $role->name);
 
 		// If deletion is not desired, redirect to list
-				if ( isset($_POST['no']) AND $this->valid_post() )
-						$this->request->redirect(Route::get('admin/role')->uri());
+		if (isset($_POST['no']) AND $this->valid_post())
+		{
+			$this->request->redirect(Route::get('admin/role')->uri());
+		}
 
-				// If deletion is confirmed
-				if ( isset($_POST['yes']) AND $this->valid_post() )
-				{
-						try
-						{
-								$role->delete(); //delete the role
+		// If deletion is confirmed
+		if (isset($_POST['yes']) AND $this->valid_post())
+		{
+			try
+			{
+				$role->delete(); //delete the role
 				Message::success(__('Role: :name deleted successful!', array(':name' => $role->name)));
 
 				if ( ! $this->_internal)
+				{
 					$this->request->redirect(Route::get('admin/role')->uri());
-						}
-						catch (Exception $e)
-						{
+				}
+			}
+			catch (Exception $e)
+			{
 				Kohana::$log->add(Log::ERROR, 'Error occured deleting role id: :id, :message',
-							array(':id' => $role->id, ':message' => $e->getMessage()));
+							array(':id' => $role->id, ':message' => $e->getMessage())
+				);
 				Message::error('An error occured deleting blog, :post.',array(':post' => $post->title));
 
 				if ( ! $this->_internal)
+				{
 					$this->request->redirect(Route::get('admin/role')->uri());
-						}
 				}
+			}
+		}
 
-				$this->response->body($view);
+		$this->response->body($view);
 	}
-
 }
