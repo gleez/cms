4f69d2d1f4e30d8425fdc6e76df6eee036ed0966
Sergey Yakovlev <sadhooklay@gmail.com>
2013-06-30 14:09:53 +0400

    New feature: Added Estonian translation
    Thanks to Sergei Moroz

---
+++ modules/user/i18n/et.php
@@ -0,0 +1,193 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Localization for Estonian
+ *
+ * @package    User\I18n
+ * @author     Sergei Moroz <sergeimoroz@gmail.com>
+ * @copyright  (c) 2011-2013 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
+ */
+return array
+(
+	// :
+	':field must be a valid address with the http:// or https:// prefix' => ':field должна быть правильным адресом и начинаться с http:// или https://',
+	':nick joined.' => ':nick присоединился.',
+	':prov account' => 'Akaunt :prov',
+	':role Permissions' => 'Полномочия :role',
+	':site - Reset password for :name' => ':site - Сброс пароля для :name',
+	':site Account' => 'Akaunt :site',
+	':user Permissions' => 'Полномочия :user',
+	// %
+	"%title successfully updated!" => '%title успешно обновлён!',
+	// A
+	'A request to reset the password for your account has been made at !site.' => 'С сайта !site был осуществлен запрос на сброс пароля для вашей учётной записи.',
+	'About' => 'О пользователе',
+	'Account %title created successful!' => 'Пользователь %title успешно создан!',
+	'Activity' => 'Активность',
+	'Add New Role' => 'Lisada uus roll',
+	'Add New User' => 'Lisada uut kasutajad',
+	'Add Role' => 'Lisada roll',
+	'Administer permissions' => 'Администрирование полномочий',
+	'Administer users' => 'Администрирование пользователей',
+	'Access profiles' => 'Доступ к профилям',
+	'Access to all profiles' => 'Доступ ко всем профилям',
+	'After logging in, you will be redirected, so you can change your password. We do not encourage you to lose your password again. Be aware that this behaviour can be dangerous.' => 'После входа в систему вы будете перенаправлены для того чтоб изменить свой пароль. Мы не рекомендуем вам терять свой пароль повторно. Эта ситуация потенциально опасна.',
+	'Allow' => 'Разрешить',
+	'Already have an account?' => 'Akaunt on juba?',
+	'Already have an account? Choose how you would like to sign in' => 'Akaunt on juba? Выберите способ авторизации',
+	// B
+	'Best Regards' => 'Parimate soovidega',
+	'Bio' => 'Bio',
+	'Bio must be less than :param2 characters long.' => 'Адрес должен быть не больше :param2 символов.',
+	'Birthday' => Sünipäev',
+	// C
+	'Change Avatar' => 'Vahetada pilt',
+	'Change own username' => 'Vahetada login',
+	'Change Password' => 'Vahetada parool',
+	'Change your avatar' => 'Vahetada pilt',
+	'Close' => 'Panna kinni',
+	'Create One.' => 'Uus akaunt.',
+	'Current password' => 'Praegune parool',
+	'Confirm Password' => 'Korrata parool',
+	'Confirm new password' => 'Kinnitage uus parool',
+	// D
+	'Delete User' => 'Удалить пользователя',
+	'Description of Role must be less than :param2 characters long' => 'Описание роли должно быть менее :param2 символов',
+	'Disallow' => 'Keelama',
+	'Display Name' => 'Отображаемое имя',
+	"Don't have an account? :url" => 'Нет аккаунта? :url',
+	"Don't have an account?" => 'Akauntid pole?',
+	// E
+	'Edit Account' => 'Parandada akaunt',
+	'Edit Permission' => 'Изменить полномочие',
+	'Edit Permissions' => 'Изменить полномочия',
+	'Edit profile' => 'Parandada profilit',
+	'Editing profile'=> 'Изменение профиля',
+	'Edit role %name' => 'Parandada roll %name',
+	'Edit User' => 'Parandada kasutajad',
+	// F
+	'Fast, safe & secure way!' => 'Быстро и безопасно!',
+	'Female' => 'Naine',
+	'Fill in the information below to register' => 'Для регистрации заполните регистрационную анкету ниже',
+	'First Visit' => 'Первое посещение',
+	'Forgot Password?' => 'Unustasid parolli?',
+	// G
+	'Gender' => 'Пол',
+	'Gleez CMS allows users to register, login, log out, maintain user profiles, etc. Users of the site may not use their own names to post content until they have signed up for a user account.' => 'Gleez CMS позволяет пользователям регистрироваться, проходить авторизацию, управлять профилями пользователей и т.п. Пользователи сайта не смогут использовать свои собственные имена для размещения материалов, пока они не зарегистрировали учетную запись пользователя. ',
+	'Gleez users and group management' => 'Управление пользователями и группами в Gleez',
+	'Go to' => 'Minema edasi',
+	// H
+	'Hello :name!' => 'Tere, :name!',
+	'Home Page' => 'Kodu leht',
+	// I
+	"If you don't use any of these services, you can create an account." => "Если вы не используете ни один из этих сервисов, вы можете зарегистрировать аккаунт.",
+	'Inherit' => 'Наследовать',
+	'Instructions to reset your password are being sent to your email address %mail.' => 'Инструкции для сброса вашего пароля были высланы на ваш email адрес %mail.',
+	// J
+	'Joined on' => 'Участник с',
+	// K
+	// L
+	'Last Active' => 'Последняя активность',
+	'Last Visit' => 'Viimane külastamine',
+	'Loading data from server' => 'Загрузка ',
+	'Login' => 'Login sisse',
+	'Login with :provider account' => 'Войти с помощью учётной записи :provider',
+	// M
+	'Mail' => 'Post',
+	'Mail must be a valid email address.' => 'Email адрес должен быть в правильном формате.',
+	'Male' => 'Мужской',
+	'Managing user authority' => 'Управление аутентификацией пользователей',
+	'Minimum password length &mdash; :count characters' => 'Минимальная длина пароля &mdash; :count символа(ов)',
+	'My Account' => 'Minu akaunt',
+	// N
+	'Name' => 'Nimi',
+	'Name of Role must be at least :param2 characters long' => 'Имя роли должно быть не менее :param2 символов',
+	'Name of Role must be less than :param2 characters long' => 'Имя роли должно быть менее :param2 символов',
+	'New password' => 'Uus parool',
+	'New password (again)' => 'Kordage parool',
+	'No more than 800 characters' => 'Не более 800 символов',
+	// O
+	'Old password is incorrect.' => 'praegune parool on vale',
+	'Only authorized users can post comments. :register or login using one of these services:' => 'Только авторизованные пользователи могут оставлять комментарии. :register или войдите с помощью одного их этих сервисов:',
+	// P
+	'Password' => 'parool',
+	'Password or username is incorrect.' => 'Parool või kasutaja nimi on vale',
+	'Password Confirm must be the same as Password.' => 'Подтверждение пароля должно соответствовать паролю.',
+	'Password must be at least :param2 characters long.' => 'Пароль должен быть не короче :param2 символов.',
+	'Password successfully changed! We hope you feel safer now.' => 'Пароль успешно изменён! Мы надеемся теперь вы чувствуете себя в безопасности.',
+	'Private' => 'Личное',
+	'Permission' => 'Полномочие',
+	'Permissions' => 'Полномочия',
+	'Permissions let you control what users can do on your site. Each user role (defined on the :user-roles) has its own set of permissions. Permissions also allow trusted users to share the administrative burden of running a busy site.' => 'Полномочия позволяют вам контролировать то, что пользователи могут делать на вашем сайте. Каждая пользовательская роль (определённая в :user-roles) имеет свой набор полномочий.  Полномочия так же позволяют предоставлять доверенным пользователям бремя административного ведения рабочего сайта.',
+	'Permissions let you control what users can do on your site. User defined permission override role based permissions.' => 'Разрешения позволяют вам контролировать то, что пользователи могут делать на вашем сайте. Пользовательские разрешения переопределяют разрешения определённые для ролей.',
+	'Permissions save failed!' => 'Не удалось сохранить полномочия!',
+	'Permissions saved successfully!' => 'Полномочия успешно сохранены!',
+	'Photo' => 'Pilt',
+	'Photo successfully uploaded!' => 'Фотография успешно загружена',
+	'Please register' => 'Palun registreerige',
+	'Profile %title' => 'Profil %title',
+	'Public' => 'Публичное',
+	// Q
+	// R
+	'Register new account' => 'Registreerida uus akaunt',
+	'Repeat entered password' => 'Kordage parool',
+	'Reset password' => 'Сброс пароля',
+	'Reset' => 'Сброс',
+	'Role %name saved successful!' => 'Роль %name успешно создана!',
+	'Role %name updated successful!' => 'Роль %name успешно обновлена!',
+	"Role doesn't exists!" => "Roll pole leitud!",
+	'Role name must not be empty' => 'Rolli nimi ei tohi olla tühi',
+	'Roles allow you to fine tune the security and administration of Gleez CMS. A role defines a group of users that have certain privileges as defined in user permissions. Examples of roles include: anonymous user, authenticated user, moderator, administrator and so on. In this area you will define the role names of the various roles.' => 'Роли позволяют точно настроить безопасность и управление Gleez CMS. Роль определяет группу пользователей, которые имеют некоторые привилегии, определенные в пользовательских привилегиях. Например, роли могут быть использованы для следующих групп пользователей: анонимный пользователь, авторизованный пользователь, модератор, администратор и т.д. Здесь вы можете назначать имена ролям и управлять ими.',
+	// S
+	'Save Permissions' => 'Сохранить полномочия',
+	'Security code' => 'Turvakood',
+	'Sign in using social network:' => 'Войти с помощью социальных сетей:',
+	'Sign in using your registered account:' => 'Войдите используя ваш зарегистрированный аккаунт:',
+	"Special" => 'Особая роль',
+	'Special Role' => 'Особая роль',
+	'Stay Signed in' => 'Оставаться в системе',
+	// T
+	'The ability to change own username' => 'On võimalus paranada oma nime (login)',
+	'The ability to change profile' => 'On võimalus parandada profileid',
+	'The username must be at least :param2 characters long.' => 'Имя пользователя должно быть не менее :param2 символов.',
+	'The username must be less than :param2 characters long.' => 'Имя пользователя должно быть не менее :param2 символов.',
+	'The values you entered in the password fields didn\'t match.' => 'Значения введённые вами в поля для паролей не совпадают.',
+	'This account is blocked' => 'Se akaunt on lukustatud',
+	"This email don't exists." => 'Se email adress pole leitud.',
+	"This is a one-time URL, so it can be used only once. It expires after one day and nothing will happen if it's not used." => 'Это одноразовая ссылка и она может быть использована одни раз. Срок действия ссылки один день и ничего не произойдёт если она за это время не используется.',
+	'Try to come up with a complex password' => 'Proovige mõelda valja raske parool',
+	// U
+	'Update Profile' => 'Обновить профиль пользователя',
+	'Upload' => 'Alla ladima',
+	'Uploading Photos' => 'Ala ladima pilt',
+	'User Account' => 'Kasutaja akaunt',
+	"User doesn't exists!" => "Kasutaja pole leitud!",
+	'User Registration' => 'Kaasutaja regestratsioon',
+	'User registration not allowed' => 'gestratsioon keelatud',
+	'user roles page' => 'странице пользовательских ролей',
+	'Users management' => 'Управление пользователями',
+	'Username/Email' => 'Kasutaja nimi/Email',
+	'Username' => 'Kastuja nimi',
+	'Username for login' => 'Имя пользователя для авторизации',
+	'Username for logging in can\'t be changed.' => 'Имя пользователя для авторизации не может быть изменено.',
+	'Users' => 'Kasutaja',
+	// V
+	'Visits' => 'Külastamine',
+	// W
+	'Warning! Give to trusted roles only; this permission has security implications.' => 'Внимание! Предоставляйте это только доверенным ролям; данные полномочия имеют последствия для безопасности.',
+	'Welcome to Gleez!' => 'Tere tulemast Gleez!',
+	'Welcome, %title!' => 'Tere tulemast, %title!',
+	'Will be private' => 'Будет скрыто от посторонних',
+	'Will be public' => 'Будет публичным',
+	// X
+	// Y
+	'You can sign in from any of the following services:' => 'Вы можете авторизоваться с помощью одного из этих сервисов:',
+	'You may now log in to !uri_brief clicking on this link or copying and pasting it in your browser.' => 'Теперь вы можете войти на сайт нажав на ссылку !uri_brief или скопировав и вставив её в адресную строку браузера',
+	'You must confirm password.' => 'Te peate kinnitama oma parool.',
+	'You must provide a password.' => 'Teie peate kirjutama parool.',
+	'You must provide a username.' => 'Teie peate kirjutama kasutaja nime.',
+	'You must provide an email address.' => 'Teie peate kirjutama email adress.',
+	'You must provide old password.' => 'Teie peate kirjutama praegune parool.',
+	'Your picture will be changed proportionally to the size of :w&times;:h' => 'Ваше изображение будет пропорционально изменено до размеров :w&times;:h',
+	// Z
+);
