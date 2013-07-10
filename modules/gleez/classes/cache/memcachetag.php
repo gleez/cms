d5a3fa6b1e9fee4740e7904f300242d373cbb0c9
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-10 18:34:47 +0400

    Updated issue #373: Getting rid of the Gleez namespace for Cache Classes

--- modules/gleez/classes/cache/memcachetag.php
+++ modules/gleez/classes/cache/memcachetag.php
@@ -1,3 +1,80 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access allowed.');
+/**
+ * See [Cache_Memcache]
+ *
+ * @package    Gleez\Cache\Base
+ * @version    2.0
+ * @author     Kohana Team
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2009-2012 Kohana Team
+ * @copyright  (c) 2012-2013 Gleez Technologies
+ * @license    http://kohanaphp.com/license
+ * @license    http://gleezcms.org/license Gleez CMS License
+ */
+class Cache_MemcacheTag extends Cache_Memcache implements Cache_Tagging {
 
-class Cache_MemcacheTag extends Gleez_Cache_MemcacheTag {}
+	/**
+	 * Constructs the memcache object
+	 *
+	 * @param  array  $config  configuration
+	 * @throws  Cache_Exception
+	 */
+	protected function __construct(array $config)
+	{
+		parent::__construct($config);
+
+		if ( ! method_exists($this->_memcache, 'tag_add'))
+		{
+			throw new Cache_Exception('Memcached-tags PHP plugin not present. Please see http://code.google.com/p/memcached-tags/ for more information');
+		}
+	}
+
+	/**
+	 * Set a value based on an id with tags
+	 *
+	 * @param   string   $id        id
+	 * @param   mixed    $data      data
+	 * @param   integer  $lifetime  lifetime [Optional]
+	 * @param   array    $tags      tags [Optional]
+	 * @return  boolean
+	 */
+	public function set_with_tags($id, $data, $lifetime = NULL, array $tags = NULL)
+	{
+		$id = $this->_sanitize_id($this->config('prefix').$id);
+
+		$result = $this->set($id, $data, $lifetime);
+
+		if ($result and $tags)
+		{
+			foreach ($tags as $tag)
+			{
+				$this->_memcache->tag_add($tag, $id);
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Delete cache entries based on a tag
+	 *
+	 * @param   string  $tag  tag
+	 * @return  boolean
+	 */
+	public function delete_tag($tag)
+	{
+		return $this->_memcache->tag_delete($tag);
+	}
+
+	/**
+	 * Find cache entries based on a tag
+	 *
+	 * @param   string  $tag  tag
+	 * @return  void
+	 * @throws  Cache_Exception
+	 */
+	public function find($tag)
+	{
+		throw new Cache_Exception('Memcached-tags does not support finding by tag');
+	}
+}
--- modules/gleez/classes/cache/tagging.php
+++ modules/gleez/classes/cache/tagging.php
@@ -1,3 +1,42 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access allowed.');
+/**
+ * Gleez Cache Tagging Interface
+ *
+ * @package    Gleez\Cache\Base
+ * @author     Kohana Team
+ * @author     Sandeep Sangamreddi - Gleez
+ * @copyright  (c) 2009-2012 Kohana Team
+ * @copyright  (c) 2012-2013 Gleez Technologies
+ * @license    http://kohanaphp.com/license
+ * @license    http://gleezcms.org/license Gleez CMS License
+ */
+interface Cache_Tagging {
 
-interface Cache_Tagging extends Gleez_Cache_Tagging {}
+	/**
+	 * Set a value based on an id. Optionally add tags.
+	 *
+	 * [!!] Note: Some caching engines don't support tagging
+	 *
+	 * @param   string   $id        id
+	 * @param   mixed    $data      data
+	 * @param   integer  $lifetime  lifetime [Optional]
+	 * @param   array    $tags      tags [Optional]
+	 * @return  boolean
+	 */
+	public function set_with_tags($id, $data, $lifetime = NULL, array $tags = NULL);
+
+	/**
+	 * Delete cache entries based on a tag
+	 *
+	 * @param   string  $tag  tag
+	 */
+	public function delete_tag($tag);
+
+	/**
+	 * Find cache entries based on a tag
+	 *
+	 * @param   string  $tag  tag
+	 * @return  array
+	 */
+	public function find($tag);
+}
