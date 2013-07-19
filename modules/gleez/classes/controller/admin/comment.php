41bab67d3e2add802697b2bc9068db10a9099ae9
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-07-19 22:16:54 +0530

    Update feature: Configure popup to use the specefied dataTable instead autodetect

--- modules/gleez/classes/controller/admin/comment.php
+++ modules/gleez/classes/controller/admin/comment.php
@@ -287,7 +287,7 @@ class Controller_Admin_Comment extends Controller_Admin {
 						HTML::anchor($post->post->url, $post->post->title, array('class'=>'action-view')),
 						Date::formatted_time($post->created),
 						HTML::icon($post->edit_url.$this->_destination, 'icon-edit', array('class'=>'action-edit', 'title'=> __('Edit'))),
-						HTML::icon($post->delete_url.$this->_destination, 'icon-trash', array('class'=>'action-delete', 'title'=> __('Delete'), 'data-toggle' => 'popup', 'data-title' => __('Delete')))
+						HTML::icon($post->delete_url.$this->_destination, 'icon-trash', array('class'=>'action-delete', 'title'=> __('Delete'), 'data-toggle' => 'popup', 'data-table' => '#admin-list-paths'))
 				));
 			}
 		}
