d2bd00f7a7f72c6b416b758bb9a1d72729e3a833
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-21 15:52:00 +0400

    Fixed issue: Cleanup validation messages

--- modules/gleez/messages/models/tag.php
+++ modules/gleez/messages/models/tag.php
@@ -2,7 +2,7 @@
 
 return array(
 	'name' => array(
-		'not_empty' => 'Tag name can\'t be empty.',
-		'tag_available' => 'Tag :param1 already exists.',
+		'not_empty' => 'Tag name can\'t be empty',
+		'tag_available' => 'Tag :param1 already exists',
 	),
 );
--- modules/user/messages/models/mail.php
+++ modules/user/messages/models/mail.php
@@ -2,8 +2,8 @@
 
 return array(
 	'mail' => array(
-		'not_empty' => 'You must provide an email address.',
-		'email_not_available' => "This email don't exists.",
-		'email' => 'Mail must be a valid email address.',
+		'not_empty' => 'You must provide an email address',
+		'email_not_available' => "This email don't exists",
+		'email' => 'Mail must be a valid email address',
 	),
 );
