53a356153212b709ad11566ddf658e4e126b0839
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-21 15:17:50 +0400

    Fixed issue: Cleanup taxonomy from

--- modules/gleez/views/admin/taxonomy/form.php
+++ modules/gleez/views/admin/taxonomy/form.php
@@ -1,24 +1,34 @@
 <?php defined('SYSPATH') OR die('No direct script access.') ?>
 
-<?php $parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add');
-	echo Form::open(Route::get('admin/taxonomy')->uri($parms), array('id'=>'vocab-form', 'class'=>'form')) ?>
+<div class="help">
+	<?php _e('Add new vocabulary to your site, edit and reorganize existing vocabularies.') ?>
+</div>
+
+<?php $params = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add');
+	echo Form::open(Route::get('admin/taxonomy')->uri($params), array('id'=>'vocab-form', 'class'=>'vocab-form form form-horizontal well clearfix')) ?>
 
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
-<div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
-	<?php echo Form::label('name', __('Name'), array('class' => 'control-label')) ?>
-   	<?php echo Form::input('name', $post->rawname, array('class' => 'text small')); ?>
-</div>
+	<div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
+		<?php echo Form::label('name', __('Name'), array('class' => 'control-label')) ?>
+		<div class="controls">
+			<?php echo Form::input('name', $post->rawname, array('class' => 'input-xlarge')); ?>
+		</div>
+	</div>
 
-<div class="control-group <?php echo isset($errors['type']) ? 'error': ''; ?>">
-	<?php echo Form::label('type', __('Type'), array('class' => 'control-label')) ?>
-	<?php echo Form::select('type', Gleez::types(), $post->type, array('class' => 'list small')); ?> 
-</div>
+	<div class="control-group <?php echo isset($errors['type']) ? 'error': ''; ?>">
+		<?php echo Form::label('type', __('Type'), array('class' => 'control-label')) ?>
+		<div class="controls">
+			<?php echo Form::select('type', Gleez::types(), $post->type, array('class' => 'input-xlarge')); ?>
+		</div>
+	</div>
 	
-<div class="control-group <?php echo isset($errors['description']) ? 'error': ''; ?>">
- 	<?php echo Form::label('description', __('Description'), array('class' => 'control-label')) ?>
- 	<?php echo Form::textarea('description', $post->description, array('class' => 'input-large', 'rows' => 5)) ?>
-</div>
+	<div class="control-group <?php echo isset($errors['description']) ? 'error': ''; ?>">
+		<?php echo Form::label('description', __('Description'), array('class' => 'control-label')) ?>
+		<div class="controls">
+			<?php echo Form::textarea('description', $post->description, array('class' => 'input-xlarge', 'rows' => 5)) ?>
+		</div>
+	</div>
 
-<?php echo Form::submit('vocab', __('Save'), array('class' => 'btn btn-success pull-right')) ?>
+	<?php echo Form::submit('vocab', __('Save'), array('class' => 'btn btn-success pull-right')) ?>
 <?php echo Form::close() ?>
