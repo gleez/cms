0449c98d9f8a450e5e1d25f824b4dfcb5ad0d84d
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-07-21 17:27:07 +0530

    New Feature: Add Assets::popup() helper method for adding popup assets

--- modules/gleez/classes/gleez/assets.php
+++ modules/gleez/classes/gleez/assets.php
@@ -853,6 +853,20 @@ class Gleez_Assets {
 	}
 
 	/**
+	 * Popup Modal windows
+	 */
+	public static function popup()
+	{
+		Assets::css('form', 'media/css/form.css', array('weight' => 2));
+		Assets::css('popup', 'media/css/popup.css', array('bootstrap'), array('media' => 'screen', 'weight' => 15));
+		
+		Assets::js('form', 'media/js/jquery.form.min.js', array('jquery'), FALSE, array('weight' => 15));
+		Assets::js('ajaxform', 'media/js/gleez.ajaxform.js', NULL, FALSE, array('weight' => 17));
+		Assets::js('autocomplete', 'media/js/gleez.typeahead.js', 'gleez');
+		Assets::js('popup', 'media/js/gleez.popup.js', array('bootstrap'), FALSE, array('weight' => 20));
+	}
+	
+	/**
 	 * Enforce static usage
 	 */
 	private function __contruct() {}
