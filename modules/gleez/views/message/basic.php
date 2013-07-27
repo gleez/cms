a10ab61518b38b780f2e7dd34ef919a2497d583a
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-27 08:22:02 +0400

    Updated feature: Replaced close icon

--- modules/gleez/views/message/basic.php
+++ modules/gleez/views/message/basic.php
@@ -3,7 +3,7 @@
 <?php if (isset($messages) AND !empty($messages)): ?>
 	<?php foreach ($messages as $message): ?>
 		<div class="alert alert-<?php echo $message->type ?>">
-			<button type="button" class="close" data-dismiss="alert">&times;</button>
+			<button type="button" class="close" data-dismiss="alert"><i class="icon-remove-circle"></i></button>
 			<?php echo $message->text ?>
 		</div>
         <?php endforeach; ?>
