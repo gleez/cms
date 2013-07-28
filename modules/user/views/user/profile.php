4c901fcbcf134f71d70e616902f690e4b322e09e
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-29 01:12:00 +0400

    Updated feature: Updated user profile view

--- modules/user/views/user/profile.php
+++ modules/user/views/user/profile.php
@@ -1,14 +1,16 @@
 <?php defined('SYSPATH') OR die('No direct script access.'); ?>
-<?php $use_gravatar = Config::get('site.use_gravatars', FALSE); ?>
+
 <div class="row-fluid" itemscope itemtype="http://schema.org/Person">
 
 	<div id="Panel" class="span4">
 		<div id="photo" class="well">
-			<?php echo $avatar; ?>
+			<div class="Photo">
+				<?php echo User::getAvatar($user, array('size' => 150)); ?>
+			</div>
 
 			<?php if ($is_owner OR ACL::check('administer users')): ?>
 				<ul class="nav nav-list">
-					<?php if ( ! $use_gravatar): ?>
+					<?php if ( ! Config::get('site.use_gravatars', FALSE)): ?>
 						<li><?php echo HTML::anchor('user/photo', '<i class="icon-upload"></i>'.__('Change Avatar'), array('id' => 'add-pic', 'title' => __('Change your avatar'))) ?></li>
 					<?php endif; ?>
 					<li><?php echo HTML::anchor('user/edit', '<i class="icon-pencil"></i>'.__('Edit Account')) ?></li>
