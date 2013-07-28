9401bf6a42f0af3679fc1529be977f0bbcf1dc18
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-29 01:57:25 +0400

    Updated feature: Updated post meta

--- modules/gleez/views/page/teaser.php
+++ modules/gleez/views/page/teaser.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.') ?>
 
-<div id="post-<?php echo $post->id; ?>" class="<?php echo $post->type . ' post teaser post-' . $post->status; ?>">
+<div class="<?php echo $post->type ?> post teaser">
 
 	<?php if($post->taxonomy OR $config->use_submitted): ?>
 		<div class="row-fluid meta">
@@ -11,7 +11,7 @@
 						<?php echo HTML::anchor($post->user->url, $post->user->nick, array('title' => $post->user->nick)); ?>
 					</span>
 
-					<span class="DateCreated"><?php echo Date::date_format($post->created); ?></span>
+					<span class="date-created"><?php echo Date::date_format($post->created); ?></span>
 				</div>
 			<?php endif;?>
 		
@@ -22,7 +22,7 @@
 	<div class="post-content"> <?php echo $post->teaser; ?> </div>
 
 	<?php if ($post->tagcloud): ?>
-		<div class="tags"><?php echo __('Tagged with :tag', array(':tag' => $post->tagcloud) ); ?></div>
+		<div class="tagcloud"><?php echo __('Tagged with :tag', array(':tag' => $post->tagcloud) ); ?></div>
 	<?php endif;?>
 
 </div>
