13781c2efe203db03bdb23bea71f233c4828cf92
Sergey Yakovlev <sadhooklay@gmail.com>
2013-08-14 16:27:43 +0400

    Fixed issue #523: Minor fixes

--- modules/gleez/classes/controller/welcome.php
+++ modules/gleez/classes/controller/welcome.php
@@ -4,6 +4,7 @@
  *
  * @package    Gleez\Controller
  * @author     Gleez Team
+ * @version    1.0.0
  * @copyright  (c) 2011-2013 Gleez Technologies
  * @license    http://gleezcms.org/license  Gleez CMS License
  */
