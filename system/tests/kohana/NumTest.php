d8509c0ba9738bc093513a1ef5851a2c99644b54
Sergey Yakovlev <sadhooklay@gmail.com>
2013-08-30 02:42:00 +0400

    Fixed issue: Fixed Num::bytes. See http://goo.gl/ChqY1x

--- system/tests/kohana/NumTest.php
+++ system/tests/kohana/NumTest.php
@@ -48,7 +48,10 @@ class Kohana_NumTest extends Unittest_TestCase
 			array(204800.0, '200K'),
 			array(5242880.0, '5MiB'),
 			array(1000.0, 1000),
-			array(2684354560.0, '2.5GB'),
+			array(2684354560.0, '2.5GB', FALSE),
+			array(2684354560.0, '2.5GiB', FALSE),
+			array(2684354560.0, '2.5GiB', TRUE),
+			array(2500000000.0, '2.5GB'),
 		);
 	}
 	
@@ -58,12 +61,14 @@ class Kohana_NumTest extends Unittest_TestCase
 	 * @test
 	 * @covers Num::bytes
 	 * @dataProvider provider_bytes
-	 * @param integer Expected Value
-	 * @param string  Input value
+	 *
+	 * @param integer  $expected  Expected Value
+	 * @param string   $size      Input value
+	 * @param boolean  $si        Use SI prefixes?
 	 */
-	public function test_bytes($expected, $size)
+	public function test_bytes($expected, $size, $si = TRUE)
 	{
-		$this->assertSame($expected, Num::bytes($size));
+		$this->assertSame($expected, Num::bytes($size, $si));
 	}
 	
 	/**
