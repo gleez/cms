fa2d2b88e9a69907f7d963045f8fcc71b7156699
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-07 19:31:54 +0400

    Fixed issue: Added i18n ability to welcome email

--- modules/user/views/email/welcome_signup.php
+++ modules/user/views/email/welcome_signup.php
@@ -1,16 +1,13 @@
-Dear <?php echo $name; ?>,
+<?php echo __('Hello :name!', array(':name' => ($nick ? $nick : $name))) . PHP_EOL ?>
+<?php echo __('Your account at :site (:url) has been activated.', array(':site' => Config::get('site.site_name', 'Gleez CMS'), ':url' => $url)) . PHP_EOL ?>
 
-Your account at Gleez(http://www.gleez.com) has been activated.
+<?php echo __('You may now log in to !uri_brief clicking on this link or copying and pasting it in your browser.', array('!uri_brief' => $uri_brief)) . PHP_EOL ?>
 
-You may now log in by clicking on this link or copying and pasting it in your browser:
-
-http://gleez.com/user/login
-
-username: <?php echo $name; ?>
-
-email: <?php echo $mail; ?>
+<?php echo __('Your registration data:') . PHP_EOL ?>
+<?php echo __('username: :name', array(':name' => $name)) . PHP_EOL ?>
+<?php echo __('email: :mail', array(':mail' => $mail)) . PHP_EOL ?>
 
 --
-Warm Regards,
-Gleez Team!
-www.gleez.com
+
+<?php echo __('Best regards, :site team', array(':site' => Config::get('site.site_name', 'Gleez CMS'))) . PHP_EOL ?>
+<?php echo URL::site('', TRUE) ?>
