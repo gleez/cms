096e7f53ea3bdc909aab88ebb664cbab5c62a5da
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-12 02:02:23 +0400

    Updated Issue #373: Removed system/utf8/exception

---
+++ modules/gleez/classes/utf8/exception.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package    Gleez
+ * @category   Exceptions
+ * @author     Gleez Team
+ * @version    1.0.0
+ * @copyright  (c) 2011-2013 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
+ */
+class UTF8_Exception extends Gleez_Exception {}
