b8d58d668e9170293e6615dde21f380a4c72c164
Gleez Technologies <github@gleez.tech>
2013-11-16 02:11:14 +0530

    Update feature: Form views parity with BS3

--- modules/gleez/views/form/confirm.php
+++ modules/gleez/views/form/confirm.php
@@ -4,7 +4,7 @@
 	<p><?php echo __('This action cannot be undone.'); ?></p>
 
 	<div class="clearfix"></div>
-	<?php echo Form::submit('no', __('Cancel'), array('class' => 'btn')) ?> &nbsp;
+	<?php echo Form::submit('no', __('Cancel'), array('class' => 'btn btn-default')) ?> &nbsp;
 	<?php echo Form::submit('yes', __('Delete'), array('class' => 'btn btn-danger')) ?>
 
 <?php echo Form::close() ?>
