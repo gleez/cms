6f0a270b58fc43108a088ff43d01ca0d557ea698
Gleez Technologies <github@gleez.tech>
2013-11-16 02:07:27 +0530

    Update feature: Page Views parity with BS3

--- modules/gleez/views/page/list.php
+++ modules/gleez/views/page/list.php
@@ -1,13 +1,23 @@
+<div class="row">
+	<div class="col-md-1 pull-right">
+		<?php echo HTML::icon($rss_link, 'icon-rss', array('title' => 'RSS 2.0', 'class' => 'pull-right')); ?>
+	</div>
+</div>
 <?php foreach($posts as $i => $post): ?>
-	<div id="post-<?php echo $i; ?>" class="post-list <?php echo ($post->sticky) ? ' sticky' : ' post-'. $post->status; ?>">
-		<h2 class="post-title">
-			<?php echo HTML::anchor($post->url, $post->title) ?>
-		</h2>
+	<div id="post-<?php echo $post->id; ?>" class="post-list <?php echo ($post->sticky) ? ' sticky' : ' post-'.$post->status; ?>">
+		<div class="title-holder">
+			<h2 class="post-title">
+				<?php echo HTML::anchor($post->url, $post->title); ?>
+			</h2>
+		</div>
+		<?php if ($post->promote): ?>
+			<i class="post-bookmark clearfix"></i>
+		<?php endif; ?>
 		<?php
 			echo View::factory($post->type.'/teaser')
-					->set('post', $post)
-					->set('config', $config)
-					->set('page_title', TRUE);
+				->set('post',       $post)
+				->set('config',     $config)
+				->set('page_title', TRUE);
 		?>
 	</div>
 <?php endforeach; ?>
