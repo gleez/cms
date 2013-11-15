bfc18bf12dd1665608aa41e383cefd08190e0ca0
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-16 02:12:12 +0530

    Update Feature: Comment Views parity with BS3

--- modules/gleez/views/comment/form.php
+++ modules/gleez/views/comment/form.php
@@ -9,19 +9,19 @@
 
 			<div class='inside' id="submitpost">
 				<div id="minor-publishing">
-					<div class="control-group <?php echo isset($errors['status']) ? 'error': ''; ?>">
-						<?php echo Form::label('status', __('Change Status'), array('class' => 'abovecontent')) ?>
-						<?php echo Form::select('status', Comment::status(), $post->status, array('class' => 'list large')); ?>
+					<div class="form-group <?php echo isset($errors['status']) ? 'error': ''; ?>">
+						<?php echo Form::label('status', __('Change Status'), array('class' => 'abovecontent control-label')) ?>
+						<?php echo Form::select('status', Comment::status(), $post->status, array('class' => 'form-control')); ?>
 					</div>
 
-					<div class="control-group <?php echo isset($errors['author_name']) ? 'error': ''; ?>">
-						<?php echo Form::label('author_name', __('Author'), array('class' => 'above')) ?>
-						<?php echo Form::input('author_name', $post->user->name,array('class' => 'text large'), 'autocomplete/user'); ?>
+					<div class="form-group <?php echo isset($errors['author_name']) ? 'error': ''; ?>">
+						<?php echo Form::label('author_name', __('Author'), array('class' => 'above control-label')) ?>
+						<?php echo Form::input('author_name', $post->user->name,array('class' => 'form-control'), 'autocomplete/user'); ?>
 					</div>
 
-					<div class="control-group <?php echo isset($errors['author_date']) ? 'error': ''; ?>">
-						<?php echo Form::label('author_date', __('Date'), array('class' => 'abovecontent') ) ?>
-						<?php echo Form::input('author_date', Date::formatted_time($post->created), array('class' => 'text large')); ?>
+					<div class="form-group <?php echo isset($errors['author_date']) ? 'error': ''; ?>">
+						<?php echo Form::label('author_date', __('Date'), array('class' => 'abovecontent control-label') ) ?>
+						<?php echo Form::input('author_date', Date::formatted_time($post->created), array('class' => 'form-control')); ?>
 					</div>
 				</div>
 
@@ -33,7 +33,7 @@
 					<?php endif; ?>
 
 					<div id="publishing-action">
-						<?php echo Form::submit('comment', __('Save'), array('class' => 'btn')) ?>
+						<?php echo Form::submit('comment', __('Save'), array('class' => 'btn btn-default')) ?>
 					</div>
 				</div>
 			</div>
@@ -48,19 +48,19 @@
 			<h3 class='hndle'><?php echo __('Author') ?></h3>
 
 			<div class="inside">
-				<div class="control-group <?php echo isset($errors['guest_name']) ? 'error': ''; ?>">
-					<?php echo Form::label('guest_name', __('Your Name'), array('class' => 'nowrap') ) ?>
-					<?php echo Form::input('guest_name', $post->guest_name, array('class' => 'text medium')); ?>
+				<div class="form-group <?php echo isset($errors['guest_name']) ? 'error': ''; ?>">
+					<?php echo Form::label('guest_name', __('Your Name'), array('class' => 'control-label nowrap') ) ?>
+					<?php echo Form::input('guest_name', $post->guest_name, array('class' => 'form-control')); ?>
 				</div>
 
-				<div class="control-group <?php echo isset($errors['guest_email']) ? 'error': ''; ?>">
-					<?php echo Form::label('guest_email', __('Email'), array('class' => 'nowrap') ) ?>
-					<?php echo Form::input('guest_email', $post->guest_email, array('class' => 'text medium')); ?>
+				<div class="form-group <?php echo isset($errors['guest_email']) ? 'error': ''; ?>">
+					<?php echo Form::label('guest_email', __('Email'), array('class' => 'control-label nowrap') ) ?>
+					<?php echo Form::input('guest_email', $post->guest_email, array('class' => 'form-control')); ?>
 				</div>
 
-				<div class="control-group <?php echo isset($errors['guest_url']) ? 'error': ''; ?>">
-					<?php echo Form::label('guest_url', __('Website'), array('class' => 'nowrap') ) ?>
-					<?php echo Form::input('guest_url', $post->guest_url, array('class' => 'text medium')); ?>
+				<div class="form-group <?php echo isset($errors['guest_url']) ? 'error': ''; ?>">
+					<?php echo Form::label('guest_url', __('Website'), array('class' => 'control-label nowrap') ) ?>
+					<?php echo Form::input('guest_url', $post->guest_url, array('class' => 'form-control')); ?>
 				</div>
 			</div>
 		</div>
@@ -70,10 +70,10 @@
 		<h3 class='hndle'><?php echo __('Leave a Comment') ?></h3>
 
 		<div class="inside">
-			<div class="control-group <?php echo isset($errors['body']) ? 'error': ''; ?>">
+			<div class="form-group <?php echo isset($errors['body']) ? 'error': ''; ?>">
 				<?php echo Form::hidden('comment_post_id', $item->id); ?>
 				<?php echo Form::hidden('comment_post_type', $item->type); ?>
-				<?php echo Form::textarea('body', $post->rawbody, array('class' => 'textarea full', 'rows' => 7)); ?>
+				<?php echo Form::textarea('body', $post->rawbody, array('class' => 'form-control textarea', 'rows' => 7)); ?>
 			</div>
 		</div>
 
@@ -84,16 +84,16 @@
 <div class="clearfix"></div>
 
 <?php if ($use_captcha  AND ! $captcha->promoted()) : ?>
-	<div class="control-group <?php echo isset($errors['captcha']) ? 'error': ''; ?>">
-		<?php echo Form::label('_captcha', __('Security'), array('class' => 'nowrap')) ?>
-		<?php echo Form::input('_captcha', '', array('class' => 'text tiny align')); ?>
+	<div class="form-group <?php echo isset($errors['captcha']) ? 'error': ''; ?>">
+		<?php echo Form::label('_captcha', __('Security'), array('class' => 'form-control nowrap')) ?>
+		<?php echo Form::input('_captcha', '', array('class' => 'text form-control')); ?>
 		<?php echo $captcha; ?>
 	</div>
 <?php endif; ?>
 
 <?php if ( ! ACL::check('administer comment') OR ! $is_edit) : ?>
 	<br>
-	<?php echo Form::submit('comment', __('Post Comment'), array('class' => 'btn')) ?>
+	<?php echo Form::submit('comment', __('Post Comment'), array('class' => 'btn btn-default')) ?>
 <?php endif; ?>
 
 <?php echo Form::close() ?>
