e1f36a4f56d9dc73b63b6f47abaa1b4fdcf3a71e
Gleez Technologies <github@gleez.tech>
2013-11-16 16:08:02 +0530

    New feature #594 : Use panels for widgets, initial proposal

--- modules/gleez/views/widgets/html.php
+++ modules/gleez/views/widgets/html.php
@@ -1,12 +1,11 @@
-<div id="widget-<?php echo $widget->module; ?>-<?php echo Text::plain($widget->name); ?> <?php echo (isset($id)) ? 'widget-'.$id : '' ?>" class="widget blockme widget-<?php echo Text::plain($widget->name); ?> <?php echo ($widget->menu) ? 'widget-menu' : ''; ?> <?php echo (isset($zebra)) ? 'widget-'.$zebra : '' ?>">
-
-	<?php if ($widget->show_title): ?>
-		<div class="widget-header">
+<div id="widget-<?php echo $widget->module; ?>-<?php echo Text::plain($widget->name); ?> <?php echo (isset($id)) ? 'widget-'.$id : '' ?>" class="panel panel-default widget blockme widget-<?php echo Text::plain($widget->name); ?> <?php echo ($widget->menu) ? 'widget-menu' : ''; ?> <?php echo (isset($zebra)) ? 'widget-'.$zebra : '' ?>">
+  	<?php if ($widget->show_title): ?>
+		<div class="panel-heading">
 			<i class="fa <?php echo Text::plain($widget->icon); ?>"></i>
-			<h3><?php echo Text::plain($title); ?></h3>
+			<h3 class="panel-title"><?php echo Text::plain($title); ?></h3>
 		</div>
-	<?php endif; ?>
-
-	<div class="widget-content"><?php echo $content; ?></div>
-
+  	<?php endif; ?>
+	<div class="panel-body">
+		<?php echo $content; ?>
+	</div>
 </div>
