1c91c01c3ccc4a051ea37523e2399455dfbdb93e
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-16 16:12:32 +0530

    Update feature: Move icon into h3 to inline for widget #594

--- modules/gleez/views/widgets/html.php
+++ modules/gleez/views/widgets/html.php
@@ -1,8 +1,7 @@
 <div id="widget-<?php echo $widget->module; ?>-<?php echo Text::plain($widget->name); ?> <?php echo (isset($id)) ? 'widget-'.$id : '' ?>" class="panel panel-default widget blockme widget-<?php echo Text::plain($widget->name); ?> <?php echo ($widget->menu) ? 'widget-menu' : ''; ?> <?php echo (isset($zebra)) ? 'widget-'.$zebra : '' ?>">
   	<?php if ($widget->show_title): ?>
 		<div class="panel-heading">
-			<i class="fa <?php echo Text::plain($widget->icon); ?>"></i>
-			<h3 class="panel-title"><?php echo Text::plain($title); ?></h3>
+			<h3 class="panel-title"><i class="fa <?php echo Text::plain($widget->icon); ?>"></i> <?php echo Text::plain($title); ?></h3>
 		</div>
   	<?php endif; ?>
 	<div class="panel-body">
