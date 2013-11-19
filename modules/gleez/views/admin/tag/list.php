1c6b07c23864800b8383251ce0cdb1d2e750e64b
Gleez Technologies <gleez@server.gleeztech.org>
2013-11-19 17:49:08 +0530

    Update feature: Admin/tag views parity with BS3

--- modules/gleez/views/admin/tag/list.php
+++ modules/gleez/views/admin/tag/list.php
@@ -7,7 +7,7 @@
 		<p><?php echo __('The tags module allows you to create free-tagging for everything, to define the various properties of your content, for example \'Countries\' or \'Colors\'.'); ?></p>
 	</div>
 
-	<?php echo HTML::anchor(Route::get('admin/tag')->uri(array('action' =>'add')), '<i class="icon-plus icon-white"></i> '.__('Add New Tag'), array('class' => 'btn btn-success pull-right')) ?>
+	<?php echo HTML::anchor(Route::get('admin/tag')->uri(array('action' =>'add')), '<i class="fa fa-plus fa-white"></i> '.__('Add New Tag'), array('class' => 'btn btn-success pull-right')) ?>
 	<div class='clearfix'></div><br>
 
 	<table id = "admin-list-tags" class="table table-striped table-bordered table-highlight" data-toggle="datatable" data-target="<?php echo $url?>" data-sorting='[["0", "desc"]]'>
