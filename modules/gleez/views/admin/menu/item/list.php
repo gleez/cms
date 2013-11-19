a1b131e2c6f2793a542fb2509803ae20a3884bd4
Gleez Technologies <gleez@server.gleeztech.org>
2013-11-19 17:46:46 +0530

    Update feature: Admin/menu views parity with BS3

--- modules/gleez/views/admin/menu/item/list.php
+++ modules/gleez/views/admin/menu/item/list.php
@@ -1,4 +1,4 @@
-<?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'add', 'id' => $id)), '<i class="icon-plus icon-white"></i>'.__('Add New Item'), array('title'=>__('Add New Item'), 'class' => 'btn btn-success pull-right')); ?>
+<?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'add', 'id' => $id)), '<i class="fa fa-plus fa-white"></i>'.__('Add New Item'), array('title'=>__('Add New Item'), 'class' => 'btn btn-success pull-right')); ?>
 <div class='clearfix'></div><br/>
 
 	<?php echo Form::open(Route::get('admin/menu/item')->uri(array('action' => 'confirm', 'id' => $id)), array('id'=>'menu-form', 'class'=>'form')); ?>
@@ -38,8 +38,8 @@
 				</td>
 
 				<td class="action">
-					<?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'edit', 'id' => $item['id'])), '<i class="btn-icon-only icon-edit"></i>', array('class' => 'btn btn-small', 'title' => __('Edit Item'))) ?>
-					<?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'delete', 'id' => $item['id'])), '<i class="btn-icon-only icon-trash"></i>', array('class' => 'btn btn-small', 'title' => __('Delete Item'))) ?>
+					<?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'edit', 'id' => $item['id'])), '<i class="fa fa-edit"></i>', array('class' => 'btn btn-sm btn-default', 'title' => __('Edit Item'))) ?>
+					<?php echo HTML::anchor(Route::get('admin/menu/item')->uri(array('action' => 'delete', 'id' => $item['id'])), '<i class="fa fa-trash-o"></i>', array('class' => 'btn btn-sm btn-default', 'title' => __('Delete Item'))) ?>
 				</td>
 			  </tr>
 			<?php endforeach ?>
