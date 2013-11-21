05566d74e998ed22fbbb1a3384bb1c1dfa5b5b29
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-22 00:17:23 +0530

    Update feature: admin/user/form with BS3 parity

--- modules/user/views/admin/user/form.php
+++ modules/user/views/admin/user/form.php
@@ -2,33 +2,43 @@
 	$parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add');
 	$destination = isset($GET['destination']) ? $GET['destination']: '';
 
-	echo Form::open( Route::get('admin/user')->uri($parms).URL::query(array($destination)), array('id'=>'user-form ', 'class'=>'user-form form'));
+	echo Form::open( Route::get('admin/user')->uri($parms).URL::query(array($destination)), array('id'=>'user-form', 'class'=>'user-form form'));
 	include Kohana::find_file('views', 'errors/partial');
 ?>
 
-<div class="control-group <?php echo isset($errors['name']) ? 'error': ''; ?>">
-	<?php echo Form::label('username', __('User name')) ?>
-	<?php print Form::input('name', $post->name, array('class' => 'text small')); ?>
+<div class="form-group <?php echo isset($errors['name']) ? 'has-error': ''; ?>">
+	<?php echo Form::label('username', __('User name'), array('class' => 'control-label col-sm-12')) ?>
+	<div class="controls col-sm-5">
+	<?php print Form::input('name', $post->name, array('class' => 'form-control')); ?>
+	</div>
 </div>
 
-<div class="control-group <?php echo isset($errors['pass']) ? 'error': ''; ?>">
-	<?php echo Form::label('password', __('Password')) ?>
-	<?php echo Form::Password('pass', null, array('class' => 'text small')) ?>
+<div class="form-group <?php echo isset($errors['pass']) ? 'has-error': ''; ?>">
+	<?php echo Form::label('password', __('Password'), array('class' => 'control-label col-sm-12')) ?>
+	<div class="controls col-sm-5">
+	<?php echo Form::Password('pass', null, array('class' => 'form-control')) ?>
+	</div>
 </div>
 
-<div class="control-group <?php echo isset($errors['nick']) ? 'error': ''; ?>">
-	<?php echo Form::label('nick', __('Nick name')) ?>
-	<?php print Form::input('nick', $post->nick, array('class' => 'text small')); ?>
+<div class="form-group <?php echo isset($errors['nick']) ? 'has-error': ''; ?>">
+	<?php echo Form::label('nick', __('Nick name'), array('class' => 'control-label col-sm-12')) ?>
+	<div class="controls col-sm-5">
+	<?php print Form::input('nick', $post->nick, array('class' => 'form-control')); ?>
+	</div>
 </div>
 
-<div class="control-group <?php echo isset($errors['mail']) ? 'error': ''; ?>">
-	<?php echo Form::label('mail', __('Email')) ?>
-	<?php print Form::input('mail', $post->mail, array('class' => 'text small')); ?>
+<div class="form-group <?php echo isset($errors['mail']) ? 'has-error': ''; ?>">
+	<?php echo Form::label('mail', __('Email'), array('class' => 'control-label col-sm-12')) ?>
+	<div class="controls col-sm-5">
+	<?php print Form::input('mail', $post->mail, array('class' => 'form-control')); ?>
+	</div>
 </div>
 
-<div class="control-group <?php echo isset($errors['status']) ? 'error': ''; ?>">
-	<?php echo Form::label('active', __('Status')) ?>
-	<?php print Form::select('status', array(0 => __('Blocked'), 1 => __('Active')), $post->status, array('class' => 'list small')); ?>
+<div class="form-group <?php echo isset($errors['status']) ? 'has-error': ''; ?>">
+	<?php echo Form::label('active', __('Status'), array('class' => 'control-label col-sm-12')) ?>
+	<div class="controls col-sm-5">
+	<?php print Form::select('status', array(0 => __('Blocked'), 1 => __('Active')), $post->status, array('class' => 'form-control')); ?>
+	</div>
 </div>
 
 <table class="table table-striped table-bordered table-highlight">
