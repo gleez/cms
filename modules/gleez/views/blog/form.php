da0711482bc72ce45e6fafa0a3e3716fbfa584a8
Gleez Technologies <github@gleez.tech>
2013-11-22 21:51:11 +0530

    Update feature: Use panels and BS3 fixes for blog/add form

--- modules/gleez/views/blog/form.php
+++ modules/gleez/views/blog/form.php
@@ -79,10 +79,11 @@
 
 		<div id="side-info-column" class="col-md-3">
 			<?php if (ACL::check('administer content') OR ACL::check('administer page')): ?>
-				<div id="submitdiv" class="postbox">
-					<h3 class='hndle'><?php echo __('Publication') ?></h3>
-
-					<div class='inside' id="submitpost">
+				<div id="submitdiv" class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title"><?php echo __('Publication') ?></h3>
+				</div>
+					<div class='panel-body' id="submitpost">
 						<div id="minor-publishing">
 							<div class="form-group <?php echo isset($errors['status']) ? 'has-error': ''; ?>">
 								<?php echo Form::label('status', __('Status'), array('class' => 'control-label')) ?>
@@ -96,11 +97,11 @@
 									echo Form::hidden('sticky', 0);
 									echo Form::hidden('promote', 0);
 								?>
-								<div class="controls">
-									<?php echo Form::label('sticky', Form::checkbox('sticky', TRUE, $sticky).__('Sticky this Post'), array('class' => 'checkbox')) ?>
+								<div class="controls checkbox">
+									<?php echo Form::label('sticky', Form::checkbox('sticky', TRUE, $sticky).__('Sticky this Post')) ?>
 								</div>
-								<div class="controls">
-									<?php echo Form::label('promote', Form::checkbox('promote', TRUE, $promote).__('Promote this Post'), array('class' => 'checkbox')) ?>
+								<div class="controls checkbox">
+									<?php echo Form::label('promote', Form::checkbox('promote', TRUE, $promote).__('Promote this Post')) ?>
 								</div>
 							</div>
 
@@ -120,11 +121,12 @@
 								</div>
 							<?php endif; ?>
 						</div>
-
+					</div>
+						<div class="panel-footer">
 						<div id="major-publishing-actions" class="row">
-							<?php if ($blog->loaded() AND ACL::post('delete', $blog)): ?>
-								<div id="delete-action" class="pull-left">
-									<i class="fa fa-trash"></i>
+							<?php if ($blog->loaded() AND ACL::post('delete', $post)): ?>
+								<div id="delete-action" class="btn btn-default pull-left">
+									<i class="fa fa-trash-o"></i>
 									<?php echo HTML::anchor($blog->delete_url.URL::query($destination), __('Move to Trash'), array('class' => 'submitdelete')) ?>
 								</div>
 							<?php endif; ?>
@@ -133,14 +135,17 @@
 								<?php echo Form::submit('blog', __('Save'), array('class' => 'btn btn-success pull-right')) ?>
 							</div>
 						</div>
+
 					</div>
 				</div>
 			<?php endif; ?>
 
 			<?php if($config->use_category) : ?>
-				<div id="categorydiv" class="postbox">
-					<h3 class='hndle'><?php echo __('Category'); ?></h3>
-					<div class='inside'>
+				<div id="categorydiv" class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"><?php echo __('Category'); ?></h3>
+				</div>
+					<div class='panel-body'>
 						<div class="form-group <?php echo isset($errors['categories']) ? 'has-error': ''; ?>">
 							<?php echo Form::select('categories[1]', $terms, $blog->terms_form, array('class' => 'form-control')); ?>
 						</div>
@@ -149,10 +154,12 @@
 			<?php endif; ?>
 
 			<?php if( $config->use_comment) : ?>
-				<div id="commentdiv" class="postbox">
-					<h3 class='hndle'><?php echo  __('Comments'); ?></h3>
+				<div id="commentdiv" class="panel panel-default">
+					<div class="panel-heading">
+					<h3 class="panel-title"><?php echo  __('Comments'); ?></h3>
+					</div>
 
-					<div class='inside'>
+					<div class='panel-body'>
 						<div class="form-group <?php echo isset($errors['comment']) ? 'has-error': ''; ?>">
 							<?php
 								if ( ! isset($blog->comment))
@@ -166,16 +173,16 @@
 							?>
 
 							<?php echo Form::label('comment', __('Discussion') ) ?>
-							<div class="controls">
-								<?php echo Form::label('comment', Form::radio('comment', 0, $comment1).__('Disabled'), array('class' => 'radio')) ?>
+							<div class="controls radio">
+								<?php echo Form::label('comment', Form::radio('comment', 0, $comment1).__('Disabled')) ?>
 							</div>
 
-							<div class="controls">
-								<?php echo Form::label('comment', Form::radio('comment', 1, $comment2).__('Read only'), array('class' => 'radio')) ?>
+							<div class="controls radio">
+								<?php echo Form::label('comment', Form::radio('comment', 1, $comment2).__('Read only')) ?>
 							</div>
 
-							<div class="controls">
-								<?php echo Form::label('comment', Form::radio('comment', 2, $comment3).__('Read/Write'), array('class' => 'radio')) ?>
+							<div class="controls radio">
+								<?php echo Form::label('comment', Form::radio('comment', 2, $comment3).__('Read/Write')) ?>
 							</div>
 
 						</div>
