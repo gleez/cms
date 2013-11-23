495e93ea8fcd93b1a975ce1e033ee2963d9c69b2
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-11-23 21:30:09 +0530

    Update feature: Update default admin layout

--- modules/gleez/views/layouts/admin.php
+++ modules/gleez/views/layouts/admin.php
@@ -7,61 +7,42 @@
 	<?php echo Meta::links(); ?>
 	<!-- HTML5 shim and Respond.js, for IE6-8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
-		<script src="/media/js/html5.js"></script>
-		<script src="/media/js/respond.min.js"></script>
+		<?php echo HTML::script('media/js/html5.js', NULL, TRUE); ?>
+		<?php echo HTML::script('media/js/respond.min.js', NULL, TRUE); ?>
 	<![endif]-->
 	<?php echo Assets::css(); ?>
 </head>
 <body id="<?php echo $page_id; ?>" class="<?php echo $page_class; ?>">
 
-
 	<!-- ########## Navbar start ########## -->
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      	<div class="admin-nav container">
-	        <div class="navbar-header">
-	          	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-		            <span class="sr-only">Toggle navigation</span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-		            <span class="icon-bar"></span>
-	          	</button>
-	          	<?php echo HTML::anchor('/', $site_name, array('class' => 'navbar-brand', 'title' => $site_name)) ?>
-	        </div>
-        	<div class="navbar-collapse collapse">
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="admin-nav container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only"><?php _e('Toggle navigation'); ?></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<?php echo HTML::anchor($site_url, $site_name, array('class' => 'navbar-brand', 'title' => $site_name)) ?>
+			</div>
+			<div class="navbar-collapse collapse">
 
-
-          		<ul class="nav navbar-nav navbar-right">
-						<?php if (User::is_guest()): ?>
-							<?php if (Kohana::$config->load('auth')->get('register')): ?>
-								<li><a href="<?php echo URL::site('/user/register'); ?>"><?php echo __('Sign Up')?></a></li>
-							<?php endif; ?>
-							<li><a href="<?php echo URL::site('/user/login'); ?>"><i class="fa fa-chevron-left"></i><?php echo __('Sign In') ?></a></li>
-						<?php else:  ?>
-						<li class="dropdown">
-							<a data-toggle="dropdown" class="dropdown-toggle" href="#">
-								<i class="fa fa-user"></i><?php echo $_user->nick; ?><b class="caret"></b>
-							</a>
-
-							<ul class="dropdown-menu">
-								<?php if (User::is_admin()): ?>
-									<li><a href="<?php echo URL::site('/admin') ?>"><i class="fa fa-dashboard"></i> <?php echo __('Dashboard') ?></a></li>
-									<li class="divider"></li>
-								<?php endif; ?>
-								<li><a href="<?php echo URL::site('/user/profile') ?>"><i class="fa fa-cog"></i> <?php echo __('Profile') ?></a></li>
-								<li><a href="<?php echo URL::site("/user/edit") ?>"><i class="fa fa-pencil"></i> <?php echo __('Account') ?></a></li>
-								<li class="divider"></li>
-								<li><a href="<?php echo URL::site('/user/logout'); ?>"><i class="fa fa-power-off"></i> <?php echo __('Sign Out') ?></a></li>
-							</ul>
-						</li>
-						<?php endif; ?>
-          		</ul>
-        	</div><!--/.nav-collapse -->
-      	</div>
-    </div>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a href="<?php echo URL::site('/user/profile'); ?>"><i class="fa fa-user"></i> <?php echo $_user->nick; ?></a>
+					</li>
+					<li>
+						<a href="<?php echo URL::site('/user/logout'); ?>" title="<?php echo __('Sign Out') ?>"><i class="fa fa-power-off"></i></a>
+					</li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
 	<!-- ########## Navbar end ########## -->
 
 	<!-- ########## admin / container start ########## -->
-	<div class="admin-container container" itemscope itemtype="http://schema.org/WebPage">
+	<div class="admin-container container">
 		<?php include Kohana::find_file('views', 'admin.tpl'); ?>
 	</div>
 	<!-- ########## template / container end ########## -->
