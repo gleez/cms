07a8e252ab62b3dccd9243c52f2f7c0162e21a51
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-25 21:41:16 +0400

    Updated feature: Renamed Vocabulary and Terms to Groups and Categories into Views

--- modules/gleez/views/admin/taxonomy/form.php
+++ modules/gleez/views/admin/taxonomy/form.php
@@ -1,14 +1,14 @@
 <div class="help">
-	<?php _e('Add new vocabulary to your site, edit and reorganize existing vocabularies.') ?>
+	<?php _e('Add new category groups to your site, edit and reorganize existing categories.') ?>
 </div>
 
 <?php $params = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add');
-	echo Form::open(Route::get('admin/taxonomy')->uri($params), array('id'=>'vocab-form', 'class'=>'vocab-form form form-horizontal well clearfix')) ?>
+	echo Form::open(Route::get('admin/taxonomy')->uri($params), array('id'=>'vocab-form', 'class'=>'form form-horizontal well', 'role' => 'form')) ?>
 
 	<?php include Kohana::find_file('views', 'errors/partial'); ?>
 
 	<div class="form-group <?php echo isset($errors['name']) ? 'has-error': ''; ?>">
-		<?php echo Form::label('name', __('Name'), array('class' => 'control-label col-md-3')) ?>
+		<?php echo Form::label('name', __('Group Name'), array('class' => 'control-label col-md-3')) ?>
 		<div class="controls col-md-5">
 			<?php echo Form::input('name', $post->rawname, array('class' => 'form-control')); ?>
 		</div>
@@ -18,15 +18,21 @@
 		<?php echo Form::label('type', __('Type'), array('class' => 'control-label col-md-3')) ?>
 		<div class="controls col-md-5">
 			<?php echo Form::select('type', Gleez::types(), $post->type, array('class' => 'form-control')); ?>
+			<span class="help-block"><?php _e('For what type of content you intend to use categories from this group?') ?></span>
 		</div>
 	</div>
-	
+
 	<div class="form-group <?php echo isset($errors['description']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('description', __('Description'), array('class' => 'control-label col-md-3')) ?>
 		<div class="controls col-md-5">
 			<?php echo Form::textarea('description', $post->description, array('class' => 'form-control', 'rows' => 5)) ?>
+			<span class="help-block"><?php _e('Description not visible by default, however some themes may show it. The main purpose of this description - to inform administrator about the group.') ?></span>
 		</div>
 	</div>
 
-	<?php echo Form::submit('vocab', __('Save'), array('class' => 'btn btn-success pull-right')) ?>
+	<div class="form-group ab-wrapper">
+		<div class="col-md-12">
+			<?php echo Form::button('vocab', __('Save'), array('class' => 'btn btn-success pull-right', 'type' => 'submit'))?>
+		</div>
+	</div>
 <?php echo Form::close() ?>
