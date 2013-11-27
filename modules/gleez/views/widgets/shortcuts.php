61a488d49782d118ec4a21b652af519235921474
Gleez Technologies <gleez@server.gleeztech.org>
2013-11-27 21:13:22 +0530

    Fixed issue: Fixed with admin shortcuts loop.

--- modules/gleez/views/widgets/shortcuts.php
+++ modules/gleez/views/widgets/shortcuts.php
@@ -1,10 +1,10 @@
 <div class="shortcuts">
-	<?php foreach ($items as $key => $item): ?>
-		<ul class="quick-actions">
+	<ul class="quick-actions">
+		<?php foreach ($items as $key => $item): ?>
 			<li>
 				<?php $title = '<i class="shortcut-icon fa '.$item['image'].' fa-2x"></i></br><span class="shortcut-label">'.Text::plain($item['title']).'</span>'; ?>
 				<?php echo HTML::anchor($item['url'], $title, array('class' => 'shortcut')); ?>
 			</li>
-		</ul>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+	</ul>
 </div>
