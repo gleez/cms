d9297445bd96037537d6acf46914bead47a2de7b
Gleez Technologies <gleez@server.gleeztech.org>
2013-11-27 17:55:23 +0530

    Fixed issue: Better admin shortcuts on dashboard

--- modules/gleez/views/widgets/shortcuts.php
+++ modules/gleez/views/widgets/shortcuts.php
@@ -1,6 +1,10 @@
 <div class="shortcuts">
 	<?php foreach ($items as $key => $item): ?>
-		<?php $title = '<i class="shortcut-icon fa '.$item['image'].'"></i><span class="shortcut-label">'.Text::plain($item['title']).'</span>'; ?>
-		<?php echo HTML::anchor($item['url'], $title, array('class' => 'shortcut')); ?>
+		<ul class="quick-actions">
+			<li>
+				<?php $title = '<i class="shortcut-icon fa '.$item['image'].' fa-2x"></i></br><span class="shortcut-label">'.Text::plain($item['title']).'</span>'; ?>
+				<?php echo HTML::anchor($item['url'], $title, array('class' => 'shortcut')); ?>
+			</li>
+		</ul>
 	<?php endforeach; ?>
 </div>
