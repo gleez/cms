26ead134b0a664db92d6c6ca72404d8655b2c045
Gleez Technologies <github@gleez.tech>
2013-12-01 21:52:40 +0530

    Update feature: Fix admin page settings form element wrappers

--- modules/gleez/views/admin/page/settings.php
+++ modules/gleez/views/admin/page/settings.php
@@ -28,14 +28,14 @@
 
 <?php echo Form::open($action, array('class'=>'page-settings-form form form-horizontal')) ?>
 
-	<div class="form-group <?php echo isset($errors['items_per_page']) ? 'error': ''; ?>">
+	<div class="form-group <?php echo isset($errors['items_per_page']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('title', __('Page entries per page'), array('class' => 'control-label col-sm-3')) ?>
 		<div class="controls col-sm-4">
 			<?php echo Form::select('items_per_page', HTML::per_page(), $post['items_per_page'], array('class' => 'form-control')); ?>
 		</div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['default_status']) ? 'error': ''; ?>">
+	<div class="form-group <?php echo isset($errors['default_status']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('default_status', __('Default Page Status'), array('class' => 'control-label col-sm-3')) ?>
 		<div class="controls col-sm-4">
 			<?php echo Form::select('default_status', Post::status(), isset($post['default_status']) ? $post['default_status'] : NULL, array('class' => 'form-control')); ?>
@@ -56,43 +56,86 @@
 			echo Form::hidden('comment_anonymous', 0);
 		?>
 		<div class="controls set-check">
-			<?php
-				echo Form::label('use_excerpt',       Form::checkbox('use_excerpt', TRUE, $use_excerpt).__('Enable excerpt'), array('class' => 'checkbox'));
-				echo Form::label('use_comment',       Form::checkbox('use_comment', TRUE, $use_comment).__('Enable comments'), array('class' => 'checkbox'));
-				echo Form::label('use_authors',       Form::checkbox('use_authors', TRUE, $use_authors).__('Enable authors'), array('class' => 'checkbox'));
-				echo Form::label('use_captcha',       Form::checkbox('use_captcha', TRUE, $use_captcha).__('Enable captcha'), array('class' => 'checkbox'));
-				echo Form::label('use_category',      Form::checkbox('use_category', TRUE, $use_category).__('Enable Category'), array('class' => 'checkbox'));
-				echo Form::label('use_tags',          Form::checkbox('use_tags', TRUE, $use_tags).__('Enable tag cloud'), array('class' => 'checkbox'));
-				echo Form::label('use_submitted',     Form::checkbox('use_submitted', TRUE, $use_submitted).__('Show Submitted Info'), array('class' => 'checkbox'));
-				echo Form::label('use_cache',         Form::checkbox('use_cache', TRUE, $use_cache).__('Enable Page Cache'), array('class' => 'checkbox'));
-				echo Form::label('comment_anonymous', Form::checkbox('comment_anonymous', TRUE, $comment_anonymous).__('Allow anonymous commenting (with contact information)'), array('class' => 'checkbox'));
-			?>
+			<div class="checkbox">
+				<?php echo Form::label('use_excerpt',       Form::checkbox('use_excerpt', TRUE, $use_excerpt).__('Enable excerpt'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_comment',       Form::checkbox('use_comment', TRUE, $use_comment).__('Enable comments'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_authors',       Form::checkbox('use_authors', TRUE, $use_authors).__('Enable authors'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_captcha',       Form::checkbox('use_captcha', TRUE, $use_captcha).__('Enable captcha'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_category',      Form::checkbox('use_category', TRUE, $use_category).__('Enable Category'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_tags',          Form::checkbox('use_tags', TRUE, $use_tags).__('Enable tag cloud'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_submitted',     Form::checkbox('use_submitted', TRUE, $use_submitted).__('Show Submitted Info'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('use_cache',         Form::checkbox('use_cache', TRUE, $use_cache).__('Enable Page Cache'));?>
+			</div>
+			
+			<div class="checkbox">
+				<?php echo Form::label('comment_anonymous', Form::checkbox('comment_anonymous', TRUE, $comment_anonymous).__('Allow anonymous commenting (with contact information)'));?>
+			</div>
 		</div>
 	</div>
 
 	<hr>
 
-	<div class="form-group <?php echo isset($errors['comment']) ? 'error': ''; ?>">
+	<div class="form-group <?php echo isset($errors['comment']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('comment', __('Allow people to post comments'), array('class' => 'control-label col-sm-3')) ?>
 		<div class="controls col-sm-4">
-			<?php echo Form::label('comment', Form::radio('comment', 0, $comment1).__('Disabled'), array('class' => 'radio')) ?>
-			<?php echo Form::label('comment', Form::radio('comment', 1, $comment2).__('Read only'), array('class' => 'radio')) ?>
-			<?php echo Form::label('comment', Form::radio('comment', 2, $comment3).__('Read/Write'), array('class' => 'radio')) ?>
+			<div class="radio">
+				<?php echo Form::label('comment', Form::radio('comment', 0, $comment1).__('Disabled')) ?>
+			</div>
+			
+			<div class="radio">
+				<?php echo Form::label('comment', Form::radio('comment', 1, $comment2).__('Read only')) ?>
+			</div>
+			
+			<div class="radio">
+				<?php echo Form::label('comment', Form::radio('comment', 2, $comment3).__('Read/Write')) ?>
+			</div>
 			<p class="help-block"><?php echo __('These settings may be overridden for individual posts.') ?></p>
 		</div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['comment_default_mode']) ? 'error': ''; ?>">
+	<div class="form-group <?php echo isset($errors['comment_default_mode']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('comment_default_mode', __('Comment display mode'), array('class' => 'control-label col-sm-3')) ?>
 		<div class="controls col-sm-4">
-			<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 1, $mode1).__('Flat list &mdash; collapsed'), array('class' => 'radio')) ?>
-			<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 2, $mode2).__('Flat list &mdash; expanded'), array('class' => 'radio')) ?>
-			<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 3, $mode3).__('Threaded list &mdash; collapsed'), array('class' => 'radio')) ?>
-			<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 4, $mode4).__('Threaded list &mdash; expanded'), array('class' => 'radio')) ?>
+			<div class="radio">
+				<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 1, $mode1).__('Flat list &mdash; collapsed')) ?>
+			</div>
+			
+			<div class="radio">
+				<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 2, $mode2).__('Flat list &mdash; expanded')) ?>
+			</div>
+			
+			<div class="radio">
+				<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 3, $mode3).__('Threaded list &mdash; collapsed')) ?>
+			</div>
+			
+			<div class="radio">
+				<?php echo Form::label('comment_default_mode', Form::radio('comment_default_mode', 4, $mode4).__('Threaded list &mdash; expanded')) ?>
+			</div>
 		</div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['comment_order']) ? 'error': ''; ?>">
+	<div class="form-group <?php echo isset($errors['comment_order']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('comment_order', __('Comment Order'), array('class' => 'control-label col-sm-3')) ?>
 		<div class="controls col-sm-4">
 			<?php echo Form::select('comment_order', array('asc'=> __('Older'), 'desc'=>__('Newer')), isset($post['comment_order']) ? $post['comment_order'] : 'asc', array('class' => 'form-control')); ?>
@@ -100,7 +143,7 @@
 		</div>
 	</div>
 
-	<div class="form-group <?php echo isset($errors['comments_per_page']) ? 'error': ''; ?>">
+	<div class="form-group <?php echo isset($errors['comments_per_page']) ? 'has-error': ''; ?>">
 		<?php echo Form::label('comments_per_page', __('Comments per page'), array('class' => 'control-label col-sm-3')) ?>
 		<div class="controls col-sm-4">
 			<?php echo Form::select('comments_per_page', HTML::per_page(), isset($post['comments_per_page']) ? $post['comments_per_page'] : 50, array('class' => 'form-control')); ?>
