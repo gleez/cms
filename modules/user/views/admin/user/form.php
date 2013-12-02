084216504c5fd87f1819395362accbcbefcb331f
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-03 02:59:04 +0400

    Fixed issue: Cleanup user form

--- modules/user/views/admin/user/form.php
+++ modules/user/views/admin/user/form.php
@@ -2,46 +2,46 @@
 	$parms = isset($post->id) ? array('id' => $post->id, 'action' => 'edit') : array('action' => 'add');
 	$destination = isset($GET['destination']) ? $GET['destination']: '';
 
-	echo Form::open( Route::get('admin/user')->uri($parms).URL::query(array($destination)), array('id'=>'user-form', 'class'=>'user-form form'));
+	echo Form::open( Route::get('admin/user')->uri($parms).URL::query(array($destination)), array('id'=>'user-form', 'class'=>'user-form form form-horizontal', 'role' => 'form'));
 	include Kohana::find_file('views', 'errors/partial');
 ?>
 
 <div class="form-group <?php echo isset($errors['name']) ? 'has-error': ''; ?>">
-	<?php echo Form::label('username', __('User name'), array('class' => 'control-label col-sm-12')) ?>
+	<?php echo Form::label('username', __('User name'), array('class' => 'control-label col-sm-3')) ?>
 	<div class="controls col-sm-5">
 	<?php print Form::input('name', $post->name, array('class' => 'form-control')); ?>
 	</div>
 </div>
 
 <div class="form-group <?php echo isset($errors['pass']) ? 'has-error': ''; ?>">
-	<?php echo Form::label('password', __('Password'), array('class' => 'control-label col-sm-12')) ?>
+	<?php echo Form::label('password', __('Password'), array('class' => 'control-label col-sm-3')) ?>
 	<div class="controls col-sm-5">
 	<?php echo Form::Password('pass', null, array('class' => 'form-control')) ?>
 	</div>
 </div>
 
 <div class="form-group <?php echo isset($errors['nick']) ? 'has-error': ''; ?>">
-	<?php echo Form::label('nick', __('Nick name'), array('class' => 'control-label col-sm-12')) ?>
+	<?php echo Form::label('nick', __('Nick name'), array('class' => 'control-label col-sm-3')) ?>
 	<div class="controls col-sm-5">
 	<?php print Form::input('nick', $post->nick, array('class' => 'form-control')); ?>
 	</div>
 </div>
 
 <div class="form-group <?php echo isset($errors['mail']) ? 'has-error': ''; ?>">
-	<?php echo Form::label('mail', __('Email'), array('class' => 'control-label col-sm-12')) ?>
+	<?php echo Form::label('mail', __('Email'), array('class' => 'control-label col-sm-3')) ?>
 	<div class="controls col-sm-5">
 	<?php print Form::input('mail', $post->mail, array('class' => 'form-control')); ?>
 	</div>
 </div>
 
 <div class="form-group <?php echo isset($errors['status']) ? 'has-error': ''; ?>">
-	<?php echo Form::label('active', __('Status'), array('class' => 'control-label col-sm-12')) ?>
+	<?php echo Form::label('active', __('Status'), array('class' => 'control-label col-sm-3')) ?>
 	<div class="controls col-sm-5">
 	<?php print Form::select('status', array(0 => __('Blocked'), 1 => __('Active')), $post->status, array('class' => 'form-control')); ?>
 	</div>
 </div>
 
-<table class="table table-striped table-bordered table-highlight">
+<table class="table table-striped table-bordered table-apparent">
 	<thead>
 		<tr>
 			<th>#</th>
@@ -59,6 +59,11 @@
 </table>
 
 <?php echo Form::hidden('site_url', URL::site(NULL, TRUE), array('id'=>'site_url')) ?>
-<?php echo Form::submit('user', __('Save'), array('class' => 'btn btn-success pull-right')) ?>
+
+<div class="form-group ab-wrapper">
+	<div class="col-sm-12">
+		<?php echo Form::button('user', __('Save'), array('class' => 'btn btn-success pull-right', 'type' => 'submit'))?>
+	</div>
+</div>
 
 <?php echo Form::close(); ?>
