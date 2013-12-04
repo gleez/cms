f76d725a936dc1153a9146f3d6268fcf150be7f7
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-05 01:52:37 +0400

    Fixed issue: Added some messages for Confirm Reset Password form

--- modules/user/messages/models/_external.php
+++ modules/user/messages/models/_external.php
@@ -3,11 +3,11 @@
 return array(
 	'pass' => array(
 		'not_empty' => 'You must provide a password',
-		'min_length' => 'Password must be at least :param2 characters long',
+		'min_length' => ':field must be at least :param2 characters long',
 	),
 	'pass_confirm' => array (
 		'not_empty' => 'You must confirm password',
-		'matches' => 'Password Confirm must be the same as Password',
+		'matches' => ':field must be the same as :param3',
 	),
 	'old_pass' => array(
 		'not_empty' => 'You must provide old password',
