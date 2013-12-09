35f540e1a6b05c69e8fc207f51423bf148b737ee
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-09 13:53:53 +0400

    Updated feature: Amended System Info Widget

--- modules/gleez/views/widgets/systeminfo.php
+++ modules/gleez/views/widgets/systeminfo.php
@@ -17,16 +17,56 @@
 			<td><?php echo HTML::chars(php_uname("n")); ?></td>
 		</tr>
 		<tr>
-			<th><?php _e('Webserver') ?></th>
-			<td><?php echo $_SERVER['SERVER_SOFTWARE']; ?></td>
+			<th><?php _e('Server Software') ?></th>
+			<td><?php echo isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : __('Not available'); ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('Server Signature') ?></th>
+			<td><?php echo isset($_SERVER['SERVER_SIGNATURE']) ? $_SERVER['SERVER_SIGNATURE'] : __('Not available'); ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('Server Name') ?></th>
+			<td><?php echo isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : __('Not available'); ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('Server Address') ?></th>
+			<td><?php
+					// Windows running IIS v6 does not include $_SERVER['SERVER_ADDR']
+					echo gethostbyname($_SERVER['SERVER_NAME']);
+				?>
+			</td>
+		</tr>
+		<tr>
+			<th><?php _e('Server Port') ?></th>
+			<td><?php echo isset($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : __('Not available'); ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('OS Type') ?></th>
+			<td><?php echo php_uname(); ?></td>
 		</tr>
 		<tr>
 			<th><?php _e('PHP Version') ?></th>
 			<td><?php echo HTML::chars(PHP_VERSION); ?></td>
 		</tr>
 		<tr>
+			<th><?php _e('Zend Engine Version') ?></th>
+			<td><?php echo zend_version(); ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('Platform') ?></th>
+			<td><?php echo (PHP_INT_SIZE * 8).'Bit'; ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('Loaded Extensions') ?></th>
+			<td><?php echo implode(', ', get_loaded_extensions()); ?></td>
+		</tr>
+		<tr>
 			<th><?php _e('MySQL Version') ?></th>
 			<td><?php echo HTML::chars(Database::instance()->version(TRUE)); ?></td>
+		</tr>
+		<tr>
+			<th><?php _e('Memory Limit') ?></th>
+			<td><?php echo System::get_memory_limit(); ?></td>
 		</tr>
 		<tr>
 			<th><?php _e('Load Average') ?></th>
