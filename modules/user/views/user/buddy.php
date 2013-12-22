6cf39e5c9c0c96901051e9128eac7ad3a978400b
Gleez Technologies <github@gleez.tech>
2013-12-22 19:23:20 +0530

    Update feature: Minor changes to Buddy

--- modules/user/views/user/buddy.php
+++ modules/user/views/user/buddy.php
@@ -2,16 +2,16 @@
 	<div class="row vcard">
 	    <div class="list-group list-all panel panel-default">
 		
-		<div class="panel-heading"><h3 class="panel-title">Friends</h3></div>
+		<div class="panel-heading"><h3 class="panel-title"><?php echo __('Friends (%total)', array('%total' => $total)); ?></h3></div>
 		
 		<?php foreach($friends as $id): ?>
 			<div class="list-group-item allusers panel-body">
 				<?php $accept = User::lookup($id); ?>
 				<div class="col-md-5">
-					<?php echo HTML::anchor("user/view/".$accept->id , User::getAvatar($accept), array('class' => 'action-view', 'size' => 80, 'title'=> __('view profile'))) ?>
+					<?php echo HTML::anchor("user/view/".$accept->id , User::getAvatar($accept), array('class' => 'action-view', 'size' => 80, 'title'=> __('View profile'))) ?>
 				</div>
 				<div class="col-md-6">
-					<?php echo HTML::anchor("user/view/".$accept->id , $accept->nick, array('class' => 'action-view', 'title'=> __('view profile'))) ?></br>
+					<?php echo HTML::anchor("user/view/".$accept->id , $accept->nick, array('class' => 'action-view', 'title'=> __('View profile'))) ?></br>
 					<?php echo HTML::anchor("#", $accept->mail, array('title'=> __('mail'))) ?></br>
 					<?php echo ($accept->dob != 0)? $accept->dob : '__'; ?></br>
 					<?php echo HTML::anchor("$accept->homepage", $accept->homepage) ?></br>
