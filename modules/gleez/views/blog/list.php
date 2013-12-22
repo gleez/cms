1b62cc3ab6b9f0cec1e36ce79e7ab72bdcd6c86b
Sandeep Sangamreddi <sandeepone@gmail.com>
2013-12-22 20:54:06 +0530

    Update feature: Moved cerber/blog/view gleez/blog/view

--- modules/gleez/views/blog/list.php
+++ modules/gleez/views/blog/list.php
@@ -1,25 +1,26 @@
 <div class="row">
 	<div class="col-md-1 pull-right">
-		<?php echo HTML::icon($rss_link, 'fa-rss', array('title' => 'RSS 2.0', 'class' => 'pull-right')); ?>
+		<?php echo HTML::icon($rss_link, 'fa-rss', array('title' => 'RSS 2.0', 'class' => 'post-rss')) ?>
 	</div>
 </div>
 <?php foreach($posts as $i => $post): ?>
-	<div id="blog-<?php echo $post->id; ?>" class="blog-list <?php echo ($post->sticky) ? ' sticky' : ' blog-'.$post->status; ?>">
-		<div class="title-holder">
-			<h2 class="blog-title">
-				<?php echo HTML::anchor($post->url, $post->title); ?>
+	<article id="blog-<?php echo $post->id; ?>" class="blog-list <?php echo ($post->sticky) ? ' sticky' : ' blog-'.$post->status; ?>" itemscope itemtype="http://schema.org/Article">
+		<header>
+			<h2 class="post-title" itemprop="name">
+				<?php echo HTML::anchor($post->url, $post->title, array('itemprop' => 'url')); ?>
 			</h2>
-		</div>
+		</header>
+
 		<?php if ($post->promote): ?>
 			<i class="post-bookmark clearfix"></i>
 		<?php endif; ?>
 		<?php
-			echo View::factory($post->type.'/teaser')
-				->set('post',       $post)
-				->set('config',     $config)
-				->set('page_title', TRUE);
+		echo View::factory('blog/teaser')
+			->set('post',       $post)
+			->set('config',     $config)
+			->set('page_title', TRUE);
 		?>
-	</div>
+	</article>
 <?php endforeach; ?>
 
 <?php echo $pagination; ?>
