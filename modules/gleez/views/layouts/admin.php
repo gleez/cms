76745466fb556f7ed264baf7d7055b71d194beb1
Gleez Technologies <github@gleez.tech>
2013-12-22 20:56:40 +0530

    Update feature: Moved cerber/layouts to gleez/layouts

--- modules/gleez/views/layouts/admin.php
+++ modules/gleez/views/layouts/admin.php
@@ -10,6 +10,9 @@
 		<?php echo HTML::script('media/js/html5shiv.js', NULL, TRUE); ?>
 		<?php echo HTML::script('media/js/respond.min.js', NULL, TRUE); ?>
 	<![endif]-->
+	<!--[if gte IE 9]>
+		<?php echo HTML::script('media/css/ie-gte-9.css', NULL, TRUE); ?>
+	<![endif]-->
 	<?php echo Assets::css(); ?>
 </head>
 <body id="<?php echo $page_id; ?>" class="<?php echo $page_class; ?>">
