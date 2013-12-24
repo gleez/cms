7de8d72d24c4b49bb77424bc74681227fec7093f
Sergey Yakovlev <sadhooklay@gmail.com>
2013-12-24 19:47:06 +0400

    Updated feature: Added Message menu item

--- modules/gleez/views/layouts/welcome.php
+++ modules/gleez/views/layouts/welcome.php
@@ -46,9 +46,10 @@
 								<li class="divider"></li>
 								<li class="dropdown-header"><?php _e('Profile') ?></li>
 								<li><a href="<?php echo URL::site('/user/profile') ?>"><i class="fa fa-fw fa-cog"></i> <?php echo __('My Profile') ?></a></li>
+								<li><a href="<?php echo URL::site('/message/inbox') ?>"><i class="fa fa-fw fa-envelope"></i> <?php echo __('My Messages') ?></a></li>
 								<li class="dropdown-header"><?php _e('Settings') ?></li>
-								<li><a href="<?php echo URL::site("/user/edit") ?>"><i class="fa fa-fw fa-pencil"></i> <?php echo __('Profile Settings') ?></a></li>
-								<li><a href="<?php echo URL::site("/user/password") ?>"><i class="fa fa-fw fa-lock"></i> <?php echo __('Change Password') ?></a></li>
+								<li><a href="<?php echo URL::site('/user/edit') ?>"><i class="fa fa-fw fa-pencil"></i> <?php echo __('Profile Settings') ?></a></li>
+								<li><a href="<?php echo URL::site('/user/password') ?>"><i class="fa fa-fw fa-lock"></i> <?php echo __('Change Password') ?></a></li>
 								<li class="divider"></li>
 								<?php if (User::is_admin()): ?>
 									<li><a href="<?php echo URL::site('/admin') ?>"><i class="fa fa-fw fa-dashboard"></i> <?php echo __('Dashboard') ?></a></li>
