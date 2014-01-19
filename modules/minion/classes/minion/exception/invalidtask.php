db86bd3478dc6807b46261af02ca930efe2ee0c7
Sandeep Sangamreddi <sandeepone@gmail.com>
2014-01-19 21:35:01 +0530

    Removed Kohana namespace in minion module

--- modules/minion/classes/minion/exception/invalidtask.php
+++ modules/minion/classes/minion/exception/invalidtask.php
@@ -1,3 +1,18 @@
 <?php
+/**
+ * Invalid Task Exception
+ *
+ * @package    Kohana
+ * @category   Minion
+ * @author     Kohana Team
+ * @copyright  (c) 2009-2011 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+class Minion_Exception_Invalidtask extends Minion_Exception {
 
-class Minion_Exception_Invalidtask extends Kohana_Minion_Exception_InvalidTask {}
+	public function format_for_cli()
+	{
+		return 'ERROR: '. $this->getMessage().PHP_EOL;
+	}
+
+}
