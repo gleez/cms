16f7c35fdc2ff1c731c3110e3f9b8895efae14e4
Sandeep Sangamreddi <sandeepone@gmail.com>
2014-01-19 17:00:53 +0530

    New fearure: Minion Database migrations support (WIP) initial commit

---
+++ modules/minion/classes/task/db/migrate/status.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Help task to display general instructons and list all tasks
+ *
+ * @package    Gleez\Minion\DB\Migrate
+ * @author     Gleez Team
+ * @version    1.0.0
+ * @copyright  (c) 2011-2014 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
+ */
+class Task_Db_Migrate_Status extends Minion_Task
+{
+	protected function _execute(array $options)
+	{
+
+		Minion_CLI::write('db:migrate:status shows current migration status');
+	}
+}
