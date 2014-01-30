99b22416b214f1390042855f6e5aaa296c53a8d7
Sandeep Sangamreddi <sandeepone@gmail.com>
2014-01-30 23:45:13 +0530

    Update feature: (WIP) Changed TableDrag js into standalone greet plugin.
    Currently it works for the menu items and need to fix for the widgets

--- modules/gleez/views/admin/menu/item/list.php
+++ modules/gleez/views/admin/menu/item/list.php
@@ -3,7 +3,7 @@
 
 	<?php echo Form::open(Route::get('admin/menu/item')->uri(array('action' => 'confirm', 'id' => $id)), array('id'=>'menu-form', 'class'=>'form')); ?>
 
-	<table id="admin-list-menu-items" class="table table-striped table-bordered table-highlight">
+	<table id="admin-list-menu-items" class="table table-striped table-bordered table-highlight" data-toggle="tabledrag">
 		<thead>
 			<tr>
 				<th><?php echo __('Name') ?></th>
@@ -14,7 +14,7 @@
 		</thead>
 		<tbody>
 		<?php foreach ($items as $item): ?>
-			<tr id="item-row-<?php echo $item['id'] ?>" class="draggable <?php echo Text::alternate("odd", "even") ?>">
+			<tr id="item-row-<?php echo $item['id'] ?>" class="draggable">
 				<td id="item-<?php echo $item['id'] ?>"  class="lid-<?php echo $item['lvl'] ?>">
 					<?php
 						$c = 2;
@@ -32,9 +32,9 @@
 				</td>
 
 				<td class="tabledrag-hide">
-					<?php echo Form::weight('mlid:'.$item['id'].'[weight]', 0, array('class' => 'menu-weight')) ?>
-					<?php echo Form::hidden('mlid:'.$item['id'].'[plid]', $item['pid'], array('class' => 'menu-plid')) ?>
-					<?php echo Form::hidden('mlid:'.$item['id'].'[mlid]', $item['id'], array('class' => 'menu-mlid')) ?>
+					<?php echo Form::weight('mlid:'.$item['id'].'[weight]', 0, array('class' => 'row-weight')) ?>
+					<?php echo Form::hidden('mlid:'.$item['id'].'[plid]', $item['pid'], array('class' => 'row-parent')) ?>
+					<?php echo Form::hidden('mlid:'.$item['id'].'[mlid]', $item['id'], array('class' => 'row-id')) ?>
 				</td>
 
 				<td class="action">
