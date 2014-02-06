2a0c7d064dd6146de3066eb07cb57287161cef97
Gleez Technologies <github@gleez.tech>
2014-02-06 21:21:05 +0530

    Fixed Issue: Updated admin layout and css fixes

--- modules/gleez/views/widgets/html.php
+++ modules/gleez/views/widgets/html.php
@@ -1,4 +1,4 @@
-<div id="widget-<?php echo $widget->module; ?>-<?php echo Text::plain($widget->name); ?> <?php echo (isset($id)) ? 'widget-'.$id : '' ?>" class="nav nav-sidebar panel panel-default widget blockme widget-<?php echo Text::plain($widget->name); ?> <?php echo ($widget->menu) ? 'widget-menu' : ''; ?> <?php echo (isset($zebra)) ? 'widget-'.$zebra : '' ?>">
+<div id="widget-<?php echo $widget->module; ?>-<?php echo Text::plain($widget->name); ?> <?php echo (isset($id)) ? 'widget-'.$id : '' ?>" class="panel panel-default widget widget-<?php echo Text::plain($widget->name); ?> <?php echo ($widget->menu) ? 'widget-menu' : ''; ?> <?php echo (isset($zebra)) ? 'widget-'.$zebra : '' ?>">
   	<?php if ($widget->show_title): ?>
 		<div class="panel-heading">
 			<h3 class="panel-title"><i class="fa <?php echo Text::plain($widget->icon); ?>"></i> <?php echo Text::plain($title); ?></h3>
