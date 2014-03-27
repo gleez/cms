6990f2d1810b303a26d828f1284caad47ab4f44e
Sergey Yakovlev <sadhooklay@gmail.com>
2014-03-27 10:06:42 +0400

    Fixed issue: Fixed button layout

--- modules/gleez/views/admin/taxonomy/list.php
+++ modules/gleez/views/admin/taxonomy/list.php
@@ -5,8 +5,10 @@
 	<div class="help">
 		<p><?php _e('Categories are needed for grouping content. Categories are grouped by category groups. For example, a category group called "Fruit" would contain the categories "Apple" and "Banana".'); ?></p>
 	</div>
-	<div class="col-sm-12 clearfix">
-		<?php echo HTML::anchor($add_url, '<i class="fa fa-plus fa-white"></i> '.__('Add New Group'), array('class' => 'btn btn-success pull-right')) ?>
+	<div class="row">
+		<div class="col-sm-12 form-actions-right">
+			<?php echo HTML::anchor($add_url, '<i class="fa fa-plus fa-white"></i> '.__('Add New Group'), array('class' => 'btn btn-success pull-right')) ?>
+		</div>
 	</div>
 
 	<table id="admin-list-vocabs" class="table table-striped table-bordered table-highlight" data-toggle="datatable" data-target="<?php echo $url?>" data-sorting='[["0", "desc"]]'>
