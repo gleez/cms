3e94bf33a341366100256601941ab8fc38a5c801
Sergey Yakovlev <sadhooklay@gmail.com>
2014-04-02 11:50:55 +0400

    Fixed issue: Renamed term to category

--- modules/gleez/classes/controller/page.php
+++ modules/gleez/classes/controller/page.php
@@ -483,7 +483,7 @@ class Controller_Page extends Template {
 
 		if ( ! $term->loaded())
 		{
-			throw HTTP_Exception::factory(404, 'Term ":term" Not Found', array(':term'=>$id));
+			throw HTTP_Exception::factory(404, 'Category ":term" not found', array(':term' => $id));
 		}
 
 		$this->title = __(':term', array(':term' => $term->name));
