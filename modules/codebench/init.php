d9ce4fc584c8068cf9c1d8dd9791bd937aaeb5a4
Sergey Yakovlev <sadhooklay@gmail.com>
2014-04-26 05:24:54 +0400

    Fixed issue: Fixed bug with finding classes
    + Initial pass for PSR-0 autoloading support
    + Updated Bench/Transliterate test (see gleez/cms#721)

--- modules/codebench/init.php
+++ modules/codebench/init.php
@@ -1,8 +1,22 @@
 <?php
+/**
+ * Setting the Routes
+ *
+ * @package    Codebench\Routing
+ * @author     Gleez Team
+ * @copyright  (c) 2011-2014 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
+ */
 
-// Catch-all route for Codebench classes to run
-Route::set('codebench', 'codebench(/<class>)')
+/** Routing setup */
+if (!Route::cache())
+{
+
+	// Catch-all route for Codebench classes to run
+	Route::set('codebench', 'codebench(/<class>)')
 	->defaults(array(
 		'controller' => 'codebench',
-		'action' => 'index',
-		'class' => NULL));
+		'action'     => 'index',
+		'class'      => NULL
+	));
+}
