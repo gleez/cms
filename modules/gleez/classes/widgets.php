fe325e8375f14e5624f54a9221a5ad3369123782
Gleez Technologies <gleez@server.gleeztech.org>
2014-07-30 13:57:47 +0530

    Fixed issue: Fix widget menu styling and updated cerber theme

--- modules/gleez/classes/widgets.php
+++ modules/gleez/classes/widgets.php
@@ -490,7 +490,7 @@ class Widgets {
 		}
 
 		$widget->name = str_replace('/', '-', $widget->name);
-		$widget->menu = (bool)(strpos($widget->name, 'menu-'));
+		$widget->menu = (bool)(strpos($widget->name, 'menu-') !== false);
 
 		return View::factory('widgets/' .$format)
 			->set('content', $widget->content)
