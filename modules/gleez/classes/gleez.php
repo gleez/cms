4cb5fc60b6e6d2c57723b531e68eeac84a92d2ec
Gleez Technologies <github@gleez.tech>
2014-08-04 13:16:21 +0530

    Bump version number

--- modules/gleez/classes/gleez.php
+++ modules/gleez/classes/gleez.php
@@ -4,7 +4,7 @@
  *
  * @package    Gleez
  * @author     Gleez Team
- * @version    1.1.4
+ * @version    1.1.5
  * @copyright  (c) 2011-2014 Gleez Technologies
  * @license    http://gleezcms.org/license Gleez CMS License
  */
@@ -14,7 +14,7 @@ class Gleez {
 	 * Release version
 	 * @type string
 	 */
-	const VERSION = '1.1.4';
+	const VERSION = '1.1.5';
 
 	/**
 	 * Minimal required version of php
