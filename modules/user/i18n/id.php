9e7021077af1c259c6c7885f74ac4bd3c22ae0e2
Disketsa - Azuya <hitamputih82@yahoo.com>
2014-08-12 23:02:37 +0700

    Fix Copyright

--- modules/user/i18n/id.php
+++ modules/user/i18n/id.php
@@ -4,7 +4,7 @@
  *
  * @package   user\I18n
  * @author    Azuya <disketsa@gmail.com>
- * @copyright (c) 2011-2013 Gleez Technologies
+ * @copyright (c) 2011-2014 Gleez Technologies
  * @license   http://gleezcms.org/license Gleez CMS License
  */
 return array(
