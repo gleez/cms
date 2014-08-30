e1f786ecaf007ad71bdf8e1cb4abb389c827d59a
Serghei Iakovlev <sadhooklay@gmail.com>
2014-08-31 01:16:24 +0300

    Updated translation: Separated and amended translation

---
+++ modules/mongodb/i18n/ru.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Translation into Russian
+ *
+ * @package   Gleez\I18n
+ * @author    Serghei Iakovlev <sadhooklay@gmail.com>
+ * @copyright (c) 2011-2014 Gleez Technologies
+ * @license   http://gleezcms.org/license Gleez CMS License
+ */
+return array(
+	// :
+	':name is read only!' => 'Поле :name доступно только для чтения!',
+	':name is write only!' => 'Поле :name доступно только для записи!',
+	// _
+	'_id of the document must be string or array of strings' => '_id документа должен быть строкой или массивом строк',
+	// A
+	// B
+	// C
+	// D
+	// E
+	// F
+	'Failed to load Mango config group: :group' => 'Mango не удалось загрузить конфигурационную группу :group',
+	// G
+	// H
+	// I
+	// J
+	// K
+	// L
+	// M
+	'Method :method not found' => 'Метод :method не найден',
+	'Method :name not found for :class.' => 'Метод ":method" для класса ":class" не найден.',
+	"Method doesn't exist: :method" => 'Метод ":method" не найден',
+	'Minimal required version of MongoDB driver is :version' => 'Необходима версия MongoDB драйвера не ниже :version',
+	// N
+	// O
+	// P
+	'Param $wtimeout must be numeric value.' => 'Параметр $wtimeout должен иметь числовое значение.',
+	// Q
+	// R
+	// S
+	// T
+	'The code must be a string or an instance of MongoCode' => 'Код должен быть строкой или экземпляром MongoCode',
+	'The cursor has already been instantiated' => 'Экземпляр курсора уже создан',
+	'The cursor has already started iterating' => 'Курсор уже начал итерацию',
+	'The php-mongo extension is not installed or is disabled.' => 'Расширение php-mongo не установлено или выключено.',
+	// U
+	'Unable to connect to MongoDB server. MongoDB said :message' => 'Не удалось подключиться к серверу MongoDB. Ответ MongoDB :message',
+	// Z
+	// Y
+	// X
+	// W
+	// V
+);
