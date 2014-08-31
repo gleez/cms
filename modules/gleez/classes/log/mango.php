5ebabdb8502f0fb65e7f549e8c552c8f60268e80
Serghei Iakovlev <sadhooklay@gmail.com>
2014-08-31 16:34:18 +0300

    Fixed issue: Cleanup Log_Mango

--- modules/gleez/classes/log/mango.php
+++ modules/gleez/classes/log/mango.php
@@ -12,46 +12,39 @@ use \Gleez\Mango\Client;
 /**
  * MongoDB log writer
  *
- * ### System Requirements
- *
- * - MondoDB 2.4 or higher
- * - PHP-extension MongoDB 1.4.0 or higher
- *
- * @package    Gleez\Logging
- * @author     Gleez Team
- * @version    0.2.4
+ * @package Gleez\Logging
+ * @author  Gleez Team
+ * @version 0.2.5
  */
 class Log_Mango extends Log_Writer {
 
 	/**
 	 * Collection for log
-	 * Use [Capped Collection](http://docs.mongodb.org/manual/core/capped-collections/)
-	 * to support high-bandwidth inserts
 	 * @var string
 	 */
-	protected $_collection;
+	private $collection;
 
 	/**
 	 * Database instance name
 	 * @var string
 	 */
-	protected $_name;
+	private $name;
 
 	/**
 	 * MongoDB object
 	 * @var MongoDB
 	 */
-	protected $_db;
+	private $db;
 
 	/**
 	 * Class constructor
 	 *
 	 * Creates a new MongoDB logger using [\Gleez\Mango\Client]
 	 *
-	 * Example:
-	 * ~~~
+	 * Example:<br>
+	 * <code>
 	 * $writer = new Log_Mango($collection);
-	 * ~~~
+	 * </code>
 	 *
 	 * @param   string  $collection  Collection name [Optional]
 	 * @param   string  $name        Database instance name [Optional]
@@ -60,39 +53,34 @@ class Log_Mango extends Log_Writer {
 	 */
 	public function __construct($collection = 'logs', $name = 'default')
 	{
-		$this->_collection  = $collection;
-		$this->_name        = $name;
+		$this->collection = $collection;
+		$this->name       = $name;
 
 		// Getting Mango instance
-		$this->_db = Client::instance($this->_name);
+		$this->db = Client::instance($this->name);
 	}
 
 	/**
 	 * Writes each of the messages into the MongoDB collection
 	 *
-	 * Example:
-	 * ~~~
+	 * Example:<br>
+	 * <code>
 	 * $writer->write($messages);
-	 * ~~~
+	 * </code>
 	 *
 	 * @param   array  $messages  An array of log messages
 	 *
-	 * @uses    Arr::merge
-	 * @uses    Request::$client_ip
-	 * @uses    Request::$user_agent
-	 * @uses    Request::uri
-	 * @uses    Request::initial
-	 * @uses    Text::plain
-	 * @uses    Log_Writer::$strace_level
+	 * @uses    Log_Writer::_log_levels
+	 * @uses    \Gleez\Mango\Client::selectCollection
+	 * @uses    \Gleez\Mango\Collection::batchInsert
 	 */
 	public function write(array $messages)
 	{
 		$logs      = array();
-		$exception = NULL;
+		$exception = null;
 
-		foreach ($messages as $message)
-		{
-			$exception = isset($message['additional']['exception']) ? $message['additional']['exception'] : NULL;
+		foreach ($messages as $message) {
+			$exception = isset($message['additional']['exception']) ? $message['additional']['exception'] : null;
 			$message['level'] = $this->_log_levels[$message['level']];
 
 			unset($message['additional'], $message['trace']);
@@ -100,11 +88,9 @@ class Log_Mango extends Log_Writer {
 			// FIX: $message should consist of an array of strings
 			$message = array_filter($message, 'is_string');
 
-			// See \MongoDate::__toString
 			$message['time'] = new MongoDate(strtotime($message['time']));
 
-			if ($exception && method_exists($exception, 'getTraceAsString'))
-			{
+			if ($exception && method_exists($exception, 'getTraceAsString')) {
 				// Re-use as much as possible, just resetting the body to the trace
 				$message['body']  = $exception->getTraceAsString();
 				$message['level'] = $this->_log_levels[Log_Writer::$strace_level];
@@ -115,6 +101,6 @@ class Log_Mango extends Log_Writer {
 		}
 
 		// Write messages
-		$this->_db->{$this->_collection}->batchInsert($logs);
+		$this->db->{$this->collection}->batchInsert($logs);
 	}
 }
