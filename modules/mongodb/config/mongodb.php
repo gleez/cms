a5bace318b5131c510f526cb8acda1e54029ae88
Serghei Iakovlev <sadhooklay@gmail.com>
2014-10-10 23:35:46 +0300

    Fixed issue: MongoDB driver fixes

--- modules/mongodb/config/mongodb.php
+++ modules/mongodb/config/mongodb.php
@@ -48,7 +48,7 @@ return array(
 				 *                            the query to MongoDB and piggy back a getLastError command (GLE) with
 				 *                            the Write Concern option at the same time.
 				 *                            See http://www.php.net/manual/en/mongo.writeconcerns.php
-				 * int      wtimeout          The number of milliseconds to wait for \MongoDB::$w replications to take
+				 * int      wTimeoutMS        The number of milliseconds to wait for \MongoDB::$w replications to take
 				 *                            place.
 				 */
 				'db'               => 'gleez', // Required
@@ -58,7 +58,7 @@ return array(
 				'password'         => FALSE,   // Optional
 				'replicaSet'       => FALSE,   // Optional
 				'w'                => 1,       // Optional
-				'wtimeout'         => 10000,   // Optional
+				'wTimeoutMS'       => 10000,   // Optional
 			),
 		),
 		/**
