2bc978aaddaf448d1a9126d5d2b188e2110f7c40
Gleez Technologies <github@gleez.tech>
2015-01-03 17:40:38 +0530

    New feature: Dynamic fontawesome icons list from css file

--- modules/gleez/classes/html.php
+++ modules/gleez/classes/html.php
@@ -8,7 +8,7 @@
  * @package    Gleez\Helpers
  * @author     Gleez Team
  * @version    1.1.4
- * @copyright  (c) 2011-2014 Gleez Technologies
+ * @copyright  (c) 2011-2015 Gleez Technologies
  * @license    http://gleezcms.org/license  Gleez CMS License
  */
 class HTML {
