9285f07850855d64151e48da80705f63861e6036
Sandeep Sangamreddi <sandeepone@gmail.com>
2015-03-08 16:25:11 +0530

    Bump version number

--- modules/gleez/classes/gleez.php
+++ modules/gleez/classes/gleez.php
@@ -4,7 +4,7 @@
  *
  * @package    Gleez
  * @author     Gleez Team
- * @version    1.1.6
+ * @version    1.1.7
  * @copyright  (c) 2011-2015 Gleez Technologies
  * @license    http://gleezcms.org/license Gleez CMS License
  */
@@ -14,7 +14,7 @@ class Gleez {
 	 * Release version
 	 * @type string
 	 */
-	const VERSION = '1.1.6';
+	const VERSION = '1.1.7';
 
 	/**
 	 * Minimal required version of php
