e1e8a9c3f90993bfc6830aac9d18a889502627b2
Serghei Iakovlev <sadhooklay@gmail.com>
2015-03-15 19:37:52 +0200

    Added some defaults

--- app/Application.php
+++ app/Application.php
@@ -13,6 +13,22 @@ use RuntimeException;
 
 class Application
 {
+    // Release version and codename
+    const VERSION  = '2.0.0-dev';
+    const CODENAME = 'Bocydium Globulare';
+
+    // Common environment type constants for consistency and convenience
+    const PRODUCTION  = 'production';
+    const STAGING     = 'staging';
+    const TESTING     = 'testing';
+    const DEVELOPMENT = 'development';
+
+    /**
+     * Current environment
+     * @var int
+     */
+    protected $environment = Application::DEVELOPMENT;
+
 	/**
 	 * Application constructor
 	 */
@@ -46,6 +62,60 @@ class Application
 	 */
 	protected function initEnvironment()
 	{
+        /**
+         * @const APP_START_TIME The start time of the application, used for profiling
+         */
+        define('APP_START_TIME', microtime(TRUE));
+
+        /**
+         * @const APP_START_MEMORY The memory usage at the start of the application, used for profiling
+         */
+        define('APP_START_MEMORY', memory_get_usage(defined('HHVM_VERSION')));
+
+        if (function_exists('mb_internal_encoding')) {
+            // Set internal character encoding to UTF-8.
+            mb_internal_encoding('UTF-8');
+        }
+
+        if (function_exists('mb_substitute_character')) {
+            // Set the mb_substitute_character to "none"
+            mb_substitute_character('none');
+        }
+
+        // Check environment variable
+        if (isset($_SERVER['APP_ENV'])) {
+            // Get environment variable from $_SERVER, .htaccess, apache.conf, nginx.conf, etc.
+            $env = $_SERVER['APP_ENV'];
+        } elseif (get_cfg_var('APP_ENV')) {
+            // Get environment variable from php.ini or from ini_get('user_ini.filename')
+            $env = get_cfg_var('APP_ENV');
+        } elseif (getenv('APP_ENV')) {
+            // Get environment variable from system environment
+            $env = getenv('APP_ENV');
+        } else {
+            $env = Application::DEVELOPMENT;
+        }
+
+        $env = strtoupper($env);
+
+        // Set environment variable if a 'APP_ENV' environment variable has been supplied.
+        defined(__CLASS__.'::'.$env) && $this->environment = constant(__CLASS__.'::'.$env);
+
+        // Enable xdebug parameter collection in development mode to improve fatal stack traces.
+        if (Application::DEVELOPMENT ===  $this->environment && extension_loaded('xdebug')) {
+            ini_set('xdebug.collect_params', 4);
+        }
+
+        if (Application::DEVELOPMENT ===  $this->environment) {
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL | E_STRICT);
+        } else {
+            ini_set('display_errors', 0);
+            ini_set('display_startup_errors', 0);
+            error_reporting(E_ALL ^ E_NOTICE);
+        }
+
 		if (!is_file(DOCROOT . '/vendor/autoload.php')) {
 			throw new RuntimeException(sprintf(
 				'Composer autoloader not found. Try to run "composer install" at :%s',
