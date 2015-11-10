3032b4ec09fea8beb3e60b6e6baa5a8db8244f79
Serghei Iakovlev <sadhooklay@gmail.com>
2015-11-10 22:10:26 +0200

    Updated issue #373: Moved View_Exception

---
+++ modules/gleez/classes/view/exception.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package    Gleez\View
+ * @author     Gleez Team
+ * @copyright  (c) 2011-2015 Gleez Technologies
+ * @license    http://gleezcms.org/license  Gleez CMS License
+ */
+class View_Exception extends Gleez_Exception
+{}
